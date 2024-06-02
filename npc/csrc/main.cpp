d19e1fd247bd47a4bc74fffaf031509a6b66327b
tracer-ysyx <tracer@ysyx.org>
2024-06-02 19:54:16 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:54:16 up  5:25,  1 user,  load average: 0.65, 0.69, 0.87

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,20 +1,36 @@
 #include <nvboard.h>
 #include <Vexample.h>
 
-static TOP_NAME dut;
+static dut_NAME dut;
 
-void nvboard_bind_all_pins(TOP_NAME* top);
+void nvboard_bind_all_pins(dut_NAME *dut);
 
+void single_cycle()
+{
+  dut->clk = 0;
+  dut->eval();
+  dut->clk = 1;
+  dut->eval();
+}
 
+void reset(int n)
+{
+  dut->rst = 1;
+  while (n-- > 0)
+    single_cycle();
+  dut->rst = 0;
+}
 
-int main() {
+int main()
+{
   nvboard_bind_all_pins(&dut);
   nvboard_init();
 
-  // reset(10);
+  reset(10); // 复位10个周期
 
-  while(1) {
-    nvboard_update();
+  while (1)
+  {
     dut.eval();
+    single_cycle();
   }
 }
