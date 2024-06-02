b95f87cbeda55b49b95e1a49250934f4195169b7
tracer-ysyx <tracer@ysyx.org>
2024-06-02 10:16:00 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:16:00 up  2:01,  1 user,  load average: 0.85, 0.73, 0.71

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <verilated.h>
+#include "../obj_dir/Vexample.h"
+#include <verilated_vcd_c.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char **argv, char **env){
+    Verilated::commandArgs(argc, argv);
+    Vexample* top = new Vexample;
+
+    int rec_time=0;
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *trace = new VerilatedVcdC;
+    top->trace(trace, 1);
+    trace->open("waveform.vcd");
+    while (rec_time<1000) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+
+    trace->dump(rec_time);
+    // 全局时钟计时
+    rec_time++;
+  }
+    trace->close();
+    top->final();
+    delete top;
+    exit(0);
 }
