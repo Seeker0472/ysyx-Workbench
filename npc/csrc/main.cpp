a6fa2b4f54425d90d3d5c013f39eb108f3dc2c74
tracer-ysyx <tracer@ysyx.org>
2024-06-02 19:19:40 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:19:40 up  4:51,  1 user,  load average: 1.48, 1.65, 1.45

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,34 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <verilated.h>
-#include "../obj_dir/Vexample.h"
-#include <verilated_vcd_c.h>
+#include <nvboard.h>
+#include <Vexample.h>
 
-int main(int argc, char **argv, char **env){
-    Verilated::commandArgs(argc, argv);
-    Vexample* top = new Vexample;
+static TOP_NAME dut;
 
-    int rec_time=0;
-    Verilated::traceEverOn(true);
-    VerilatedVcdC *trace = new VerilatedVcdC;
-    top->trace(trace, 1);
-    trace->open("waveform.vcd");
-    while (rec_time<1000) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
+void nvboard_bind_all_pins(TOP_NAME* top);
 
-    trace->dump(rec_time);
-    // 全局时钟计时
-    rec_time++;
+
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  // reset(10);
+
+  while(1) {
+    nvboard_update();
+    dut.eval();
   }
-    trace->close();
-    top->final();
-    delete top;
-    exit(0);
 }
--- npc/vsrc/example.v
+++ npc/vsrc/example.v
@@ -1,6 +1,7 @@
 module example(
   input a,
   input b,
+  // input clk,
   output f
 );
   assign f = a ^ b;
