bb3a736c095751ff4284c12753c8d66a4b7e8f42
郭明轩 <gmx472@qq.com>
2024-06-02 20:18:51 +0800

    流水

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,34 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <verilated.h>
-#include "../obj_dir/Vexample.h"
-#include <verilated_vcd_c.h>
+#include <nvboard.h>
+#include <Vexample.h>
+#include<iostream>
 
-int main(int argc, char **argv, char **env){
-    Verilated::commandArgs(argc, argv);
-    Vexample* top = new Vexample;
+static TOP_NAME dut;
 
-    int rec_time=0;
-    Verilated::traceEverOn(true);
-    VerilatedVcdC *trace = new VerilatedVcdC;
-    top->trace(trace, 1);
-    trace->open("waveform.vcd");
-    while (rec_time<1000) {
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
+void nvboard_bind_all_pins(TOP_NAME *dut);
 
-    trace->dump(rec_time);
-    // 全局时钟计时
-    rec_time++;
+void single_cycle(){
+  dut.clk = 0;
+  dut.eval();
+  dut.clk = 1;
+  dut.eval();
+}
+
+void reset(int n){
+  dut.rst = 1;
+  while (n-- > 0)
+    single_cycle();
+  dut.rst = 0;
+}
+
+int main(){
+  std::cout<<"start";
+  nvboard_bind_all_pins(&dut);
+  std::cout<<"start";
+
+  nvboard_init();
+  reset(10); // 复位10个周期
+  std::cout<<"start";
+
+  while (1)
+  {
+    dut.eval();
+    single_cycle();
+    nvboard_update();
+
   }
-    trace->close();
-    top->final();
-    delete top;
-    exit(0);
 }
