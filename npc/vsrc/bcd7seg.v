07a17d19577504969e33eed8f0c6011c718d8681
tracer-ysyx <tracer@ysyx.org>
2024-06-03 21:51:35 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:35 up  7:25,  1 user,  load average: 0.26, 0.36, 0.46

--- npc/vsrc/bcd7seg.v
+++ npc/vsrc/bcd7seg.v
@@ -9,7 +9,6 @@ module bcd7seg(
       4'b0001 : h = 7'b1001111;
       4'b0010 : h = 7'b0010010;
       4'b0011 : h = 7'b0000110;
-      // 4'b0011 : h = 7'b0110011;
       4'b0100 : h = 7'b1001100;
       4'b0101 : h = 7'b0100100;
       4'b0110 : h = 7'b0100000;
@@ -19,4 +18,16 @@ module bcd7seg(
       default : h = 7'b1111111;
     endcase
   end
+endmodule
+
+module encoder(
+  input [7:0] enco,
+  output reg [6:0] h,
+)
+int i;
+always @(*)begin
+  for(i=0;i<8;i++)
+    if(enco[i]==1)
+      bcd7seg(i[3:0],h)
+end
 endmodule
