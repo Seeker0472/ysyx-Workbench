3b143ddcf0b897cac02c788f9f587e695277ee32
tracer-ysyx <tracer@ysyx.org>
2024-06-03 22:02:39 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:02:39 up  7:36,  1 user,  load average: 1.70, 1.23, 0.80

--- npc/vsrc/bcd7seg.v
+++ npc/vsrc/bcd7seg.v
@@ -27,7 +27,7 @@ integer i;
 reg [3:0] bcd_input;
 bcd7seg seg0(bcd_input, h);
 always @(enco) begin
-    bcd_input = 4'b0000;
+    bcd_input = 4'b1111;
     for(i = 0; i < 8; i = i + 1) begin
         if(enco[i] == 1) begin
             bcd_input=i[3:0];
