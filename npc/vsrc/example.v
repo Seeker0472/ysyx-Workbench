2c0a162595961c025c82f8106c0564282a64a9c4
tracer-ysyx <tracer@ysyx.org>
2024-06-03 19:54:12 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:54:12 up  5:28,  1 user,  load average: 0.66, 0.49, 0.31

--- npc/vsrc/example.v
+++ npc/vsrc/example.v
@@ -97,7 +97,7 @@ module example(y,x0,x1,x2,x3,f);
   input [1:0] x2;
   input [1:0] x3;
   output [1:0] f;
-  MuxKeyWithDefault #(4,2,2) i0 (f,y,1'b00,{
+  MuxKeyWithDefault #(4,2,2) i0 (f,y,2'b00,{
     2'b00, x0,
     2'b01, x1,
     2'b10, x2,
