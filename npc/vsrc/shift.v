5778434f9fd2c35fc34f6033ce0e0bd863a9fd82
tracer-ysyx <tracer@ysyx.org>
2024-06-04 15:51:53 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:53 up  8:35,  1 user,  load average: 0.95, 0.65, 0.59

--- npc/vsrc/shift.v
+++ npc/vsrc/shift.v
@@ -1,8 +1,10 @@
 module shift(clk,h);
 input clk;
 output reg [6:0] h;
+output reg [6:0] k;
 reg [7:0] num = 8'b00000001;
-bcd7seg seg0(num, h);
+bcd7seg seg0(num[3:0], h);
+bcd7seg seg1(num[7:4], h);
 always @(posedge clk)begin
     num<={num[6:0],num[0]^num[2]^num[3]^num[4]};
 end
