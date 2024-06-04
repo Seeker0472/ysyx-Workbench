e1345c362f28f5ea31fb63b2c01cfbe40aa5f71a
tracer-ysyx <tracer@ysyx.org>
2024-06-04 09:51:20 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:51:20 up  2:34,  1 user,  load average: 1.02, 0.63, 0.45

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,5 +1,5 @@
 #include <nvboard.h>
-#include <Vencoder.h>
+#include <Valu.h>
 #include<iostream>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
---
+++ npc/vsrc/alu.v
@@ -0,0 +1,46 @@
+module mux43b(add,sub,rev,land,lor,eo,daxiao,eq,op,out);
+input [3:0] add;
+input [3:0] sub;
+input [3:0] rev;
+input [3:0] land;
+input [3:0] lor;
+input [3:0] eo;
+input [3:0] qudaxiao;
+input [3:0] eq;
+input [2:0] op;
+output [3:0] out;
+MuxKeyWithDefault #(8,3,4) i0(out,op,4'b0000,{
+    3'b000,add,
+    3'b001,sub,
+    3'b010,rev,
+    3'b011,land,
+    3'b100,lor,
+    3'b101,eo,
+    3'b110,qudaxiao,
+    3'b111,eq,
+})
+endmodule
+
+module add(A,B,OUT);
+input [3:0] A;
+input [3:0] B;
+output [3:0] OUT;
+assign OUT=A+B
+endmodule
+
+module alu(A,B,op,OUT);
+input [3:0] A;
+input [3:0] B;
+input [2:0] op;
+output [6:0] OUT;
+
+reg [3:0] add_out;
+add add0(A,B,add_out);
+
+reg [3:0] bcd_input;
+bcd7seg seg0(bcd_input, OUT);
+
+// always@(*) begin
+
+// end
+endmodule
