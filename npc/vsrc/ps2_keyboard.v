0a18abf3f9b721327129c747a8b9d1338e5d29cd
tracer-ysyx <tracer@ysyx.org>
2024-06-07 10:25:01 +0800

    > sim RTL ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:25:01 up 14 min,  1 user,  load average: 0.58, 0.52, 0.37

---
+++ npc/vsrc/ps2_keyboard.v
@@ -0,0 +1,141 @@
+module ps2_keyboard(clk,clrn,ps2_clk,ps2_data,data,
+                    ready,nextdata_n,overflow);
+    input clk,clrn,ps2_clk,ps2_data;
+    input nextdata_n;
+    output [7:0] data;
+    output reg ready;
+    output reg overflow;     // fifo overflow
+    // internal signal, for test
+    reg [9:0] buffer;        // ps2_data bits
+    reg [7:0] fifo[7:0];     // data fifo
+    reg [2:0] w_ptr,r_ptr;   // fifo write and read pointers
+    reg [3:0] count;  // count ps2_data bits
+    // detect falling edge of ps2_clk
+    reg [2:0] ps2_clk_sync;
+
+    always @(posedge clk) begin
+        ps2_clk_sync <=  {ps2_clk_sync[1:0],ps2_clk};
+    end
+
+    wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
+
+    always @(posedge clk) begin
+        if (clrn == 0) begin // reset
+            count <= 0; w_ptr <= 0; r_ptr <= 0; overflow <= 0; ready<= 0;
+        end
+        else begin
+            if ( ready ) begin // read to output next data
+                if(nextdata_n == 1'b0) //read next data
+                begin
+                    r_ptr <= r_ptr + 3'b1;
+                    if(w_ptr==(r_ptr+1'b1)) //empty
+                        ready <= 1'b0;
+                end
+            end
+            if (sampling) begin
+              if (count == 4'd10) begin
+                if ((buffer[0] == 0) &&  // start bit
+                    (ps2_data)       &&  // stop bit
+                    (^buffer[9:1])) begin      // odd  parity
+                    fifo[w_ptr] <= buffer[8:1];  // kbd scan code
+                    w_ptr <= w_ptr+3'b1;
+                    ready <= 1'b1;
+                    overflow <= overflow | (r_ptr == (w_ptr + 3'b1));
+                end
+                count <= 0;     // for next
+              end else begin
+                buffer[count] <= ps2_data;  // store ps2_data
+                count <= count + 3'b1;
+              end
+            end
+        end
+    end
+    assign data = fifo[r_ptr]; //always set output data
+
+endmodule
+
+`timescale 1ns / 1ps
+module ps2_keyboard_model(
+    output reg ps2_clk,
+    output reg ps2_data
+    );
+parameter [31:0] kbd_clk_period = 60;
+initial ps2_clk = 1'b1;
+
+task kbd_sendcode;
+    input [7:0] code; // key to be sent
+    integer i;
+
+    reg[10:0] send_buffer;
+    begin
+        send_buffer[0]   = 1'b0;  // start bit
+        send_buffer[8:1] = code;  // code
+        send_buffer[9]   = ~(^code); // odd parity bit
+        send_buffer[10]  = 1'b1;  // stop bit
+        i = 0;
+        while( i < 11) begin
+            // set kbd_data
+            ps2_data = send_buffer[i];
+            #(kbd_clk_period/2) ps2_clk = 1'b0;
+            #(kbd_clk_period/2) ps2_clk = 1'b1;
+            i = i + 1;
+        end
+    end
+endtask
+
+endmodule
+
+`timescale 1ns / 1ps
+module keyboard_sim;
+
+/* parameter */
+parameter [31:0] clock_period = 10;
+
+/* ps2_keyboard interface signals */
+reg clk,clrn;
+wire [7:0] data;
+wire ready,overflow;
+wire kbd_clk, kbd_data;
+reg nextdata_n;
+
+ps2_keyboard_model model(
+    .ps2_clk(kbd_clk),
+    .ps2_data(kbd_data)
+);
+
+ps2_keyboard inst(
+    .clk(clk),
+    .clrn(clrn),
+    .ps2_clk(kbd_clk),
+    .ps2_data(kbd_data),
+    .data(data),
+    .ready(ready),
+    .nextdata_n(nextdata_n),
+    .overflow(overflow)
+);
+
+initial begin /* clock driver */
+    clk = 0;
+    forever
+        #(clock_period/2) clk = ~clk;
+end
+
+initial begin
+    clrn = 1'b0;  #20;
+    clrn = 1'b1;  #20;
+    model.kbd_sendcode(8'h1C); // press 'A'
+    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1;//read data
+    model.kbd_sendcode(8'hF0); // break code
+    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
+    model.kbd_sendcode(8'h1C); // release 'A'
+    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
+    model.kbd_sendcode(8'h1B); // press 'S'
+    #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
+    #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
+    model.kbd_sendcode(8'hF0); // break code
+    model.kbd_sendcode(8'h1B); // release 'S'
+    #20;
+    $stop;
+end
+
+endmodule
