e9936ab2de4859401c436730aadee28fb002da1f
tracer-ysyx <tracer@ysyx.org>
2024-06-09 22:48:02 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:48:02 up  2:03,  1 user,  load average: 1.35, 0.73, 0.65

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -108,7 +108,7 @@ while(fscanf(fp,"%d %s",&result,exp)){
   bool ok=true;
   int res=expr(exp,&ok);
   if(ok&&result!=res)
-    printf("fail!");
+    printf("fail!  %s\n",exp);
   else
     printf("OK");
 }}
