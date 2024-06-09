04d00063eb3dba5eb30aacd454aa171828ccb372
tracer-ysyx <tracer@ysyx.org>
2024-06-09 21:59:36 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:59:36 up  1:14,  1 user,  load average: 0.59, 0.73, 0.76

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,7 +14,7 @@
 ***************************************************************************************/
 
 #include <common.h>
-// #define TEST_CALC
+#define TEST_CALC
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
@@ -32,7 +32,6 @@ while(fscanf(fp,"%d %s",&result,exp)){
   int res=expr(exp,&ok);
   if(ok&&result!=res)
     printf("fail!");
-
 }
 
 #endif
