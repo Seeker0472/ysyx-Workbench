b82e53396e793083cf9c1a45fcb2e4f044dfd81c
tracer-ysyx <tracer@ysyx.org>
2024-06-09 10:50:56 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:56 up  1:39,  1 user,  load average: 1.49, 0.78, 0.56

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,7 +89,7 @@ static int cmd_print_status(char *args){
 static int cmd_scan_mem(char *args){
   int n;
   paddr_t addr;
-  sscanf(args,"%d %u",&n,&addr);
+  sscanf(args,"%d %x",&n,&addr);
   for(;n>0;n--){
     //这个似乎有问题，看懂了以后自己实现！！！
     // printf("readMem %lu %lu %lu %lu" ,warp_pmem_read(addr,1),warp_pmem_read(addr+1,1),warp_pmem_read(addr+2,1),warp_pmem_read(addr+3,1));
