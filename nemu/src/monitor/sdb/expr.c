8bfe6e27f9e34ac73df1e002593bb4ab63bcb5c5
tracer-ysyx <tracer@ysyx.org>
2024-06-10 16:18:43 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:43 up 19:33,  1 user,  load average: 0.64, 0.46, 0.39

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -44,9 +44,9 @@ static struct rule {
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
+  {"0x[0-9]+", TK_HEX}, // hex-TODO:怎么处理？
   {"[0-9]+",TK_NUM},
   {" +", TK_NOTYPE},    // spaces
-  {"0x[0-9]+", TK_HEX}, // hex-TODO:怎么处理？
   {"$\\w+",TK_REG},     // reg-TODO:怎么处理？
   {"\\(",'('},
   {"\\)",')'},
