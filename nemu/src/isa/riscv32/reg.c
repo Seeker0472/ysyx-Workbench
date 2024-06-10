3f0d20975100152cdaad8e55d4d475e4c037c043
tracer-ysyx <tracer@ysyx.org>
2024-06-10 20:57:01 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:57:01 up 1 day, 12 min,  1 user,  load average: 1.12, 0.78, 0.66

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -34,7 +34,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   // printf("%lu",cpu.pc);
   if(s[0]=='$')
     s+=1;
-  if(strcmp(s,"pc")){
+  if(strcmp(s,"pc")==0){
     *success=true;
     return cpu.pc;
   }
