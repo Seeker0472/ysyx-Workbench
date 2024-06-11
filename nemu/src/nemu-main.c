73bf0526d7a541a697f6448e410a2804474df932
tracer-ysyx <tracer@ysyx.org>
2024-06-11 16:51:05 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:51:05 up  8:11,  1 user,  load average: 0.72, 0.39, 0.40

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -23,16 +23,31 @@ int is_exit_status_bad();
 word_t expr(char *e, bool *success) ;
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-#ifdef TEST_CALC
 
-
-#endif
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
 #else
   init_monitor(argc, argv);
 #endif
+#ifdef TEST_CALC
+  FILE *fp = fopen("/home/seeker/Develop/ysyx-workbench/nemu/tools/gen-expr/build/input", "r");
+  int result;
+  char *exp = malloc(700 * sizeof(char));
+  int time=0;
+  while (fscanf(fp, "%d %s\n", &result, exp) != -1)
+  {
+    // printf("%s\n",exp);
+    bool ok = true;
+    int res = (int)expr(exp, &ok);
+    if (ok && result != res){
+      printf("fail! %d/%d %s\n", res, result, exp);
+      assert(0);
+      }
+    else
+      printf("OK:%d\n",time++);
+  }
 
+#endif
   /* Start engine. */
   engine_start();
 
