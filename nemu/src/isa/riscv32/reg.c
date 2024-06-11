164cd01b3615cba88066556dfc4b4723535dd646
tracer-ysyx <tracer@ysyx.org>
2024-06-11 15:46:00 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:00 up  7:06,  1 user,  load average: 1.31, 0.77, 0.47

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,17 +24,17 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  printf("%-4s \t%-10s\t%-5s\t","Name","Dec","Hex");  
-  printf("%-4s \t%-10s\t%-5s\t\n","Name","Dec","Hex");
+  printf("%-4s \t%-20s\t%-10s\t","Name","Dec","Hex");  
+  printf("%-4s \t%-20s\t%-10s\t\n","Name","Dec","Hex");
   bool dis=false;
   for(int i=0;i<32;i++){
-    printf("%-4s \t%-10ld\t%-5lx\t",regs[i],cpu.gpr[i],cpu.gpr[i]);
+    printf("%-4s \t%-10ld\t%-10lx\t",regs[i],cpu.gpr[i],cpu.gpr[i]);
     if(dis){
       printf("\n");
     }
     dis=!dis;
   }
-  printf("%-4s \t%-10ld\t%-5lx\t","pc",cpu.pc,cpu.pc);
+  printf("%-4s \t%-20ld\t%-10lx\t\n","pc",cpu.pc,cpu.pc);
 }
 //获取寄存器的值，s应该传入$xx
 word_t isa_reg_str2val(const char *s, bool *success) {
