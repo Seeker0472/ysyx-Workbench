5e6f0327b1e478072719985621e44883df758bce
tracer-ysyx <tracer@ysyx.org>
2024-06-15 09:28:22 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:28:22 up  1:01,  1 user,  load average: 0.24, 0.22, 0.43

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -10,11 +10,11 @@ unsigned int sim_time=0;
 static int wave_enable=false;
 
 void single_cycle(){
-  // dut.clk = 0;
+  dut.clk = 0;
   dut.eval();
   if(wave_enable)
     tfp->dump(sim_time++); // Dump波形信息
-  // dut.clk = 1;
+  dut.clk = 1;
   dut.eval();
   if(wave_enable)
     tfp->dump(sim_time++); // Dump波形信息
