b9c6938b86d547f6946e996f73b402022de9700f
tracer-ysyx <tracer@ysyx.org>
2024-06-30 15:24:42 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:42 up  6:25,  1 user,  load average: 2.74, 2.10, 1.47

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -25,10 +25,10 @@
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
 #define Info_N(fmt, ...) \
-    printf(ANSI_BG_WHITE fmt , ##__VA_ARGS__)
+    printf(ANSI_FG_WHITE fmt , ##__VA_ARGS__)
 
 #define Info_R(fmt, ...) \
-    printf(ANSI_BG_MAGENTA fmt , ##__VA_ARGS__)
+    printf(ANSI_FG_MAGENTA fmt , ##__VA_ARGS__)
 
 #define Assert(cond, format, ...) \
   do { \
