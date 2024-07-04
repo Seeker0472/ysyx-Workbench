39d82f8b08308919af6926433fd02151b14010e7
tracer-ysyx <tracer@ysyx.org>
2024-07-04 20:32:08 +0800

    > generate verilog ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:08 up 15 min,  1 user,  load average: 1.36, 0.93, 0.86

--- npc/src/main/Elaborate.scala
+++ npc/src/main/Elaborate.scala
@@ -5,7 +5,7 @@ object Elaborate extends App {
     "disallowLocalVariables",// If true, do not emit SystemVerilog locally scoped "automatic" or logic declarations - emit top level wire and reg's instead.
     "disallowPackedArrays",//If true, eliminate packed arrays for tools that don't support them (e.g. Yosys).
     "locationInfoStyle=wrapInAtSquareBracket",//wrapInAtSquareBracket: // @[perf/regress/AndNot.fir:3:10, :7:{10,17}]
-    "--split-verilog"
+    "split-verilog"
   ).reduce(_ + "," + _))
   circt.stage.ChiselStage.emitSystemVerilogFile(new core.core(), args, firtoolOptions)
 }
