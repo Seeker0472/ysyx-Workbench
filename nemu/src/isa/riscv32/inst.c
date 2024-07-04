3a3a116bc40e6d05064d3739855d52dfee7b66a8
tracer-ysyx <tracer@ysyx.org>
2024-07-04 22:03:06 +0800

    > compile NEMU ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:06 up 26 min,  1 user,  load average: 0.91, 1.09, 0.89

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -150,7 +150,7 @@ static int decode_exec(Decode *s) {
 
 
   INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh    , R,printf("%x---%x\n",src1,src2); R(rd)=mulh(src1, src2););//TODO:Why?
-  INSTPAT("0000001 ????? ????? 100 ????? 01110 11", divu   , R, R(rd)=Ext32((src1)/(src2)););
+  INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu   , R, R(rd)=Ext32((src1)/(src2)););
 
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
