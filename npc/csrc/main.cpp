98c47aef78495bf132c362902b3a8c9a49c817dc
tracer-ysyx <tracer@ysyx.org>
2024-07-05 15:14:12 +0800

    > generate verilog ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:14:12 up  1:37,  1 user,  load average: 0.74, 4.51, 5.72

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -14,49 +14,15 @@ Vcore *dut;
 unsigned int sim_time = 0;
 static int wave_enable = true;
 
-//使用DPI-C机制实现ebreak
-void call_ebreak(){
+// 使用DPI-C机制实现ebreak
+void call_ebreak()
+{
   printf("Ebreak Called!!");
   exit(0);
 }
 static char *img_file = NULL;
 static char *elf_file = NULL;
-
-static int parse_args(int argc, char *argv[]) {
-  const struct option table[] = {
-    // {"batch"    , no_argument      , NULL, 'b'},
-    // {"log"      , required_argument, NULL, 'l'},
-    // {"diff"     , required_argument, NULL, 'd'},
-    // {"port"     , required_argument, NULL, 'p'},
-    // {"help"     , no_argument      , NULL, 'h'},
-    {"elf-file" , required_argument, NULL, 'e'},
-    {0          , 0                , 0,  0 },
-  };
-  int o;
-  while ( (o = getopt_long(argc, argv, "-e:", table, NULL)) != -1) {
-    switch (o) {
-      // case 'b': sdb_set_batch_mode(); break;
-      // case 'p': sscanf(optarg, "%d", &difftest_port); break;
-      // case 'l': log_file = optarg; break;
-      // case 'd': diff_so_file = optarg; break;
-      
-      case 'e': elf_file = optarg; break;
-      case 1: img_file = optarg; printf("%s",img_file);return 0;
-      default:
-        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-        // printf("\t-b,--batch              run with batch mode\n");
-        // printf("\t-l,--log=FILE           output log to FILE\n");
-        // printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
-        // printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-        // printf("\t-e,--elf-file=FILE      input Elf File\n");
-        printf("\n");
-        exit(0);
-    }
-  }
-  return 0;
-}
-
-int default_insts[100] = {
+static uint8_t mem[100000]  = {
     0x00448493,
     0x00448493,
     0x00448493,
@@ -80,8 +46,75 @@ int default_insts[100] = {
     0x00448493,
 };
 
-int mem_read(int pc){
-  return default_insts[(pc-0x80000000)/4];
+static int parse_args(int argc, char *argv[])
+{
+  const struct option table[] = {
+      // {"batch"    , no_argument      , NULL, 'b'},
+      // {"log"      , required_argument, NULL, 'l'},
+      // {"diff"     , required_argument, NULL, 'd'},
+      // {"port"     , required_argument, NULL, 'p'},
+      // {"help"     , no_argument      , NULL, 'h'},
+      {"elf-file", required_argument, NULL, 'e'},
+      {0, 0, 0, 0},
+  };
+  int o;
+  while ((o = getopt_long(argc, argv, "-e:", table, NULL)) != -1)
+  {
+    switch (o)
+    {
+      // case 'b': sdb_set_batch_mode(); break;
+      // case 'p': sscanf(optarg, "%d", &difftest_port); break;
+      // case 'l': log_file = optarg; break;
+      // case 'd': diff_so_file = optarg; break;
+
+    case 'e':
+      elf_file = optarg;
+      break;
+    case 1:
+      img_file = optarg;
+      printf("%s", img_file);
+      return 0;
+    default:
+      printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+      // printf("\t-b,--batch              run with batch mode\n");
+      // printf("\t-l,--log=FILE           output log to FILE\n");
+      // printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+      // printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+      // printf("\t-e,--elf-file=FILE      input Elf File\n");
+      printf("\n");
+      exit(0);
+    }
+  }
+  return 0;
+}
+
+// int default_insts[100] = {
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00100073,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+//     0x00448493,
+// };
+
+int mem_read(int pc)
+{
+  return default_insts[(pc - 0x80000000) / 4];
 }
 
 void single_cycle()
@@ -104,9 +137,31 @@ void reset(int n)
   dut->reset = 0;
 }
 
+void init_img()
+{
+  if (img_file != NULL)
+  {
+    FILE *fp = fopen(img_file, "rb");
+    Assert(fp, "Can not open '%s'", img_file);
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    Log("The image is %s, size = %ld", img_file, size);
+    fseek(fp, 0, SEEK_SET);
+    int ret = fread(mem, size, 1, fp);
+    assert(ret == 1);
+    fclose(fp);
+    // return size;
+  }
+  else
+  {
+    printf("No Img file Loaded,Using Default IMG");
+  }
+}
+
 int main(int argc, char **argv)
 {
-  parse_args(argc,argv);
+  parse_args(argc, argv);
+  init_img();
   dut = new Vcore; // Initialize the DUT instance
   // Verilated::commandArgs(argc, argv);
   Verilated::traceEverOn(true); // 启用波形追踪
@@ -122,11 +177,11 @@ int main(int argc, char **argv)
     single_cycle();
     // sleep(1);
     // printf("%x\n", dut->io_pc);
-    dut->io_instr=mem_read(dut->io_pc);
-  //   if(dut->io_pc==0x80000060){
-  // tfp->close(); // 关闭VCD文件
+    dut->io_instr = mem_read(dut->io_pc);
+    //   if(dut->io_pc==0x80000060){
+    // tfp->close(); // 关闭VCD文件
 
-  //     return 0;}
+    //     return 0;}
     // std::cout<<"1"<<dut->clk<<dut->led<<std::endl;
   }
   tfp->close(); // 关闭VCD文件
