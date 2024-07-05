77781593688967a3f0d23acd35cb54fb73e49f2b
tracer-ysyx <tracer@ysyx.org>
2024-07-05 15:15:09 +0800

    > generate verilog ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:09 up  1:38,  1 user,  load average: 0.64, 3.84, 5.42

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -22,7 +22,7 @@ void call_ebreak()
 }
 static char *img_file = NULL;
 static char *elf_file = NULL;
-static uint16_t mem[100000]  = {
+static uint32_t mem[100000]  = {
     0x00448493,
     0x00448493,
     0x00448493,
