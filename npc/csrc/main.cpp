491add831e296be5e73eb724f62e6012b8bb2887
tracer-ysyx <tracer@ysyx.org>
2024-07-05 15:16:01 +0800

    > generate verilog ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:16:01 up  1:39,  1 user,  load average: 0.92, 3.43, 5.20

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -114,7 +114,7 @@ static int parse_args(int argc, char *argv[])
 
 int mem_read(int pc)
 {
-  return default_insts[(pc - 0x80000000) / 4];
+  return mem[(pc - 0x80000000) / 4];
 }
 
 void single_cycle()
@@ -145,10 +145,11 @@ void init_img()
     Assert(fp, "Can not open '%s'", img_file);
     fseek(fp, 0, SEEK_END);
     long size = ftell(fp);
-    Log("The image is %s, size = %ld", img_file, size);
+    // Log("The image is %s, size = %ld", img_file, size);
+    printf("The image is %s, size = %ld", img_file, size);
     fseek(fp, 0, SEEK_SET);
     int ret = fread(mem, size, 1, fp);
-    assert(ret == 1);
+    // assert(ret == 1);
     fclose(fp);
     // return size;
   }
