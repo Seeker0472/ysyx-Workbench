a945d74bcf479d0c47a7fd5f294675ece291ecb6
tracer-ysyx <tracer@ysyx.org>
2024-07-08 14:40:37 +0800

    > generate verilog ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:37 up  5:48,  1 user,  load average: 2.58, 1.44, 1.22

--- npc/src/main/core/ALU.scala
+++ npc/src/main/core/ALU.scala
@@ -9,6 +9,7 @@ import Constants_Val._
 class ALU extends Module{
     val io=IO(new Bundle{
       val in = new ALU_I
+      // val out
     })
 // // 默认情况下，dst 输出为 0
 //   io.dst := 0.U
@@ -17,20 +18,20 @@ class ALU extends Module{
 //   when(io.addi) {
 //     io.dst := io.src1 + io.imm
 //   }
-    val add_val = io.in.src1 + io.in.src2
-    val sub_val = io.in.src1 - io.in.src2
-    val xor = io.in.src1 ^ io.in.src2
-    val or = io.in.src1 | io.in.src2
-    val and = io.in.src1 & io.in.src2
-    val sll = io.in.src1 & io.in.src2
-    val slli = io.in.src1 & (io.in.src2 & "B1111".U)
-    val result=MuxLookup(io.in.operation,UInt(CVAL.DLEN.W))(Seq{
-      Inst.add.asUInt ->  add_val
-      Inst.sub.asUInt ->  sub_val
-      Inst.xor.asUInt ->  xor
-      Inst.or.asUInt ->  or
-      Inst.and.asUInt ->  and
-      Inst.sll.asUInt ->  sll
-      Inst.slli.asUInt ->  slli
-    })
+    // val add_val = io.in.src1 + io.in.src2
+    // val sub_val = io.in.src1 - io.in.src2
+    // val xor = io.in.src1 ^ io.in.src2
+    // val or = io.in.src1 | io.in.src2
+    // val and = io.in.src1 & io.in.src2
+    // val sll = io.in.src1 & io.in.src2
+    // val slli = io.in.src1 & (io.in.src2 & "B1111".U)
+    // val result=MuxLookup(io.in.operation,UInt(CVAL.DLEN.W))(Seq{
+    //   Inst.add.asUInt ->  add_val
+    //   Inst.sub.asUInt ->  sub_val
+    //   Inst.xor.asUInt ->  xor
+    //   Inst.or.asUInt ->  or
+    //   Inst.and.asUInt ->  and
+    //   Inst.sll.asUInt ->  sll
+    //   Inst.slli.asUInt ->  slli
+    // })
 }
--- npc/src/main/core/core.scala
+++ npc/src/main/core/core.scala
@@ -15,15 +15,16 @@ class core extends Module {
     val addr  = Input(UInt(CVAL.DLEN.W))
     val instr = Input(UInt(CVAL.ILEN.W))
   })
-  val pc = RegInit("h80000000".U(32.W))
+  val pc = RegInit("h80000000".U(CVAL.DLEN.W))
   io.pc := pc
 
-  val rs1 = Wire(UInt(5.W))
+  val src1 = Wire(UInt(CVAL.DLEN.W))
+  val src2 = Wire(UInt(CVAL.DLEN.W))
   val rd  = Wire(UInt(5.W))
   // val addi     = Reg(Bool())
   val write_en = false.B
 
-  val src1 = Wire(UInt(32.W))
+  // val src1 = Wire(UInt(32.W))
   val res  = Wire(UInt(32.W))
 
   val decoder = Module(new Decoder())
@@ -32,17 +33,18 @@ class core extends Module {
   val br_han  = Module(new ebreak_handler())
 
   decoder.io.instr := io.instr
-  alu.io.imm       := decoder.io.out.imm
-  rs1              := decoder.io.out.rs1
+  alu.io.in.src1       := decoder.io.out.src1
+  alu.io.in.src2       := decoder.io.out.src2
+  // rs1              := decoder.io.out.rs1
   rd               := decoder.io.out.rd
   // br_han.io.halt   := decoder.io.ebreak
 
-  reg.io.read_i := rs1
+  // reg.io.read_i := rs1
   src1          := reg.io.read
 
-  alu.io.src1 := src1  
+  // alu.io.src1 := src1  
   // alu.io.addi := addi
-  res      := alu.io.dst
+  res      := alu.io
   io.value := res
 
   reg.io.write_i  := rd
