237025d7b495da4c213f49fc54815502fc3bfad3
tracer-ysyx <tracer@ysyx.org>
2024-07-08 14:48:39 +0800

    > generate verilog ysyx_NAN 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:48:39 up  5:56,  1 user,  load average: 3.30, 2.56, 1.81

--- npc/src/main/core/decoder.scala
+++ npc/src/main/core/decoder.scala
@@ -49,54 +49,54 @@ class Decoder extends Module {
   })
   val Patterns = Seq(
     //block1
-    InsP(name_in = "add",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("000"), func7 = BitPat("0000000")),
-    InsP(name_in = "sub",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("000"), func7 = BitPat("0100000")),
-    InsP(name_in = "xor",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("100"), func7 = BitPat("0000000")),
-    InsP(name_in = "or",Inst_Type=Inst_Type_Enum.R_Type, opcode   = BitPat("0110011"), func3 = BitPat("110"), func7 = BitPat("0000000")),
-    InsP(name_in = "and",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("111"), func7 = BitPat("0000000")),
-    InsP(name_in = "sll",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("001"), func7 = BitPat("0000000")),
-    InsP(name_in = "srl",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("101"), func7 = BitPat("0000000")),
-    InsP(name_in = "sra",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("101"), func7 = BitPat("0100000")),
-    InsP(name_in = "slt",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("0110011"), func3 = BitPat("010"), func7 = BitPat("0000000")),
-    InsP(name_in = "sltu",Inst_Type=Inst_Type_Enum.R_Type, opcode = BitPat("0110011"), func3 = BitPat("011"), func7 = BitPat("0000000")),
+    InsP(name_in = "add",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b000"), func7 = BitPat("b0000000")),
+    InsP(name_in = "sub",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b000"), func7 = BitPat("b0100000")),
+    InsP(name_in = "xor",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b100"), func7 = BitPat("b0000000")),
+    InsP(name_in = "or",Inst_Type=Inst_Type_Enum.R_Type, opcode   = BitPat("b0110011"), func3 = BitPat("b110"), func7 = BitPat("b0000000")),
+    InsP(name_in = "and",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b111"), func7 = BitPat("b0000000")),
+    InsP(name_in = "sll",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b001"), func7 = BitPat("b0000000")),
+    InsP(name_in = "srl",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b101"), func7 = BitPat("b0000000")),
+    InsP(name_in = "sra",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b101"), func7 = BitPat("b0100000")),
+    InsP(name_in = "slt",Inst_Type=Inst_Type_Enum.R_Type, opcode  = BitPat("b0110011"), func3 = BitPat("b010"), func7 = BitPat("b0000000")),
+    InsP(name_in = "sltu",Inst_Type=Inst_Type_Enum.R_Type, opcode = BitPat("b0110011"), func3 = BitPat("b011"), func7 = BitPat("b0000000")),
     //block2
-    InsP(name_in = "addi",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010011"), func3 = BitPat("000")),
-    InsP(name_in = "xori",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010011"), func3 = BitPat("100")),
-    InsP(name_in = "ori",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("0010011"), func3 = BitPat("110")),
-    InsP(name_in = "andi",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010011"), func3 = BitPat("111")),
-    InsP(name_in = "slli",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010011"), func3 = BitPat("001"), func7 = BitPat("0000000")),
+    InsP(name_in = "addi",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010011"), func3 = BitPat("b000")),
+    InsP(name_in = "xori",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010011"), func3 = BitPat("b100")),
+    InsP(name_in = "ori",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("b0010011"), func3 = BitPat("b110")),
+    InsP(name_in = "andi",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010011"), func3 = BitPat("b111")),
+    InsP(name_in = "slli",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010011"), func3 = BitPat("b001"), func7 = BitPat("b0000000")),
     //imm[5:11]=0x00
-    InsP(name_in = "srli",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010011"), func3 = BitPat("101"), func7 = BitPat("0000000")),
+    InsP(name_in = "srli",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010011"), func3 = BitPat("b101"), func7 = BitPat("b0000000")),
     //imm[5:11]=0x00
-    InsP(name_in = "srai",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010011"), func3 = BitPat("101"), func7 = BitPat("0100000")),
+    InsP(name_in = "srai",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010011"), func3 = BitPat("b101"), func7 = BitPat("b0100000")),
     //imm[5:11]=0x20
-    InsP(name_in = "slti",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("0010011"), func3 = BitPat("010")),
-    InsP(name_in = "sltiu",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010011"), func3 = BitPat("011")),
+    InsP(name_in = "slti",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("b0010011"), func3 = BitPat("b010")),
+    InsP(name_in = "sltiu",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010011"), func3 = BitPat("b011")),
     //block3
-    InsP(name_in = "lb",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("0000011"), func3 = BitPat("000")),
-    InsP(name_in = "lh",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("0000011"), func3 = BitPat("001")),
-    InsP(name_in = "lw",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("0000011"), func3 = BitPat("010")),
-    InsP(name_in = "lbu",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0000011"), func3 = BitPat("100")),
-    InsP(name_in = "lhu",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0000011"), func3 = BitPat("101")),
+    InsP(name_in = "lb",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("b0000011"), func3 = BitPat("b000")),
+    InsP(name_in = "lh",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("b0000011"), func3 = BitPat("b001")),
+    InsP(name_in = "lw",Inst_Type=Inst_Type_Enum.I_Type, opcode  = BitPat("b0000011"), func3 = BitPat("b010")),
+    InsP(name_in = "lbu",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0000011"), func3 = BitPat("b100")),
+    InsP(name_in = "lhu",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0000011"), func3 = BitPat("b101")),
     //block4
-    InsP(name_in = "sb",Inst_Type=Inst_Type_Enum.S_Type ,opcode = BitPat("0100011"), func3 = BitPat("000")),
-    InsP(name_in = "sh",Inst_Type=Inst_Type_Enum.S_Type , opcode = BitPat("0100011"), func3 = BitPat("001")),
-    InsP(name_in = "sw",Inst_Type=Inst_Type_Enum.S_Type ,opcode = BitPat("0100011"), func3 = BitPat("010")),
+    InsP(name_in = "sb",Inst_Type=Inst_Type_Enum.S_Type ,opcode = BitPat("b0100011"), func3 = BitPat("b000")),
+    InsP(name_in = "sh",Inst_Type=Inst_Type_Enum.S_Type , opcode = BitPat("b0100011"), func3 = BitPat("b001")),
+    InsP(name_in = "sw",Inst_Type=Inst_Type_Enum.S_Type ,opcode = BitPat("b0100011"), func3 = BitPat("b010")),
     //block5
-    InsP(name_in = "beq",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("1100011"), func3 = BitPat("000")),
-    InsP(name_in = "bne",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("1100011"), func3 = BitPat("001")),
-    InsP(name_in = "blt",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("1100011"), func3 = BitPat("100")),
-    InsP(name_in = "bge",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("1100011"), func3 = BitPat("101")),
-    InsP(name_in = "blut",Inst_Type=Inst_Type_Enum.B_Type , opcode = BitPat("1100011"), func3 = BitPat("110")),
-    InsP(name_in = "bgeu", Inst_Type=Inst_Type_Enum.B_Type ,opcode = BitPat("1100011"), func3 = BitPat("111")),
+    InsP(name_in = "beq",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("b1100011"), func3 = BitPat("b000")),
+    InsP(name_in = "bne",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("b1100011"), func3 = BitPat("b001")),
+    InsP(name_in = "blt",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("b1100011"), func3 = BitPat("b100")),
+    InsP(name_in = "bge",Inst_Type=Inst_Type_Enum.B_Type , opcode  = BitPat("b1100011"), func3 = BitPat("b101")),
+    InsP(name_in = "blut",Inst_Type=Inst_Type_Enum.B_Type , opcode = BitPat("b1100011"), func3 = BitPat("b110")),
+    InsP(name_in = "bgeu", Inst_Type=Inst_Type_Enum.B_Type ,opcode = BitPat("b1100011"), func3 = BitPat("b111")),
     //block5
-    InsP(name_in = "jal",Inst_Type=Inst_Type_Enum.J_Type , opcode  = BitPat("1101111")),
-    InsP(name_in = "jalr",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("1100111"), func3 = BitPat("000")),
+    InsP(name_in = "jal",Inst_Type=Inst_Type_Enum.J_Type , opcode  = BitPat("b1101111")),
+    InsP(name_in = "jalr",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b1100111"), func3 = BitPat("b000")),
     //block6
-    InsP(name_in = "lui",Inst_Type=Inst_Type_Enum.U_Type, opcode   = BitPat("0110111")),
-    InsP(name_in = "auipc",Inst_Type=Inst_Type_Enum.U_Type,  opcode = BitPat("0010111")),
+    InsP(name_in = "lui",Inst_Type=Inst_Type_Enum.U_Type, opcode   = BitPat("b0110111")),
+    InsP(name_in = "auipc",Inst_Type=Inst_Type_Enum.U_Type,  opcode = BitPat("b0010111")),
     //blocks
-    InsP(name_in = "ecall/break",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("0010111"))
+    InsP(name_in = "ecall/break",Inst_Type=Inst_Type_Enum.I_Type, opcode = BitPat("b0010111"))
     // InsP(name_in = "ebreak", opcode = BitPat("0010111"))
   )
 
