2fcef0d19b914bbe2447a7dd3f6ffe5312ea1911
tracer-ysyx <tracer@ysyx.org>
2024-07-09 17:10:39 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:39 up  8:34,  1 user,  load average: 1.14, 0.85, 0.73

--- npc/src/main/core/ebreak_handler.scala
+++ npc/src/main/core/ebreak_handler.scala
@@ -12,7 +12,7 @@ class ebreak_handler extends BlackBox with HasBlackBoxInline {
       |module ebreak_handler(
       |  input halt
       |);
-      |always @(posedge *) begin
+      |always @(*) begin
       |   if (halt) begin
       |      call_ebreak();
       |  end
