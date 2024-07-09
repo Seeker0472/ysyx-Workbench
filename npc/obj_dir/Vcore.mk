734a91eced185d850abb706a846602c0b6c10965
tracer-ysyx <tracer@ysyx.org>
2024-07-09 21:43:52 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:52 up 40 min,  1 user,  load average: 0.55, 0.76, 0.93

--- npc/obj_dir/Vcore.mk
+++ npc/obj_dir/Vcore.mk
@@ -37,19 +37,10 @@ VM_MODPREFIX = Vcore
 VM_USER_CFLAGS = \
 	-I/home/seeker/Develop/ysyx-workbench/npc/csrc/include/ \
 	-save-temps \
-	-I/usr/lib/llvm-14/include \
-	-std=c++14 \
-	-fno-exceptions \
-	-D_GNU_SOURCE \
-	-D__STDC_CONSTANT_MACROS \
-	-D__STDC_FORMAT_MACROS \
-	-D__STDC_LIMIT_MACROS \
-	-fPIE \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
 	-lreadline \
-	-lLLVM-14 \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
