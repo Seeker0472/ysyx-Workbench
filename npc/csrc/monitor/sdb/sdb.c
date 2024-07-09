739fe1f90dd973c406cb9899fa56eae065b6c2d7
tracer-ysyx <tracer@ysyx.org>
2024-07-09 16:17:45 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:17:45 up  7:42,  1 user,  load average: 0.64, 0.77, 0.90

--- npc/csrc/monitor/sdb/sdb.c
+++ npc/csrc/monitor/sdb/sdb.c
@@ -39,7 +39,7 @@ static char* rl_gets() {
     line_read = NULL;
   }
 
-  line_read = readline("(nemu) ");
+  line_read = readline("(YDB) ");
 
   if (line_read && *line_read) {
     // add_history(line_read);TODO::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
