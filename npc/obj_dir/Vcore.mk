78b6cccf9b38d9619898f5235be5c00378fc8eda
tracer-ysyx <tracer@ysyx.org>
2024-07-10 16:53:27 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:27 up  8:20,  1 user,  load average: 1.23, 1.08, 1.00

--- npc/obj_dir/Vcore.mk
+++ npc/obj_dir/Vcore.mk
@@ -48,8 +48,8 @@ VM_USER_CFLAGS = \
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
 	-lreadline \
-	-l/home/seeker/Develop/ysyx-workbench/nemu/build/riscv32-nemu-interpreter-so \
 	-lLLVM-14 \
+	-l/home/seeker/Develop/ysyx-workbench/nemu/build/riscv32-nemu-interpreter-so \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
