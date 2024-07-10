fa1ac4b408f7c9f7d4f0e3f89149494b50986aa8
tracer-ysyx <tracer@ysyx.org>
2024-07-10 18:05:35 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:05:35 up  9:33,  1 user,  load average: 0.92, 1.14, 1.19

--- npc/csrc/utils/difftest.c
+++ npc/csrc/utils/difftest.c
@@ -6,6 +6,9 @@
 
 extern CPU_state *cpu;
 CPU_state *ref_cpu;
+extern uint32_t mem[10000000];
+extern word_t mem_size;
+
 bool difftest_check_regs(){
     return true;
 }
@@ -20,5 +23,5 @@ void difftest_init_all(){
     difftest_init(0);
     ref_cpu=(CPU_state*)malloc(sizeof(CPU_state));
     difftest_regcpy(cpu,DIFFTEST_TO_REF);
-    // difftest_memcpy();
+    difftest_memcpy(0x80000000,mem,mem_size,DIFFTEST_TO_REF);
 }
