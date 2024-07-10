37ea2c0a13ff1ca42ffdff45b2e757bc1106c0f7
tracer-ysyx <tracer@ysyx.org>
2024-07-10 14:49:44 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:44 up  6:17,  1 user,  load average: 1.86, 3.62, 3.10

--- npc/csrc/include/generated/autoconf.h
+++ npc/csrc/include/generated/autoconf.h
@@ -6,14 +6,12 @@
  */
 #define CONFIG_WATCHPOINT 1
 #define CONFIG_ITRACE 1
-#define CONFIG_RV64 1
 #define CONFIG_DIFFTEST 1
 #define CONFIG_ISA_riscv 1
 #define CONFIG_TRACE_END 10000
 #define CONFIG_FTRACE 1
 #define CONFIG_MTRACE 1
-#define CONFIG_ISA64 1
 #define CONFIG_ITRACE_COND "true"
 #define CONFIG_TRACE_START 0
 #define CONFIG_TRACE 1
-#define CONFIG_ISA "riscv64"
+#define CONFIG_ISA "riscv32"
