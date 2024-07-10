b03723b3b05e93dfa5c414a2b2b284506a2f1328
tracer-ysyx <tracer@ysyx.org>
2024-07-10 21:26:14 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:14 up  1:15,  1 user,  load average: 0.39, 0.84, 0.93

--- npc/src/main/core/decoder.scala
+++ npc/src/main/core/decoder.scala
@@ -79,7 +79,7 @@ object Is_Ebreak extends BoolDecodeField[InsP] {
   def name: String = "Is_Ebreak"
   // override def chiselType = Inst_Type_Enum()
   def genTable(op: InsP)= {
-    if(op.opcode.equals(BitPat("b1110011"))&&op.func3.equals(BitPat("b000"))&&op.func7.equals(BitPat("b0000001")))
+    if(op.opcode===BitPat("b1110011")&&op.func3===BitPat("b000")&&op.func7===BitPat("b0000001"))
       y else n
     // if(op.bitPat==BitPat("b00000000000100000000000001110011"))
       // y else n
