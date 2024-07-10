5ba8ecce97a9bc73dffcca2b3035e26478234d43
tracer-ysyx <tracer@ysyx.org>
2024-07-10 19:20:10 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:10 up 10:47,  1 user,  load average: 1.58, 0.87, 0.41

--- npc/csrc/utils/difftest.c
+++ npc/csrc/utils/difftest.c
@@ -13,7 +13,7 @@ bool difftest_check_regs(){
     if(cpu->pc!=ref_cpu->pc)
     for(int i=0;i<MUXDEF(CONFIG_RVE,16,32);i++){
         if(cpu->gpr[i]!=ref_cpu->gpr[i]){
-            printf("%d",i);
+            printf("----------------%d\n",i);
             return false;
             }
     }
