0c62a38d0f4a87f50b153825b75564784612069c
tracer-ysyx <tracer@ysyx.org>
2024-07-10 11:35:32 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:35:32 up  3:03,  1 user,  load average: 4.42, 3.49, 2.83

--- npc/csrc/execute/exec.cpp
+++ npc/csrc/execute/exec.cpp
@@ -48,7 +48,7 @@ static void trace_and_difftest(paddr_t pc, word_t inst_in)
 #ifdef CONFIG_ITRACE_COND // 在condition为true的时候记录！
     if (CONFIG_ITRACE_COND)
     {
-        printf("BUF++%s\n",buf);
+        // printf("BUF++%s\n",buf);
         log_write("%s\n", buf);
     } // 把缓冲区数据打印出来
 #endif
