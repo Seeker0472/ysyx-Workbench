85f3fbec768c9c335d55b7c7a9b97b271ae3b842
tracer-ysyx <tracer@ysyx.org>
2024-07-10 17:51:19 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:51:19 up  9:18,  1 user,  load average: 1.33, 1.09, 1.03

--- npc/csrc/memory/mem.cpp
+++ npc/csrc/memory/mem.cpp
@@ -50,9 +50,9 @@ void init_img(char *img_file)
     fseek(fp, 0, SEEK_END);
     mem_size = ftell(fp);
     // Log("The image is %s, size = %ld", img_file, size);
-    Log("The image is %s, size = %ld", img_file, size);
+    Log("The image is %s, size = %ld", img_file, mem_size);
     fseek(fp, 0, SEEK_SET);
-    int ret = fread(mem, size, 1, fp);
+    int ret = fread(mem, mem_size, 1, fp);
     // assert(ret == 1);
     fclose(fp);
     // return size;
