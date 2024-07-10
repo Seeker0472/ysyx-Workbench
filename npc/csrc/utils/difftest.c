b275ada49c39628ba5bee9582e1849bf8443c2e5
tracer-ysyx <tracer@ysyx.org>
2024-07-10 19:23:57 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:23:57 up 10:51,  1 user,  load average: 0.74, 0.86, 0.51

--- npc/csrc/utils/difftest.c
+++ npc/csrc/utils/difftest.c
@@ -13,9 +13,12 @@ bool difftest_check_regs(){
     if(cpu->pc!=ref_cpu->pc)
     for(int i=0;i<MUXDEF(CONFIG_RVE,16,32);i++){
         if(cpu->gpr[i]!=ref_cpu->gpr[i]){
+            reg_display(cpu);
+            reg_display(cpu_ref);
+
             printf("----------------%d\n",i);
             return false;
-            }
+        }
     }
     return true;
 }
@@ -32,4 +35,34 @@ void difftest_init_all(){
     ref_cpu=(CPU_state*)malloc(sizeof(CPU_state));
     difftest_regcpy(cpu,DIFFTEST_TO_REF);
     difftest_memcpy(0x80000000,mem,mem_size,DIFFTEST_TO_REF);
+}
+//temp!!
+void reg_display(CPU_state *cpu) {
+  printf("================================================regs================================================\n");
+  printf("%-4s \t%-20s\t%-10s\t","Name","Dec","Hex");  
+  printf(" | ");
+  printf("%-4s \t%-20s\t%-10s\t\n","Name","Dec","Hex");
+#ifdef CONFIG_RVE
+  for(int i=0;i<8;i++){
+    MUXDEF(CONFIG_RV64,printf("%-4s \t%-20ld\t%-10lx\t",regs[i],cpu->gpr[i],cpu->gpr[i]);,printf("%-4s \t%-20d\t%-10x\t",regs[i],cpu->gpr[i],cpu->gpr[i]););
+      
+      printf(" | ");
+    MUXDEF(CONFIG_RV64,printf("%-4s \t%-20ld\t%-10lx\t",regs[i+8],cpu->gpr[i+8],cpu->gpr[i+8]);,printf("%-4s \t%-20d\t%-10x\t",regs[i+8],cpu->gpr[i+8],cpu->gpr[i+8]););
+
+      
+      printf("\n");
+  }
+#elif
+  for(int i=0;i<16;i++){
+    MUXDEF(CONFIG_RV64,printf("%-4s \t%-20ld\t%-10lx\t",regs[i],cpu->gpr[i],cpu->gpr[i]);,printf("%-4s \t%-20d\t%-10x\t",regs[i],cpu->gpr[i],cpu->gpr[i]););
+      
+      printf(" | ");
+    MUXDEF(CONFIG_RV64,printf("%-4s \t%-20ld\t%-10lx\t",regs[i+16],cpu->gpr[i+16],cpu->gpr[i+16]);,printf("%-4s \t%-20d\t%-10x\t",regs[i+16],cpu->gpr[i+16],cpu->gpr[i+16]););
+
+      
+      printf("\n");
+  }
+#endif
+
+  MUXDEF(CONFIG_RV64,printf("%-4s \t%-20ld\t%-10lx\t\n","pc",cpu.pc,cpu.pc);,printf("%-4s \t%-20d\t%-10x\t\n","pc",cpu->pc,cpu->pc);)
 }
