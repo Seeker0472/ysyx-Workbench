557a2a43d5d937d37764c83ad8a2f86df5a46a27
tracer-ysyx <tracer@ysyx.org>
2024-07-11 19:05:07 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:05:07 up  9:53,  1 user,  load average: 0.97, 1.12, 1.00

--- npc/csrc/memory/mem.cpp
+++ npc/csrc/memory/mem.cpp
@@ -3,25 +3,21 @@
 #include <debug.h>
 #include <common.h>
 
-void log(const char * log){
-  Log("%s\n",log);
+void log(const char *log)
+{
+  Log("%s\n", log);
 }
 
-extern "C" int pmem_read(int raddr) {
+extern "C" int pmem_read(int raddr)
+{
   // 总是读取地址为`raddr & ~0x3u`的4字节返回
-  log("mem_read");
-
-}
-extern "C" void pmem_write(int waddr, int wdata, char wmask) {
-  // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
-  // `wmask`中每比特表示`wdata`中1个字节的掩码,
-  // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-  log("mem_write");
+  // log("mem_read");
+  return mem_read(addr);
 }
 
 void record_pread(paddr_t addr, int len);
 void record_pwrite(paddr_t addr, int len, word_t data);
-uint32_t mem[10000000]  = {
+uint32_t mem[10000000] = {
     0x00448493,
     0x00448493,
     0x00100073,
@@ -44,8 +40,26 @@ uint32_t mem[10000000]  = {
     0x00448493,
     0x00448493,
 };
-word_t mem_size=84;
+word_t mem_size = 84;
+extern "C" void pmem_write(int waddr, int wdata, char wmask)
+{
+  // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
+  // `wmask`中每比特表示`wdata`中1个字节的掩码,
+  // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+  // log("mem_write");
+  int aligned_addr = waddr & ~0x3u; // 对齐地址
+  uint32_t current_data = mem[(aligned_addr - 0x80000000) / 4];
+  uint32_t new_data = current_data;
 
+  for (int i = 0; i < 4; ++i)
+  {
+    if (wmask & (1 << i))
+    {
+      ((uint8_t *)&new_data)[i] = ((uint8_t *)&wdata)[i];
+    }
+  }
+  mem[(aligned_addr - 0x80000000) / 4] = new_data;
+}
 uint32_t mem_read(uint32_t pc)
 {
   // //mtrace
@@ -53,7 +67,8 @@ uint32_t mem_read(uint32_t pc)
   // record_pread(pc,4);
   return mem[(pc - 0x80000000) / 4];
 }
-uint32_t warp_pmem_read(uint32_t addr){
+uint32_t warp_pmem_read(uint32_t addr)
+{
   return mem_read(addr);
 }
 
