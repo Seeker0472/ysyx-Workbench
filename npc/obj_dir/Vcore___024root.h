0519b5430d88b1a09a4ef9d94a7d60eb24ef77ce
tracer-ysyx <tracer@ysyx.org>
2024-07-11 12:08:43 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:08:43 up  2:57,  1 user,  load average: 1.64, 1.39, 1.15

---
+++ npc/obj_dir/Vcore___024root.h
@@ -0,0 +1,90 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vcore.h for the primary calling header
+
+#ifndef VERILATED_VCORE___024ROOT_H_
+#define VERILATED_VCORE___024ROOT_H_  // guard
+
+#include "verilated.h"
+class Vcore___024unit;
+
+
+class Vcore__Syms;
+
+class alignas(VL_CACHE_LINE_BYTES) Vcore___024root final : public VerilatedModule {
+  public:
+    // CELLS
+    Vcore___024unit* __PVT____024unit;
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clock,0,0);
+    VL_IN8(reset,0,0);
+    CData/*0:0*/ core__DOT___decoder_io_out_alu_use_Imm_2;
+    CData/*0:0*/ core__DOT___decoder_io_out_pc_jump;
+    CData/*0:0*/ core__DOT___decoder_io_out_reg_write_enable;
+    CData/*7:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T;
+    CData/*7:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_1;
+    CData/*7:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_3;
+    CData/*5:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_5;
+    CData/*6:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_6;
+    CData/*7:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_11;
+    CData/*6:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_14;
+    CData/*7:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_17;
+    CData/*7:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_18;
+    CData/*0:0*/ core__DOT__decoder__DOT____VdfgRegularize_hb9908eda_0_29;
+    CData/*0:0*/ core__DOT__decoder__DOT____VdfgRegularize_hb9908eda_0_30;
+    CData/*0:0*/ core__DOT__reg_0__DOT___GEN;
+    CData/*0:0*/ __VstlFirstIteration;
+    CData/*0:0*/ __VicoFirstIteration;
+    CData/*0:0*/ __Vtrigprevexpr___TOP__clock__0;
+    CData/*0:0*/ __VactContinue;
+    SData/*9:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_13;
+    SData/*15:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_16;
+    SData/*15:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_20;
+    SData/*14:0*/ core__DOT__decoder__DOT___decodedResults_andMatrixOutputs_T_21;
+    VL_OUT(io_pc,31,0);
+    VL_OUT(io_value,31,0);
+    VL_IN(io_addr,31,0);
+    VL_IN(io_instr,31,0);
+    VL_OUT(io_inst_now,31,0);
+    IData/*31:0*/ core__DOT___exu_io_out_n_pc;
+    IData/*31:0*/ core__DOT__decoder__DOT__casez_tmp;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_0;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_1;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_2;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_3;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_4;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_5;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_6;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_7;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_8;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_9;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_10;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_11;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_12;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_13;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_14;
+    IData/*31:0*/ core__DOT__reg_0__DOT__regs_15;
+    IData/*31:0*/ core__DOT__reg_0__DOT__casez_tmp;
+    IData/*31:0*/ core__DOT__ifu__DOT__pc;
+    IData/*31:0*/ __VactIterCount;
+    VlUnpacked<CData/*0:0*/, 3> __Vm_traceActivity;
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<1> __VicoTriggered;
+    VlTriggerVec<1> __VactTriggered;
+    VlTriggerVec<1> __VnbaTriggered;
+
+    // INTERNAL VARIABLES
+    Vcore__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    Vcore___024root(Vcore__Syms* symsp, const char* v__name);
+    ~Vcore___024root();
+    VL_UNCOPYABLE(Vcore___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+};
+
+
+#endif  // guard
