312c7e3e2d308f3c56f2588f9fb6c4edba91ce23
tracer-ysyx <tracer@ysyx.org>
2024-07-12 09:07:52 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:07:52 up 53 min,  1 user,  load average: 0.58, 0.65, 0.69

--- npc/csrc/memory/mem.cpp
+++ npc/csrc/memory/mem.cpp
@@ -70,7 +70,7 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask)
       ((uint8_t *)&new_data)[i] = ((uint8_t *)&wdata)[i-offset];
     }
   }
-  printf("new_data=%x\n",new_data);
+  printf("new_data=%x off=%x\n",new_data,offset);
   mem[(aligned_addr - 0x80000000) / 4] = new_data;
 }
 
