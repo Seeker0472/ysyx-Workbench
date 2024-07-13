503380786992237de5f618b510dac1abd25851bf
tracer-ysyx <tracer@ysyx.org>
2024-07-13 16:58:02 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:02 up  8:16,  1 user,  load average: 1.51, 1.69, 1.63

--- npc/csrc/utils/time.c
+++ npc/csrc/utils/time.c
@@ -35,7 +35,7 @@ static uint64_t get_time_internal() {
 #else
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
-  uint64_t us = now.tv_sec * 100000 + now.tv_nsec / 1000;
+  uint64_t us = now.tv_sec * 10000 + now.tv_nsec / 1000;
 #endif
   return us;
 }
