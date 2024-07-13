32d14fa9c32b183a7180f2510337d1df6a9daa3c
tracer-ysyx <tracer@ysyx.org>
2024-07-13 16:59:01 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:01 up  8:17,  1 user,  load average: 1.92, 1.78, 1.67

--- npc/csrc/utils/time.c
+++ npc/csrc/utils/time.c
@@ -12,12 +12,11 @@
 *
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
-// #define CONFIG_TIMER_GETTIMEOFDAY
+#define CONFIG_TIMER_GETTIMEOFDAY
 
 #include <common.h>
-#include MUXDEF(CONFIG_TIMER_GETTIMEOFDAY, <sys/time.h>, <time.h>)
-// #include <sys/time.h>
-
+// #include MUXDEF(CONFIG_TIMER_GETTIMEOFDAY, <sys/time.h>, <time.h>)
+#include <sys/time.h>
 IFDEF(CONFIG_TIMER_CLOCK_GETTIME,
     static_assert(CLOCKS_PER_SEC == 1000000, "CLOCKS_PER_SEC != 1000000"));
 IFDEF(CONFIG_TIMER_CLOCK_GETTIME,
@@ -35,7 +34,7 @@ static uint64_t get_time_internal() {
 #else
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
-  uint64_t us = now.tv_sec * 10000 + now.tv_nsec / 10000;
+  uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
 #endif
   return us;
 }
