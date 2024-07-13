8731de8fd026e45a984d27269851a64bc48c1365
tracer-ysyx <tracer@ysyx.org>
2024-07-13 16:43:48 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:43:48 up  8:02,  1 user,  load average: 1.37, 1.41, 1.44

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,12 +1,21 @@
 #include <am.h>
 #include "../riscv.h"
 
+uint64_t am_start_time = 0;
+
+uint64_t get_time_am(){
+  uint32_t time_low = inl(RTC_ADDR);
+  uint32_t time_high = inl(RTC_ADDR + 4);
+  return ((uint64_t)time_high << 32) + time_low;
+}
+
 void __am_timer_init() {
+  am_start_time=get_time_am();
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us=((uint64_t)inl(RTC_ADDR+4))<<32;
-  uptime->us+=inl(RTC_ADDR);
+  // uptime->us=((uint64_t)inl(RTC_ADDR+4))<<32;
+  uptime->us+=get_time_am();
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
