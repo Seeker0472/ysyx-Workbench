98f90fb479df063f245a9328321f820a8e877a31
tracer-ysyx <tracer@ysyx.org>
2024-07-13 16:36:28 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:28 up  7:55,  1 user,  load average: 1.77, 1.63, 1.43

--- npc/csrc/utils/time.c
+++ npc/csrc/utils/time.c
@@ -15,7 +15,8 @@
 #define CONFIG_TIMER_GETTIMEOFDAY
 
 #include <common.h>
-#include MUXDEF(CONFIG_TIMER_GETTIMEOFDAY, <sys/time.h>, <time.h>)
+// #include MUXDEF(CONFIG_TIMER_GETTIMEOFDAY, <sys/time.h>, <time.h>)
+#include <sys/time.h>
 
 IFDEF(CONFIG_TIMER_CLOCK_GETTIME,
     static_assert(CLOCKS_PER_SEC == 1000000, "CLOCKS_PER_SEC != 1000000"));
