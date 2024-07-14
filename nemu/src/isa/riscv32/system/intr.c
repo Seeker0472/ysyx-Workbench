c9a13209944aebb9495025f550899698aba63fff
tracer-ysyx <tracer@ysyx.org>
2024-07-14 19:10:14 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:10:14 up 10:18,  1 user,  load average: 0.68, 0.71, 0.83

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -26,7 +26,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 // void set_nemu_state(int state, vaddr_t pc, int halt_ret);
 //   set_nemu_state(NEMU_END, epc, NO);
   // assert(0);
-  Log("Trigged Exception!");
+  Log("Trigged Exception!, No=%x Epc=%x",NO,epc);
 
   return 0;
 }
