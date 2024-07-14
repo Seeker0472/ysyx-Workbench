dcb5ab9edea006c8a64e590f91542dea3a894f68
tracer-ysyx <tracer@ysyx.org>
2024-07-14 20:36:28 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:36:28 up 11:44,  1 user,  load average: 0.95, 0.92, 0.83

--- am-kernels/tests/am-tests/src/tests/intr.c
+++ am-kernels/tests/am-tests/src/tests/intr.c
@@ -1,6 +1,7 @@
 #include <amtest.h>
 
 Context *simple_trap(Event ev, Context *ctx) {
+    printf("c.mepc=%x mstatus=%x mcause=%x",ctx->mepc,ctx->mstatus,ctx->mcause);
   switch(ev.event) {
     case EVENT_IRQ_TIMER:
       putch('t'); break;
