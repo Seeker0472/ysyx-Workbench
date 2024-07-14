9a257afc0dc189a84eb81a9f4564b68d672efb2d
tracer-ysyx <tracer@ysyx.org>
2024-07-14 10:59:25 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:59:25 up  2:07,  1 user,  load average: 0.70, 0.66, 0.73

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -23,7 +23,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   //mepc寄存器 - 存放触发异常的PC
   //mstatus寄存器 - 存放处理器的状态
   //mcause寄存器 - 存放触发异常的原因
-  assert(0);
+void set_nemu_state(int state, vaddr_t pc, int halt_ret);
+  set_nemu_state(NEMU_END, epc, NO);
+  // assert(0);
 
   return 0;
 }
