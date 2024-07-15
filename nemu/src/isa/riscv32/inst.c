77f427d3c68bb6b680fc39a481caf4648064054e
tracer-ysyx <tracer@ysyx.org>
2024-07-15 12:31:15 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:31:15 up  3:44,  1 user,  load average: 0.53, 0.16, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -172,7 +172,7 @@ static int decode_exec(Decode *s) {
 
   // INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall , N, NEMUTRAP(s->pc, R(MUXDEF(CONFIG_RVE,15,17))));
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc=isa_raise_intr(R(MUXDEF(CONFIG_RVE,15,17)),s->dnpc));
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc=cpu.csr[3]);
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc=cpu.csr[3]+4);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));//Invalid--非法指令！！
