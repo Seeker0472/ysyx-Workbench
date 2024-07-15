c5a1bf0fb6f3c3904efd22266f2006a629b3b833
郭明轩 <gmx472@qq.com>
2024-07-15 17:07:56 +0800

    nop

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -39,12 +39,12 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  //TODO:是不是要在栈上放置点什么东西(是应该把内容保存在栈上吗)
+  //DONE:是不是要在栈上放置点什么东西(是应该把内容保存在栈上吗)
   // Context * con= malloc(sizeof(Context));
   // Context *bottom=(Context *)kstack.end;
   // printf("%x",arg);
   Context *top=(Context *)(((void *)kstack.end)-sizeof(Context));
-  top->gpr[10]=(int)arg;
+  top->gpr[10]=(uint32_t)arg;//TODO
   top->mepc=(uintptr_t)entry;
   top->mstatus=0x1800;
   top->mcause=0xb;
