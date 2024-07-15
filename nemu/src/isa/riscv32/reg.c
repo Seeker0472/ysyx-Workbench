c34893712a1538eba9214e6c6a9516c851f2b007
tracer-ysyx <tracer@ysyx.org>
2024-07-15 09:11:28 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:11:28 up 24 min,  1 user,  load average: 0.82, 0.94, 0.63

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,8 +24,19 @@ const char *regs[] = {
 };
 
 const char *csr_regs[] ={
-};
+  "mtvec",
+  "mcause",
+  "mstatus",
+  "mepc",
 
+};
+void print_csr_reg(){
+  printf("================================================csrs================================================\n");
+  printf("%-4s \t%-20s\t%-10s\t","Name","Dec","Hex");  
+  for(int i=0;i<4;i++){
+    MUXDEF(CONFIG_RV64,printf("%-4s \t%-20ld\t%-10lx\n",csr_regs[i],cpu.csr[i],cpu.csr[i]);,printf("%-4s \t%-20d\t%-10x\n",csr_regs[i],cpu.csr[i],cpu.csr[i]););
+  }
+}
 void isa_reg_display() {
   printf("================================================regs================================================\n");
   printf("%-4s \t%-20s\t%-10s\t","Name","Dec","Hex");  
