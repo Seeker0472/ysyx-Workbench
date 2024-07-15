7719b1f3a6c2db4a90ee64fbabf9fbb23397a37c
tracer-ysyx <tracer@ysyx.org>
2024-07-15 13:27:44 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:27:44 up  4:40,  1 user,  load average: 1.79, 1.49, 1.35

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -42,6 +42,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   //TODO:是不是要在栈上放置点什么东西(是应该把内容保存在栈上吗)
   // Context * con= malloc(sizeof(Context));
   // Context *bottom=(Context *)kstack.end;
+  printf("%x",arg);
   Context *top=(Context *)(((void *)kstack.end)-sizeof(Context));
   top->mepc=(uintptr_t)entry;
   top->mstatus=0x1800;
