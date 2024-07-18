e75f1a45a7f1659cfe3959b00ddc7d2ad5f72b88
tracer-ysyx <tracer@ysyx.org>
2024-07-18 10:06:42 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:06:42 up 44 min,  1 user,  load average: 1.02, 1.08, 1.15

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -31,8 +31,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   IFDEF(CONFIG_ETRACE,Log("Trigged Exception!, No=%x Epc=%x",NO,epc););
   
 
-  // cpu.csr[1]=0xb;//mcause
-  // cpu.csr[2]=0x1800;//mstatus
+  cpu.csr[1]=0xb;//mcause
+  cpu.csr[2]=0x1800;//mstatus
   cpu.csr[3]=epc;//mepc
 
 
