398ab09f993c68c23750d1787a2aef16a8ae7547
tracer-ysyx <tracer@ysyx.org>
2024-07-19 19:59:33 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:59:33 up 11:36,  1 user,  load average: 0.69, 0.68, 1.04

--- npc/src/main/core/IFU.scala
+++ npc/src/main/core/IFU.scala
@@ -17,6 +17,14 @@ class IFU extends Module {
   })
   io.out.valid:=true.B
   io.in.ready:=true.B
+  
+  val s_idle :: s_wait_ready :: Nil = Enum(2)
+  val state = RegInit(s_idle)
+  state := MuxLookup(state, s_idle)(List(
+    s_idle       -> Mux(io.out.valid, s_wait_ready, s_idle),
+    s_wait_ready -> Mux(io.out.ready, s_idle, s_wait_ready)
+  ))
+
   val pc = RegInit("h80000000".U(CVAL.DLEN.W))
   io.out.bits.pc := pc
   io.pc     := pc
