035c33dc008553eb27632f5326b61f8da0472bef
tracer-ysyx <tracer@ysyx.org>
2024-07-19 19:45:09 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:45:09 up 11:21,  1 user,  load average: 2.17, 1.87, 1.62

--- npc/src/main/core/ebreak_handler.scala
+++ npc/src/main/core/ebreak_handler.scala
@@ -7,18 +7,18 @@ class ebreak_handler extends BlackBox with HasBlackBoxInline {
   val io = IO(new Bundle {
     val halt = Input(Bool())
   })
-  // setInline(
-  //   "ebreak_handler.v",
-  //   """import "DPI-C" function void call_ebreak();
-  //     |module ebreak_handler(
-  //     |  input halt
-  //     |);
-  //     |always @(*) begin
-  //     |   if (halt) begin
-  //     |      call_ebreak();
-  //     |  end
-  //     | end
-  //     |endmodule
-  //   """.stripMargin
-  // )
+  setInline(
+    "ebreak_handler.v",
+    """import "DPI-C" function void call_ebreak();
+      |module ebreak_handler(
+      |  input halt
+      |);
+      |always @(*) begin
+      |   if (halt) begin
+      |      call_ebreak();
+      |  end
+      | end
+      |endmodule
+    """.stripMargin
+  )
 }
