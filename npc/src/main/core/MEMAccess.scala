e4142787ab0175e151c3154c0ecab3f03ead95cc
tracer-ysyx <tracer@ysyx.org>
2024-07-20 11:22:33 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:22:33 up  2:12,  1 user,  load average: 2.79, 1.14, 0.85

--- npc/src/main/core/MEMAccess.scala
+++ npc/src/main/core/MEMAccess.scala
@@ -39,7 +39,7 @@ class MEMAccess extends Module {
     s_idle-> Mux((io.in.bits.mem_write_enable||io.in.bits.mem_read_enable ) && io.in.valid,s_busy,s_idle)
   ))
   io.in.ready:=state===s_idle
-  io.out.valid:= !(state===s_idle && ((io.in.bits.mem_write_enable||io.in.bits.mem_read_enable ) && io.in.valid))
+  io.out.valid:= (!(state===s_idle && ((io.in.bits.mem_write_enable||io.in.bits.mem_read_enable ) && io.in.valid)))||((state===s_idle && (!(io.in.bits.mem_write_enable||io.in.bits.mem_read_enable ) && io.in.valid)))
 
   //TODO:不应该在这里实例化！！！
   val mem = Module(new MEM())
