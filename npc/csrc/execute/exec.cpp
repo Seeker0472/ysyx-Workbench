e16c0f98d4f6861246207275e99437e82ebbbc26
tracer-ysyx <tracer@ysyx.org>
2024-07-21 21:49:10 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:10 up  3:01,  4 users,  load average: 1.76, 1.62, 1.23

--- npc/csrc/execute/exec.cpp
+++ npc/csrc/execute/exec.cpp
@@ -156,7 +156,7 @@ void update_reg_state()
 
 void single_cycle()
 {
-    dut->io_instr = mem_read(dut->io_pc); // 取指令
+    // dut->io_instr = mem_read(dut->io_pc); // 取指令
     dut->clock = 0;
     dut->eval();
     IFDEF(CONFIG_WAVE_FORM, tfp->dump(sim_time++);) // Dump波形信息
