e1a0ff6a0013755dd94de94cd22d15a576d020b1
tracer-ysyx <tracer@ysyx.org>
2024-07-22 13:46:30 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:46:30 up  4:07,  4 users,  load average: 1.13, 0.59, 0.44

--- npc/src/main/core/AXI_Lite_Arbiter.scala
+++ npc/src/main/core/AXI_Lite_Arbiter.scala
@@ -1,15 +1,24 @@
-package core
+// package core
 
-import chisel3._
-import chisel3.util._
-import Constants_Val._
-import core.IO._
+// import chisel3._
+// import chisel3.util._
+// import Constants_Val._
+// import core.IO._
 
-class AXI_Lite_Arbiter extends Module{
-    val io = IO(new Bundle{
-        val c1=(new AXIIO)
-        val c2=(new AXIIO)
-    })
-    val s_idle ::s_c1_busy::s_c2_busy::s_c1_valid::s_c2_valid ::Nil = Enum(5)
-    val state = RegInit(s_idle)
-}
+// class AXI_Lite_Arbiter extends Module{
+//     val io = IO(new Bundle{
+//         val c1=(new AXIIO)
+//         val c2=(new AXIIO)
+//     })
+//     val s_idle ::s_c1_busy::s_c2_busy::s_c1_valid::s_c2_valid ::Nil = Enum(5)
+//     val state = RegInit(s_idle)
+//     val axi = Module(new AXI)
+
+//     state:=MuxLookup(state,s_idle)(
+//         List(
+//             s_idle ->Mux(io.c2.RA.valid||io.c2.RA.valid,s_c2_busy,Mux(io.c1.RA.valid||io.c1.RA.valid,s_c2_busy,s_idle)),
+//             s_c1_busy ->Mux(axi.io.)
+//         )
+//     )
+    
+// }
