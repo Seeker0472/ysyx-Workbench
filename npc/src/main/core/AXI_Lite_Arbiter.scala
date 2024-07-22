38a656212d1f904959c90c36ecb71de1ae3463a5
tracer-ysyx <tracer@ysyx.org>
2024-07-22 20:09:00 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:09:00 up  4:59, 11 users,  load average: 2.35, 2.72, 2.78

--- npc/src/main/core/AXI_Lite_Arbiter.scala
+++ npc/src/main/core/AXI_Lite_Arbiter.scala
@@ -14,32 +14,31 @@ class AXI_Lite_Arbiter extends Module {
   val state                                   = RegInit(s_idle)
   // val axi                                     = Module(new AXI)
   val xbar=Module(new XBAR)
-  val xb
 
   state := MuxLookup(state, s_idle)(
     List(
       s_idle -> Mux(io.c2.RA.valid, s_c2_busy, Mux(io.c1.RA.valid, s_c1_busy, s_idle)),
-      s_c1_busy -> Mux(axi.io.RD.valid, s_idle, s_c1_busy),
-      s_c2_busy -> Mux(axi.io.RD.valid, s_idle, s_c2_busy)
+      s_c1_busy -> Mux(xbar.io.in.RD.valid, s_idle, s_c1_busy),
+      s_c2_busy -> Mux(xbar.io.in.RD.valid, s_idle, s_c2_busy)
       // s_c1_valid -> Mux(io.c1.RD.ready, s_idle, s_c1_valid),
       // s_c2_valid -> Mux(io.c2.RD.ready, s_idle, s_c2_valid)
     )
   )
   //写通道全部分配给c2
-  axi.io.WA <> io.c2.WA
-  axi.io.WD <> io.c2.WD
-  axi.io.WR <> io.c2.WR
+  xbar.io.in.WA <> io.c2.WA
+  xbar.io.in.WD <> io.c2.WD
+  xbar.io.in.WR <> io.c2.WR
   //读通道看状态
   io.c1.RA.ready      := state === s_idle
   io.c2.RA.ready      := state === s_idle
-  axi.io.RA.valid     := io.c1.RA.valid || io.c2.RA.valid
-  axi.io.RA.bits.addr := Mux(io.c1.RA.valid, io.c1.RA.bits.addr, io.c2.RA.bits.addr)
-  axi.io.RD.ready     := Mux(state === s_c1_busy, io.c1.RD.ready, io.c2.RD.ready)
-  io.c1.RD.bits.data  := axi.io.RD.bits.data
-  io.c2.RD.bits.data  := axi.io.RD.bits.data
-  io.c1.RD.bits.rresp := axi.io.RD.bits.rresp
-  io.c2.RD.bits.rresp := axi.io.RD.bits.rresp
-  io.c1.RD.valid      := Mux(state === s_c1_busy, axi.io.RD.valid, false.B)
-  io.c2.RD.valid      := Mux(state === s_c2_busy, axi.io.RD.valid, false.B)
+  xbar.io.in.RA.valid     := io.c1.RA.valid || io.c2.RA.valid
+  xbar.io.in.RA.bits.addr := Mux(io.c1.RA.valid, io.c1.RA.bits.addr, io.c2.RA.bits.addr)
+  xbar.io.in.RD.ready     := Mux(state === s_c1_busy, io.c1.RD.ready, io.c2.RD.ready)
+  io.c1.RD.bits.data  := xbar.io.in.RD.bits.data
+  io.c2.RD.bits.data  := xbar.io.in.RD.bits.data
+  io.c1.RD.bits.rresp := xbar.io.in.RD.bits.rresp
+  io.c2.RD.bits.rresp := xbar.io.in.RD.bits.rresp
+  io.c1.RD.valid      := Mux(state === s_c1_busy, xbar.io.in.RD.valid, false.B)
+  io.c2.RD.valid      := Mux(state === s_c2_busy, xbar.io.in.RD.valid, false.B)
 
 }
--- npc/src/main/core/XBAR.scala
+++ npc/src/main/core/XBAR.scala
@@ -14,8 +14,11 @@ object MEM_Area extends ChiselEnum {
 class XBAR extends Module {
   val io = IO(new Bundle {
     val in  = (new AXIIO)
-    val out = (new AXIIO)
+    // val out = (new AXIIO)
   })
+  val axi                                     = Module(new AXI)
+  val urat=Module(new URAT)
+
   val Table = TruthTable(
     Map(
       BitPat("h10000???") -> BitPat(MEM_Area.URAT.asUInt),
@@ -23,7 +26,32 @@ class XBAR extends Module {
     ),
     BitPat(MEM_Area.INV.asUInt)
   )
-  val mem_Type = RegInit(MEM_Area.INV.asUInt)
-  val mem_t    = decoder(io.in.WA.bits.addr, Table) //XBar行为
+  val mem_w_reg = RegInit(MEM_Area.INV.asUInt)
+  val mem_r_t    = decoder(io.in.WA.bits.addr, Table) //XBar行为
+  val mem_w_b    = decoder(io.in.RA.bits.addr, Table) //w_XBar行为
+  mem_w_reg:=mem_w_b
   
+
+  //选择设备(out)
+  axi.io.WA.valid:=Mux(mem_w_b===MEM_Area.SRAM.asUInt,io.in.WA.valid,false.B)//如果选择sram，就开
+  urat.io.WA.valid:=Mux(mem_w_b===MEM_Area.URAT.asUInt,io.in.WA.valid,false.B)
+  axi.io.WA.bits.addr:=io.in.WA.bits.addr
+  urat.io.WA.bits.addr:=io.in.WA.bits.addr
+  io.in.WA.ready:=Mux(mem_w_b===MEM_Area.SRAM.asUInt||mem_w_reg===MEM_Area.SRAM.asUInt,io.in.WA.ready,urat.io.WA.ready)//ready
+
+  axi.io.WD.valid:=Mux(mem_w_reg===MEM_Area.SRAM.asUInt,io.in.WD.valid,false.B)//如果选择sram，就开
+  urat.io.WD.valid:=Mux(mem_w_b===MEM_Area.URAT.asUInt,io.in.WD.valid,false.B)
+  axi.io.WD.bits:=io.in.WD.bits
+  urat.io.WD.bits:=io.in.WD.bits
+  io.in.WD.ready:=Mux(mem_w_b===MEM_Area.SRAM.asUInt||mem_w_reg===MEM_Area.SRAM.asUInt,io.in.WD.ready,urat.io.WD.ready)//ready
+
+  axi.io.WR.ready:=Mux(mem_w_reg===MEM_Area.SRAM.asUInt,io.in.WR.ready,false.B)//如果选择sram，就开
+  urat.io.WR.ready:=Mux(mem_w_b===MEM_Area.URAT.asUInt,io.in.WR.ready,false.B)
+  io.in.WR.bits:=Mux(mem_w_reg===MEM_Area.SRAM.asUInt,io.in.WR.bits,urat.io.WR.bits)
+  io.in.WR.valid:=Mux(mem_w_b===MEM_Area.SRAM.asUInt||mem_w_reg===MEM_Area.SRAM.asUInt,io.in.WR.valid,urat.io.WR.valid)//valid
+
+  axi.io.RA<>io.in.RA
+  axi.io.RD<>io.in.RD
+
+
 }
