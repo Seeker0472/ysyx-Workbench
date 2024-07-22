151914a3135fa1b3fa6293525a7e00c1e5caacf9
tracer-ysyx <tracer@ysyx.org>
2024-07-22 10:03:40 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:40 up 24 min,  4 users,  load average: 1.47, 0.98, 0.73

--- npc/src/main/core/AXI.scala
+++ npc/src/main/core/AXI.scala
@@ -36,7 +36,7 @@ class AXI extends Module {
   }
   w_state := MuxLookup(w_state, s_w_idle)(
     List(
-      s_w_idle -> Mux(io.WD.valid, s_w_wait_data, s_w_idle), //等待地址
+      s_w_idle -> Mux(io.WA.valid, s_w_wait_data, s_w_idle), //等待地址
       s_w_wait_data -> Mux(io.WD.valid, s_w_wait_result, s_w_wait_data), //等待数据(在这一周期直接调用DPI-C获取)
       s_w_wait_result -> Mux(true.B, s_w_valid, s_w_wait_data), //访问存储器，其实是为了模拟延迟
       s_w_valid -> Mux(io.WR.ready, s_w_idle, s_w_valid) //返回结果
