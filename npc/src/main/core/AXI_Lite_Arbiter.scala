34a537693fc595e3fbc65e6c1243a4aeadb3826b
tracer-ysyx <tracer@ysyx.org>
2024-07-22 14:21:54 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:54 up  4:43,  5 users,  load average: 4.99, 4.87, 3.89

--- npc/src/main/core/AXI_Lite_Arbiter.scala
+++ npc/src/main/core/AXI_Lite_Arbiter.scala
@@ -1,25 +1,33 @@
-// package core
+package core
 
-// import chisel3._
-// import chisel3.util._
-// import Constants_Val._
-// import core.IO._
+import chisel3._
+import chisel3.util._
+import Constants_Val._
+import core.IO._
 
-// class AXI_Lite_Arbiter extends Module{
-//     val io = IO(new Bundle{
-//         val c1=(new AXIIO)
-//         val c2=(new AXIIO)
-//     })
-//     val s_idle ::s_c1_busy::s_c2_busy::s_c1_valid::s_c2_valid ::Nil = Enum(5)
-//     val state = RegInit(s_idle)
-//     val axi = Module(new AXI)
+class AXI_Lite_Arbiter extends Module{
+    val io = IO(new Bundle{
+        val c1=(new AXIIO)
+        val c2=(new AXIIO)
+    })
+    val s_idle ::s_c1_busy::s_c2_busy::s_c1_valid::s_c2_valid ::Nil = Enum(5)
+    val state = RegInit(s_idle)
+    val axi = Module(new AXI)
 
-//     state:=MuxLookup(state,s_idle)(
-//         List(
-//             s_idle -> Mux(io.c2.RA.valid,s_c2_busy,Mux(io.c1.RA.valid,s_c1_busy,s_idle)),
-//             s_c1_busy->Mux(io.c1.RD.valid,s_c1_valid),
-//             s_c
-//         )
-//     )
-    
-// }
+    state:=MuxLookup(state,s_idle)(
+        List(
+            s_idle -> Mux(io.c2.RA.valid,s_c2_busy,Mux(io.c1.RA.valid,s_c1_busy,s_idle)),
+            s_c1_busy->Mux(axi.io.RD.valid,s_c1_valid,s_c1_busy),
+            s_c2_busy->Mux(axi.io.RD.valid,s_c2_valid,s_c2_busy),
+            s_c1_valid->Mux(io.c1.RD.ready,s_idle,s_c1_valid),
+            s_c2_valid->Mux(io.c2.RD.ready,s_idle,s_c2_valid),
+        )
+    )
+    //写通道全部分配给c2
+    axi.io.WA<>io.c2.WA
+    axi.io.WD<>io.c2.WD
+    axi.io.WR<>io.c2.WR
+    //读通道看状态
+    axi.io.RA<>Mux(state===s_c1_busy||state===s_c1_valid,io.c1.RA,io.c2.RA)
+    axi.io.RD<>Mux(state===s_c1_busy||state===s_c1_valid,io.c1.RD,io.c2.RD)
+}
