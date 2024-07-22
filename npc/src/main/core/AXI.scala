71f8062fbd2d82c12e2e87f1f4b08e3937dc2c15
tracer-ysyx <tracer@ysyx.org>
2024-07-22 09:46:51 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:46:51 up 8 min,  4 users,  load average: 1.38, 1.39, 0.76

--- npc/src/main/core/AXI.scala
+++ npc/src/main/core/AXI.scala
@@ -15,7 +15,7 @@ class AXI extends Module {
   val s_w_idle :: s_w_wait_data :: s_w_wait_result :: s_w_valid :: Nil = Enum(4)
 
   val r_state = RegInit(s_r_idle) //读状态
-  val w_state = RegInit(s_w_idle) //写状态
+  val w_state = RegInit(s_w_wait_data) //写状态
 
   val r_addr = Reg(UInt(CVAL.DLEN.W))
   val r_data = Reg(UInt(CVAL.DLEN.W))
@@ -36,13 +36,16 @@ class AXI extends Module {
   }
   w_state := MuxLookup(w_state, s_w_idle)(
     List(
-      s_w_idle -> Mux(io.WA.valid, s_w_wait_data, s_w_idle), //等待地址
+      // s_w_idle -> Mux(io.WA.valid, s_w_wait_data, s_w_idle), //等待地址
       s_w_wait_data -> Mux(io.WD.valid, s_w_wait_result, s_w_wait_data), //等待数据(在这一周期直接调用DPI-C获取)
       s_w_wait_result -> Mux(true.B, s_w_valid, s_w_wait_data), //访问存储器，其实是为了模拟延迟
-      s_w_valid -> Mux(io.WR.ready, s_w_idle, s_w_valid) //返回结果
+      s_w_valid -> Mux(io.WR.ready, s_w_wait_data, s_w_valid) //返回结果
     )
   )
-  when(io.WA.valid && w_state === s_w_idle) {
+  // when(io.WA.valid && w_state === s_w_idle) {
+  //   w_addr := io.WA.bits.addr
+  // }  
+  when(io.WA.valid && w_state === s_w_wait_data) {
     w_addr := io.WA.bits.addr
   }
   DPI_C_MEM.io.write_addr   := w_addr
