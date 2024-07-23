2558659d74a4cae82c4236d23c9782966100f532
tracer-ysyx <tracer@ysyx.org>
2024-07-23 11:21:52 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:52 up  1:23,  4 users,  load average: 1.75, 1.75, 1.61

--- am-kernels/tests/am-tests/src/tests/rtc.c
+++ am-kernels/tests/am-tests/src/tests/rtc.c
@@ -5,7 +5,7 @@ void rtc_test() {
   int sec = 1;
   while (1) {
     while(io_read(AM_TIMER_UPTIME).us / 1000000 < sec) ;
-    printf("%lx",io_read(AM_TIMER_UPTIME).us);
+    printf("%ld",io_read(AM_TIMER_UPTIME).us);
     rtc = io_read(AM_TIMER_RTC);
     printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     if (sec == 1) {
