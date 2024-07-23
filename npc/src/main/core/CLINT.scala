d749a5a647f5dd5d5a33900eb09956edb5cf3365
tracer-ysyx <tracer@ysyx.org>
2024-07-23 10:50:40 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:40 up 51 min,  4 users,  load average: 0.73, 0.58, 0.67

--- npc/src/main/core/CLINT.scala
+++ npc/src/main/core/CLINT.scala
@@ -5,27 +5,55 @@ import chisel3.util._
 import _root_.core.IO.AXIReadIO
 import Constants_Val._
 
-
 class CLINT extends Module {
-    val io=IO(new AXIReadIO)
+  val io                                                = IO(new AXIReadIO)
   val s_r_idle :: s_r_read_data :: s_r_wait_data :: Nil = Enum(3)
 
   val r_state = RegInit(s_r_idle) //读状态
-  val r_data = Reg(UInt(CVAL.DLEN.W))
+  val r_data  = Reg(UInt(CVAL.DLEN.W))
   r_state := MuxLookup(r_state, s_r_idle)(
     List(
       s_r_idle -> Mux(io.RA.valid, s_r_read_data, s_r_idle), //等待地址
       // s_r_wait_data -> Mux(true.B, s_r_read_data, s_r_wait_data), //访问存储器
-    //   s_r_wait_data -> Mux(true.B, s_r_read_data, s_r_wait_data), //访问存储器
+      //   s_r_wait_data -> Mux(true.B, s_r_read_data, s_r_wait_data), //访问存储器
       s_r_read_data -> Mux(io.RD.ready, s_r_idle, s_r_read_data) //返回数据
     )
   )
-  when(io.RA.valid){
-    r_data:=0.U
-  }
-  io.RD.bits.data:=r_data
-  io.RD.bits.rresp:=true.B
-  io.RD.valid:=r_state===s_r_read_data
-  io.RA.ready:=true.B
+  val raddr = Module(new DPI_C_ReadAddr)
+  raddr.io.enable := io.RA.valid
+  raddr.io.clock  := clock
+  raddr.io.read_addr:=io.RA.bits.addr
+  r_data           := raddr.io.read_data
 
+  io.RD.bits.data  := r_data
+  io.RD.bits.rresp := true.B
+  io.RD.valid      := r_state === s_r_read_data
+  io.RA.ready      := true.B
+
+}
+
+class DPI_C_ReadAddr extends BlackBox with HasBlackBoxInline {
+  val io = IO(new Bundle {
+    val read_data = Output(UInt(CVAL.DLEN.W))
+    val read_addr = Input(UInt(CVAL.DLEN.W))
+    val enable    = Input(Bool())
+    val clock     = Input(Clock())
+  })
+  setInline(
+    "dpi_c_wc.v",
+    """import "DPI-C" function int pmem_read(input int read_addr);
+      |module DPI_C_ReadAddr(
+      |  input [31:0] read_addr,
+      |  output reg [31:0] read_data,
+      |  input enable,
+      |  input clock
+      |);
+      |always @(*) begin
+      |  if (enable) begin // 有读写请求时
+      |    read_data = pmem_read(read_addr);
+      |  end
+      |end
+      |endmodule
+    """.stripMargin
+  )
 }
