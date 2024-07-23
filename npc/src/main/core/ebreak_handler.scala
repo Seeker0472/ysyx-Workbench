34831d873e59a98f91ea3f544c7e77715c942862
tracer-ysyx <tracer@ysyx.org>
2024-07-23 11:33:18 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:33:18 up  1:34,  4 users,  load average: 0.66, 0.80, 1.10

--- npc/src/main/core/ebreak_handler.scala
+++ npc/src/main/core/ebreak_handler.scala
@@ -3,22 +3,22 @@ package core
 import chisel3._
 import chisel3.util._
 
-class ebreak_handler extends BlackBox with HasBlackBoxInline {
-  val io = IO(new Bundle {
-    val halt = Input(Bool())
-  })
-  setInline(
-    "ebreak_handler.v",
-    """import "DPI-C" function void call_ebreak();
-      |module ebreak_handler(
-      |  input halt
-      |);
-      |always @(*) begin
-      |   if (halt) begin
-      |      call_ebreak();
-      |  end
-      | end
-      |endmodule
-    """.stripMargin
-  )
-}
+// class ebreak_handler extends BlackBox with HasBlackBoxInline {
+//   val io = IO(new Bundle {
+//     val halt = Input(Bool())
+//   })
+//   setInline(
+//     "ebreak_handler.v",
+//     """import "DPI-C" function void call_ebreak();
+//       |module ebreak_handler(
+//       |  input halt
+//       |);
+//       |always @(*) begin
+//       |   if (halt) begin
+//       |      call_ebreak();
+//       |  end
+//       | end
+//       |endmodule
+//     """.stripMargin
+//   )
+// }
