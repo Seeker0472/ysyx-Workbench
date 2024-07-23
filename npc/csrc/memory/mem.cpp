ea377d9182f14033c781c19a4e55ab15a57a5747
tracer-ysyx <tracer@ysyx.org>
2024-07-23 11:23:19 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:23:19 up  1:24,  4 users,  load average: 0.88, 1.45, 1.52

--- npc/csrc/memory/mem.cpp
+++ npc/csrc/memory/mem.cpp
@@ -51,11 +51,11 @@ uint32_t mem_read(uint32_t pc)
   return mem[(pc - 0x80000000) / 4];
 }
 extern "C" int get_time(int raddr){
-  if(raddr==0xa0000048){
+  if(raddr==0x10000048){
     time_now = get_time();
     return (uint32_t)time_now;
   }
-  if (raddr == 0xa000004c)
+  if (raddr == 0x1000004c)
   {
     return (uint32_t)(time_now >> 32);
   }
