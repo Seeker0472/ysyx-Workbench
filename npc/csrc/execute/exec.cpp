153adda3a1727afd02163c472cbc1a53c1044b90
tracer-ysyx <tracer@ysyx.org>
2024-07-25 18:14:17 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:14:17 up  1:13,  4 users,  load average: 2.23, 1.52, 1.10

--- npc/csrc/execute/exec.cpp
+++ npc/csrc/execute/exec.cpp
@@ -49,10 +49,10 @@ static bool state_valid()//检测下降沿
 
 static void trace_and_difftest(paddr_t pc, word_t inst_in)
 {
-    pirntf("aasdfasdf\n");
+    printf("aasdfasdf\n");
     if (!state_valid())
         return;
-    pirntf("aasdfasdf\n");
+    printf("aasdfasdf\n");
 
 #ifdef CONFIG_ITRACE
     char buf[200];
