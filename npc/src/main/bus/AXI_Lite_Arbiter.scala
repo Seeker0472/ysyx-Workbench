ed8cc5d2e478e9673b0dfab6de73232ab7d642f0
tracer-ysyx <tracer@ysyx.org>
2024-07-25 09:44:43 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:44:43 up 21 min,  1 user,  load average: 0.70, 0.41, 0.46

--- npc/src/main/bus/AXI_Lite_Arbiter.scala
+++ npc/src/main/bus/AXI_Lite_Arbiter.scala
@@ -30,8 +30,8 @@ class AXI_Lite_Arbiter extends Module {
   xbar.io.in.WD <> io.c2.WD
   xbar.io.in.WR <> io.c2.WR
   //读通道看状态
-  io.c1.RA.ready          := state === s_idle
-  io.c2.RA.ready          := state === s_idle
+  io.c1.RA.ready          := state === s_idle&&io.out.RA.ready
+  io.c2.RA.ready          := state === s_idle&&io.out.RA.ready
   xbar.io.in.RA.valid     := io.c1.RA.valid || io.c2.RA.valid
   xbar.io.in.RA.bits.addr := Mux(io.c1.RA.valid, io.c1.RA.bits.addr, io.c2.RA.bits.addr)
   xbar.io.in.RD.ready     := Mux(state === s_c1_busy, io.c1.RD.ready, io.c2.RD.ready)
