fd497e617e5507843baadab4e289f1bba8165197
tracer-ysyx <tracer@ysyx.org>
2024-07-26 15:23:12 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:23:12 up  5:50,  5 users,  load average: 1.22, 1.00, 0.89

--- am-kernels/tests/mem-test/tests/mem.c
+++ am-kernels/tests/mem-test/tests/mem.c
@@ -2,13 +2,13 @@
 
 void set(uint8_t* test) {
     for (volatile int i = 0; i < 8; i++) {
-        test[i] = (uint8_t)((uint32_t)(&test) & 0xff);
+        test[i] = (uint8_t)((uint32_t)(&(test[i])) & 0xff);
     }
 }
 
 void check_values(uint8_t* test) {
     for (volatile int i = 0; i < 8; i++) {
-        check(test[i] == (uint8_t)((uint32_t)(&test) & 0xff));
+        check(test[i] == (uint8_t)((uint32_t)(&(test[i])) & 0xff));
     }
 }
 
