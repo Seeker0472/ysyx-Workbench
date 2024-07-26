ac49132ee2ef7a10a9857bfe172973a836a8ad8e
tracer-ysyx <tracer@ysyx.org>
2024-07-26 10:56:01 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:56:01 up  1:23,  2 users,  load average: 1.30, 0.73, 0.74

--- npc/src/main/core/core.scala
+++ npc/src/main/core/core.scala
@@ -56,7 +56,7 @@ class ypc extends Module {
   io.master.awaddr            := axi_arbiter.io.out.WA.bits.addr
   io.master.awid              := 0.U
   io.master.awlen             := 0.U
-  io.master.awsize            := "b001".U(3.W)//TODO!!!!!!
+  io.master.awsize            := "b010".U(3.W)//TODO!!!!!!
   io.master.awburst           := 0.U
 
   axi_arbiter.io.out.WD.ready := io.master.wready
@@ -74,7 +74,7 @@ class ypc extends Module {
   io.master.araddr            := axi_arbiter.io.out.RA.bits.addr
   io.master.arid              := 0.U
   io.master.arlen             := 0.U
-  io.master.arsize            := "b001".U(3.W)
+  io.master.arsize            := "b010".U(3.W)
   io.master.arburst           := 0.U
 
   io.master.rready                 := axi_arbiter.io.out.RD.ready
