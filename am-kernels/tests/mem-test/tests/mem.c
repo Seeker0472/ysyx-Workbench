114aea19453d6b82c0ceecd44ef5aa3ffba61fce
tracer-ysyx <tracer@ysyx.org>
2024-07-26 11:17:59 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:17:59 up  1:45,  2 users,  load average: 0.85, 1.04, 0.94

--- am-kernels/tests/mem-test/tests/mem.c
+++ am-kernels/tests/mem-test/tests/mem.c
@@ -1,13 +1,20 @@
 #include "trap.h"
 
-int main() {
-	uint8_t test[8];
-	for(volatile int i=0;i<8;i++){
-		test[i]=(uint8_t)((uint32_t)(&test)&0xff);
-	}
-	for(volatile int i=0;i<8;i++){
-		check(test[i]==(uint8_t)((uint32_t)(&test)&0xff));
-	}
+void set(uint8_t* test) {
+    for (volatile int i = 0; i < 8; i++) {
+        test[i] = (uint8_t)((uint32_t)(&test) & 0xff);
+    }
+}
 
-	return 0;
+void check_values(uint8_t* test) {
+    for (volatile int i = 0; i < 8; i++) {
+        check(test[i] == (uint8_t)((uint32_t)(&test) & 0xff));
+    }
+}
+
+int main() {
+    uint8_t test[8];
+    set(test);
+    check_values(test);
+    return 0;
 }
