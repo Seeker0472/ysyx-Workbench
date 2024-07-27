f9ba16f7fe21701718f9c05718d7422d82c339e6
tracer-ysyx <tracer@ysyx.org>
2024-07-27 19:22:35 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:22:35 up  2:23,  1 user,  load average: 0.96, 1.16, 1.20

--- abstract-machine/am/src/riscv/soc/trm.c
+++ abstract-machine/am/src/riscv/soc/trm.c
@@ -54,8 +54,8 @@ void  bootloader() {
 }
 void init_uart(){
   outb(SERIAL_PORT+3,0x80);
-  outb(SERIAL_PORT,0x11);   //写入Divisor Latch
-  outb(SERIAL_PORT+1,0x60); 
+  outb(SERIAL_PORT,0x60);   //写入Divisor Latch
+  outb(SERIAL_PORT+1,0x00); 
   outb(SERIAL_PORT+3,0x03);
 }
 
