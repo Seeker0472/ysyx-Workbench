4aa0505c04af1a98d8e1bfa42a01f1a1b2c7392d
tracer-ysyx <tracer@ysyx.org>
2024-07-27 19:33:28 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:33:28 up  2:34,  1 user,  load average: 0.91, 0.86, 0.99

--- am-kernels/tests/tetete/hello.c
+++ am-kernels/tests/tetete/hello.c
@@ -2,21 +2,22 @@
 int main(void)
 {
   // printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
-  printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
+  // printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
 
-  // for(volatile int i=0;i<1000;i++);
-  printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
-  // for(volatile int i=0;i<1000;i++);
-
-
-  printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
-  // for(volatile int i=0;i<1000;i++);
-
-  printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
-  // for(volatile int i=0;i<1000;i++);
+  // // for(volatile int i=0;i<1000;i++);
+  // printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
+  // // for(volatile int i=0;i<1000;i++);
 
 
-  printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
+  // printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
+  // // for(volatile int i=0;i<1000;i++);
+
+  // printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
+  // // for(volatile int i=0;i<1000;i++);
+
+
+  // printf("Hello!!!!!!!!!!!!!!!!!!!!!!!!\n");
+  printf("J");
 
 
 
