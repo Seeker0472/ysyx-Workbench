67cef3bd67ebe39251af51d58fe903f9c909b280
tracer-ysyx <tracer@ysyx.org>
2024-07-28 18:40:17 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:40:17 up  9:22, 16 users,  load average: 1.05, 0.63, 0.47

--- am-kernels/tests/other-tests/tests/bitrev.c
+++ am-kernels/tests/other-tests/tests/bitrev.c
@@ -19,7 +19,7 @@ void init_bit_rev(){
 void send_data(){
     outl(SPI_MASTER + 0x18,0x80);//ss reg
     outl(SPI_MASTER + 0x10,0x810);//start
-    outl(SPI_MASTER,0x81);
+    outl(SPI_MASTER,0x71);
     outl(SPI_MASTER + 0x10,0x910);//start
 }
 void wait_data(){
