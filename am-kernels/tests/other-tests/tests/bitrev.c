f9d5d0346909811fd1122c6242f6cd82073e6780
tracer-ysyx <tracer@ysyx.org>
2024-07-28 21:22:00 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:00 up 12:04, 17 users,  load average: 1.72, 1.37, 1.12

--- am-kernels/tests/other-tests/tests/bitrev.c
+++ am-kernels/tests/other-tests/tests/bitrev.c
@@ -26,13 +26,13 @@ __attribute__((optimize("O0"))) void wait_data(){
     uint32_t x=inl(SPI_MASTER + 0x10);
     while (((x%0xfff) == (uint32_t)0x910))
         x=inl(SPI_MASTER + 0x10);
-    // int i = inl(SPI_MASTER);
-    // printf("%x\n",i);    
+    int i = inl(SPI_MASTER);
+    printf("%x\n",i);    
 
     // The Following Copies runs
 
-    int i = inl(SPI_MASTER)>>8;
-    printf("%x\n",i);
+    // int i = inl(SPI_MASTER)>>8;
+    // printf("%x\n",i);
 
     // int i = inl(SPI_MASTER);
     // for(volatile int x=0;x<1;x++);
