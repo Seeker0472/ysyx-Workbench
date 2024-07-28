c297f3417e40012bdf2aed3a4d6bfce9052c48d5
tracer-ysyx <tracer@ysyx.org>
2024-07-28 21:49:17 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:17 up 12:31, 17 users,  load average: 1.70, 1.72, 1.47

--- am-kernels/tests/other-tests/tests/bitrev.c
+++ am-kernels/tests/other-tests/tests/bitrev.c
@@ -37,10 +37,7 @@ void wait_data(){
     // The Following Copy runs(With O0 Optim)
 
     int i = inl(SPI_MASTER)>>8;
-    putch('\n');
-    putch('\n');
-    putch('\n');
-    putch('\n');
+
     printf("%d\n",i);
 
     // Runs But Result is NOT Correct
