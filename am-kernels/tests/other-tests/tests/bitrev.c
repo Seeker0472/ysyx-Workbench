ed21a9318a824ff0a629067ae441a80e16295c5e
郭明轩 <gmx472@qq.com>
2024-07-29 16:34:02 +0800

    通过SPI总线从flash中读出数据

--- am-kernels/tests/other-tests/tests/bitrev.c
+++ am-kernels/tests/other-tests/tests/bitrev.c
@@ -12,42 +12,20 @@ static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)a
 
 
 #define SPI_MASTER 0x10001000L
-// __attribute__((optimize("O0"))) 
 void init_bit_rev(){
-    outl(SPI_MASTER +0x14,0x01);//OKEY
+    outl(SPI_MASTER +0x14,0x01);//divider
 }
-// __attribute__((optimize("O0"))) 
 void send_data(){
     outl(SPI_MASTER + 0x18,0x80);//ss reg
-    outl(SPI_MASTER + 0x10,0x810);//start
+    // outl(SPI_MASTER + 0x10,0x810);//start
     outl(SPI_MASTER,0x11);
     outl(SPI_MASTER + 0x10,0x910);//start
 }
 
-//TODO!!!!!!!!!!!!!!
-
-// __attribute__((optimize("O0"))) 
 void wait_data(){
-    uint32_t x=inl(SPI_MASTER + 0x10);
-    while ((((uint32_t)x) == 0x000000910))
-        x=inl(SPI_MASTER + 0x10);
-    // int i = inl(SPI_MASTER);
-    // printf("%x\n",i);    
-
-    // The Following Copy runs(With O0 Optim)
-
+    while (((inl(SPI_MASTER + 0x10)&0x100) == 0x100));//等待
     int i = inl(SPI_MASTER)>>8;
-
-    // putch('\n');
-    // // putch('\n');
-    // printf("%x\n",i);
     assert(i==0x88);
-
-
-    // int i = inl(SPI_MASTER);
-    // for(volatile int x=0;x<1;x++);
-    // printf("%x\n",i);
-
 }
 int main(){
     init_bit_rev();
