068561f676063e2b81b255d52625e72af6a38b55
tracer-ysyx <tracer@ysyx.org>
2024-07-29 13:44:25 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:44:25 up  5:04,  3 users,  load average: 0.64, 0.81, 0.56

--- am-kernels/tests/other-tests/tests/flash_reset.c
+++ am-kernels/tests/other-tests/tests/flash_reset.c
@@ -4,31 +4,32 @@
 
 
 static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
+static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
 
+
+uint32_t * data = (uint32_t *)0x30000000L;
 
 #define SPI_MASTER 0x10001000L
 void init_bit_rev(){
     outl(SPI_MASTER +0x14,0x01);//divider
 }
-// void send_data(){
-// }
+
 uint32_t flash_read(uint32_t addr){
     outl(SPI_MASTER + 0x18,0x01);//ss reg
     outl(SPI_MASTER + 4,addr);
     outl(SPI_MASTER + 0x10,0x910);//start
-    while ((inl(SPI_MASTER + 0x10)&0x100 == 0x100));
-
-    
+    while (((inl(SPI_MASTER + 0x10)&0x100) == 0x100));//等待
+    uint32_t result = inl(SPI_MASTER);
+    return result;
 }
-// void wait_data(){
-//     uint32_t x=inl(SPI_MASTER + 0x10);
-//         x=inl(SPI_MASTER + 0x10);
-//     int i = inl(SPI_MASTER)>>8;
-//     assert(i==0x88);
-// }
+
+void check_flash(){
+    for(int i=0;i<1000;i++)
+        check(flash_read((uint32_t)(data+i))==i);
+}
+
 int main(){
     init_bit_rev();
-    send_data();
-    wait_data();
+     check_flash();
     return 0;
 }
