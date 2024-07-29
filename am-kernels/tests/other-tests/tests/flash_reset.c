105531b164a1d9cb7613b314dab505212f1cad35
tracer-ysyx <tracer@ysyx.org>
2024-07-29 15:04:54 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:04:54 up  6:24,  3 users,  load average: 1.43, 0.77, 0.60

--- am-kernels/tests/other-tests/tests/flash_reset.c
+++ am-kernels/tests/other-tests/tests/flash_reset.c
@@ -32,7 +32,7 @@ uint32_t flash_read(uint32_t addr){
 
 void check_flash(){
     for(int i=0;i<10;i++)
-        check(flash_read((uint32_t)data)==0);
+        check(flash_read((uint32_t)(data+i))==i);
 }
 
 int main(){
