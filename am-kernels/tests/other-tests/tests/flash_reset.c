bdcd379d432c0487bfcc7d5c5d5ca10166b2036c
tracer-ysyx <tracer@ysyx.org>
2024-07-30 15:50:17 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:17 up  6:28,  3 users,  load average: 2.88, 2.75, 2.86

--- am-kernels/tests/other-tests/tests/flash_reset.c
+++ am-kernels/tests/other-tests/tests/flash_reset.c
@@ -11,11 +11,11 @@ uint32_t * data = (uint32_t *)0x30000000L;
 
 #define SPI_MASTER 0x10001000L
 void init_bit_rev(){
+    outl(SPI_MASTER ,0x01);//test
     outl(SPI_MASTER +0x14,0x11);//divider
 }
 uint32_t flash_read(uint32_t addr){
     // outl(SPI_MASTER + 0x10,0x240);//start'
-    outl(SPI_MASTER ,0x01);//ss reg
     outl(SPI_MASTER + 0x18,0x01);//ss reg
     outl(SPI_MASTER + 4 ,(addr&0x00ffffff)|0x03000000);//send_addr
     outl(SPI_MASTER + 0x10,0x540);//start'
