4a8113459dfc302aba0680159070df44289fc8ac
tracer-ysyx <tracer@ysyx.org>
2024-07-30 15:29:39 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:39 up  6:07,  3 users,  load average: 3.60, 2.92, 2.63

--- am-kernels/tests/other-tests/tests/flash.c
+++ am-kernels/tests/other-tests/tests/flash.c
@@ -3,7 +3,7 @@
 uint32_t * data = (uint32_t *)0x30000000L;
 
 int main(){
-    for(int i=1;i<1000;i++)
+    for(int i=0;i<1000;i++)
         check(data[i]==i);    
     // for(int i=0;i<1;i++)
     //     printf("%d\n",data[i]);
