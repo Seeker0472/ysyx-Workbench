4a9347e6026b17b77cc9550886c06bfdc159729c
tracer-ysyx <tracer@ysyx.org>
2024-07-30 21:11:55 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:11:55 up 54 min, 17 users,  load average: 1.41, 1.39, 1.18

--- npc/csrc/execute/exec.cpp
+++ npc/csrc/execute/exec.cpp
@@ -1,7 +1,7 @@
 #include "VysyxSoCFull.h"
 #include "VysyxSoCFull___024root.h"
 #include <common.h>
-#include <diftest.h>
+#include <.h>
 #include <iostream>
 #include <utils.h>
 #include <verilated.h>
@@ -9,6 +9,32 @@
 
 VerilatedVcdC *tfp; // 用于生成波形的指针
 VysyxSoCFull *dut;
+
+// 使用DPI-C机制实现ebreak
+void call_ebreak() {
+  if (nemu_state.state ==
+      NEMU_END) // Prevent_Display_Twice!!!!!------TODO:Why called Twice????
+    return;
+  Log("Ebreak Called!!");
+  // tfp->
+  uint32_t regs_2_value =
+      dut->rootp
+          ->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__reg_0__DOT__regs_10;
+  uint32_t pc =
+      dut->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__ifu__DOT__pc;
+
+  Log("YDB: %s at pc = " FMT_WORD,
+      ((regs_2_value == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN)
+                          : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
+      pc);
+  // printf("%x\n", regs_2_value);
+
+  tfp->flush(); // not-关闭VCD文件
+  // delete tfp;
+  nemu_state.state = NEMU_END;
+  nemu_state.halt_ret = regs_2_value;
+  // exit(0);
+}
 
 #include <isa.h>
 
