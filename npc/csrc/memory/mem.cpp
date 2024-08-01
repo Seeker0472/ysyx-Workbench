5d2c9adf1d544aae2a563e16df3e27c4189e3b33
tracer-ysyx <tracer@ysyx.org>
2024-08-01 10:41:18 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:18 up 51 min,  2 users,  load average: 0.57, 0.55, 0.47

--- npc/csrc/memory/mem.cpp
+++ npc/csrc/memory/mem.cpp
@@ -77,17 +77,18 @@ void init_img(char *img_file) {
   // init_mrom(img_file);
   // size_t size = 0;
   if (img_file != NULL) {
-    FILE *fp = fopen(img_file, "rb");
-    // Assert(fp, "Can not open '%s'", img_file);
-    fseek(fp, 0, SEEK_END);
-    mem_size = ftell(fp);
-    // Log("The image is %s, size = %ld", img_file, size);
-    Log("The image is %s, size = %d", img_file, mem_size);
-    fseek(fp, 0, SEEK_SET);
-    int ret = fread(mem, mem_size, 1, fp);
-    // assert(ret == 1);
-    fclose(fp);
-    // return size;
+    //原本是把镜像加载进pmem中
+    // FILE *fp = fopen(img_file, "rb");
+    // // Assert(fp, "Can not open '%s'", img_file);
+    // fseek(fp, 0, SEEK_END);
+    // mem_size = ftell(fp);
+    // // Log("The image is %s, size = %ld", img_file, size);
+    // Log("The image is %s, size = %d", img_file, mem_size);
+    // fseek(fp, 0, SEEK_SET);
+    // int ret = fread(mem, mem_size, 1, fp);
+    // // assert(ret == 1);
+    // fclose(fp);
+    // // return size;
   } else {
     Log("No Img file Loaded,Using Default IMG");
   }
