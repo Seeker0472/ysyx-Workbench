aff5b157b07a9d9c6bad2e49527bc8675dfd3f7d
郭明轩 <gmx472@qq.com>
2024-08-01 21:57:47 +0800

    完成了psram

--- npc/csrc/memory/mem_dpic.cpp
+++ npc/csrc/memory/mem_dpic.cpp
@@ -5,17 +5,29 @@
 void record_pread(paddr_t addr, int len);
 void record_pwrite(paddr_t addr, char wmask, word_t data);
 uint32_t mem_read(uint32_t pc);
-extern uint32_t mrom[];
-extern uint32_t flash[];
-extern uint32_t mem[]; 
+extern uint32_t *mrom;
+extern uint32_t *flash;
+extern uint32_t *mem;
+extern uint32_t *psram;
 extern uint64_t time_now;
 // DPI-C Funcs
+//TODO:修mtrace
 extern "C" void flash_read(int32_t addr, int32_t *data) {
   // printf("%x---%x\n",addr,flash[addr/4]);
   *data = flash[addr / 4];
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
   *data = mrom[(addr - 0x20000000) / 4];
+}
+
+extern "C" void psram_read(int32_t addr, int32_t *data) {
+  // printf("addr=%x,data=%x\n", addr, psram[(addr - 0x80000000) / 4]);
+  *data = psram[(addr - 0x80000000) / 4];
+  // *data = 0x12345678;
+}
+extern "C" void psram_write(int32_t addr, int32_t data) {
+  // printf("addr=%x,data=%x\n",addr,data);
+  psram[(addr - 0x80000000) / 4] = data;
 }
 
 extern "C" int get_time(int raddr) {
