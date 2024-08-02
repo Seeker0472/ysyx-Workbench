e803949b1a8cd7e4e541146db9ba7f8760ada2b9
tracer-ysyx <tracer@ysyx.org>
2024-08-02 15:12:08 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:12:08 up  6:05,  8 users,  load average: 1.38, 1.03, 0.88

--- am-kernels/tests/other-tests/tests/psram.c
+++ am-kernels/tests/other-tests/tests/psram.c
@@ -1,25 +1,50 @@
 #include "trap.h"
+// #include <cstdint>
 
 uint8_t *data = (uint8_t *)0x80000000L;
 
-void __attribute__((optimize("O0"))) set() {
-  for (volatile int i = 0; i < 100; i++) {
+void __attribute__((optimize("O0"))) set_u8() {
+  for (volatile int i = 0; i < 4*1024; i++) {
     data[i] = (uint8_t)((uint32_t)(&(data[i])) & 0xff);
     // data[i] = 0x12345678;
     // data[i] = 0x12345678;
   }
 }
 
-void __attribute__((optimize("O0"))) check_values() {
-  for (volatile int i = 0; i < 100; i++) {
-    check(data[i] == (uint8_t)((uint32_t)(&(data[i])) & 0xff));
-    // check(data[i] == i);
+void __attribute__((optimize("O0"))) check_values_u8() {
+  for (volatile int i = 0; i < 2 * 1024; i++) {
+    check((data)[i] == (uint8_t)((uint32_t)(&(data[i])) & 0xff));
   }
-  // check(data[2] == 0x12345678);
+}
+
+void __attribute__((optimize("O0"))) set_u16() {
+  for (volatile int i = 0; i < 4 * 1024; i++) {
+    ((uint16_t *)data)[i] = (uint16_t)((uint32_t)(&(data[i])) & 0xffff);
+  }
+}
+
+void __attribute__((optimize("O0"))) check_values_u16() {
+  for (volatile int i = 0; i < 2 * 1024; i++) {
+    check(((uint16_t *)data)[i] == (uint16_t)((uint32_t)(&(data[i])) & 0xffff));
+  }
+}
+void __attribute__((optimize("O0"))) set_u32() {
+  for (volatile int i = 0; i < 4 * 1024; i++) {
+    ((uint32_t *)data)[i] = (uint32_t)(&(data[i]));
+
+  }
+}
+void __attribute__((optimize("O0"))) check_values_u32() {
+  for (volatile int i = 0; i < 2 * 1024; i++) {
+    check(((uint32_t *)data)[i] == (uint32_t)(&(data[i])));
+  }
 }
 
 int main() {
-  // check_values();
-  set();
-  check_values();
+  set_u8();
+  check_values_u8();
+  set_u16();
+  check_values_u16();
+  set_u32();
+  check_values_u32();
 }
