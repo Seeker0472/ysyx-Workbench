f30fa5114bdbd28ac5b20f5b30c5e0e3a382c492
tracer-ysyx <tracer@ysyx.org>
2024-08-02 15:04:56 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:04:56 up  5:58,  8 users,  load average: 0.86, 0.95, 0.82

--- am-kernels/tests/other-tests/tests/psram.c
+++ am-kernels/tests/other-tests/tests/psram.c
@@ -1,21 +1,21 @@
 #include "trap.h"
 
-uint32_t *data = (uint32_t *)0x80000000L;
+uint8_t *data = (uint8_t *)0x80000000L;
 
 void __attribute__((optimize("O0"))) set() {
-  for (volatile int i = 0; i < 10; i++) {
-    // data[i] = (uint8_t)((uint32_t)(&(data[i])) & 0xff);
-    data[i] = 0x12345678;
+  for (volatile int i = 0; i < 100; i++) {
+    data[i] = (uint8_t)((uint32_t)(&(data[i])) & 0xff);
+    // data[i] = 0x12345678;
     // data[i] = 0x12345678;
   }
 }
 
 void __attribute__((optimize("O0"))) check_values() {
-  for (volatile int i = 0; i < 10; i++) {
-    // check(data[i] == (uint8_t)((uint32_t)(&(data[i])) & 0xff));
+  for (volatile int i = 0; i < 100; i++) {
+    check(data[i] == (uint8_t)((uint32_t)(&(data[i])) & 0xff));
     // check(data[i] == i);
   }
-  check(data[2] == 0x12345678);
+  // check(data[2] == 0x12345678);
 }
 
 int main() {
