00a86108081d300cc7287b45818cf70173c2a241
tracer-ysyx <tracer@ysyx.org>
2024-08-02 10:44:21 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:44:21 up  1:37,  7 users,  load average: 0.66, 0.84, 0.69

--- npc/csrc/memory/mem_dpic.cpp
+++ npc/csrc/memory/mem_dpic.cpp
@@ -23,12 +23,12 @@ extern "C" void mrom_read(int32_t addr, int32_t *data) {
 
 extern "C" void psram_read(int32_t addr, int32_t *data) {
   // printf("addr=%x,data=%x\n", addr, psram[(addr - 0x80000000) / 4]);
-  *data = psram[(addr - 0x80000000) / 4];
+  *data = ( (uint8_t *)psram)[(addr - 0x80000000)];
   // *data = 0x12345678;
 }
 extern "C" void psram_write(int32_t addr, int32_t data) {
   // printf("addr=%x,data=%x\n",addr,data);
-  psram[(addr - 0x80000000) / 4] = data;
+  ((uint8_t *)psram)[(addr - 0x80000000)] = data;
 }
 
 extern "C" int get_time(int raddr) {
