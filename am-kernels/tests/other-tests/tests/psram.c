b7cf339d3edc4680f4938d8f2ac9afe8cbd715cb
tracer-ysyx <tracer@ysyx.org>
2024-08-02 11:21:24 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:24 up  2:14,  7 users,  load average: 1.46, 1.15, 0.79

--- am-kernels/tests/other-tests/tests/psram.c
+++ am-kernels/tests/other-tests/tests/psram.c
@@ -5,7 +5,7 @@ uint8_t *data = (uint8_t *)0x80000000L;
 void __attribute__((optimize("O0"))) set() {
   for (volatile int i = 0; i < 10; i++) {
     // data[i] = (uint8_t)((uint32_t)(&(data[i])) & 0xff);
-    data[i] = i;
+    data[i] = 1;
     // data[i] = 0x12345678;
   }
 }
