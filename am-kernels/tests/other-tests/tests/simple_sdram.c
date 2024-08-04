fd5974cee1685b741c068ed1393d6122411858b6
tracer-ysyx <tracer@ysyx.org>
2024-08-04 21:19:52 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:19:52 up  1:33,  1 user,  load average: 0.73, 0.85, 0.81

---
+++ am-kernels/tests/other-tests/tests/simple_sdram.c
@@ -0,0 +1,17 @@
+#include "trap.h"
+uint8_t *data = (uint8_t *)0xa0000000L;
+
+void __attribute__((optimize("O0"))) set_u8() {
+  data[0] = 0xff;
+  // for (volatile int i = 0; i < 1000; i++)
+  //   ;
+  uint8_t temp = data[0];
+  // for (volatile int i = 0; i < 1000; i++)
+  //   ;
+  data[10] = temp;
+}
+
+int main() {
+  set_u8();
+  return 0;
+}
