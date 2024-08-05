76f301994fc9cf86eae7728c5363e18ef8d44302
tracer-ysyx <tracer@ysyx.org>
2024-08-05 14:31:52 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:31:52 up  5:23,  4 users,  load average: 1.24, 1.46, 1.25

--- am-kernels/tests/other-tests/tests/simple_sdram.c
+++ am-kernels/tests/other-tests/tests/simple_sdram.c
@@ -2,13 +2,13 @@
 uint16_t *data = (uint16_t *)0xa0000000L;
 
 void __attribute__((optimize("O0"))) set_u8() {
-  data[3] = 0xffff;
+  data[0] = 0x1234;
   // for (volatile int i = 0; i < 1000; i++)
   //   ;
-  uint16_t temp = data[3];
+  uint16_t temp = data[0];
   // for (volatile int i = 0; i < 1000; i++)
   //   ;
-  check(temp==0xffff);
+  check(temp==0x1234);
   // data[10] = temp;
 }
 
