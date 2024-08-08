7322046e7ea316f1336bbcddbdbc4f66a1c12039
tracer-ysyx <tracer@ysyx.org>
2024-08-08 14:47:41 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:41 up 30 min,  3 users,  load average: 1.66, 1.15, 0.99

--- npc/csrc/nvb/bind.cpp
+++ npc/csrc/nvb/bind.cpp
@@ -3,4 +3,5 @@
 
 void nvboard_bind_all_pins(VysyxSoCFull* top) {
 	nvboard_bind_pin( &top->externalPins_gpio_out, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
+	nvboard_bind_pin( &top->externalPins_gpio_in, 16, SW15, SW14, SW13, SW12, SW11, SW10, SW9, SW8, SW7, SW6, SW5, SW4, SW3, SW2, SW1, SW0);
 }
