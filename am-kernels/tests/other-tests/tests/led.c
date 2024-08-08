e2c34756e73f58e66b84dcc895fe7879a3b2b570
tracer-ysyx <tracer@ysyx.org>
2024-08-08 14:39:32 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:39:32 up 21 min,  3 users,  load average: 0.92, 1.29, 0.96

--- am-kernels/tests/other-tests/tests/led.c
+++ am-kernels/tests/other-tests/tests/led.c
@@ -1,13 +1,15 @@
 #include "am.h"
 #include "trap.h"
+#include <stdint.h>
 
 static inline void outw(uintptr_t addr, uint16_t data) {
   *(volatile uint16_t *)addr = data;
 }
 
 int main() {
+  uint16_t num=0;
   while (1) {
     // putch(0);
-    outw(0x10002000L,0x2);
+    outw(0x10002000L, num++);
   }
 }
