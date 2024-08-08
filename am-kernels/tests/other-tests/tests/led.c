8a62e0c24a452d08b65c776de1f9d7600d059bd0
tracer-ysyx <tracer@ysyx.org>
2024-08-08 14:55:18 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:18 up 37 min,  3 users,  load average: 1.85, 1.44, 1.23

--- am-kernels/tests/other-tests/tests/led.c
+++ am-kernels/tests/other-tests/tests/led.c
@@ -1,17 +1,24 @@
 #include "am.h"
 #include "trap.h"
 #include <stdint.h>
+#include <stdio.h>
+
+#define GPIO_BASE 0x10002000L
 
 static inline void outw(uintptr_t addr, uint16_t data) {
   *(volatile uint16_t *)addr = data;
 }
-
+static inline uint32_t inl(uintptr_t addr) {
+  return *(volatile uint32_t *)addr;
+}
 int main() {
   uint16_t num=1;
   while (1) {
     // putch(0);
-    outw(0x10002000L, num);
+    outw(GPIO_BASE, num);
     num = (num << 1) | (num >> (15));
-    for(volatile int i=0;i<1000;i++);
+    for (volatile int i = 0; i < 1000; i++)
+      ;
+    printf("%x\n", inl(GPIO_BASE+4));
   }
 }
