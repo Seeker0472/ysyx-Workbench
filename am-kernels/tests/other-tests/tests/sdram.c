7a80aa6fb8cb2cdf62435de2f05e8c743645663a
tracer-ysyx <tracer@ysyx.org>
2024-08-08 10:25:44 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:25:44 up 1 day,  1:08,  3 users,  load average: 0.86, 1.07, 1.14

--- am-kernels/tests/other-tests/tests/sdram.c
+++ am-kernels/tests/other-tests/tests/sdram.c
@@ -3,7 +3,7 @@
 uint8_t *memory = (uint8_t *)0xa0000000;
 
 // #define SIZE 4 * 1024
-#define SIZE 4*1024
+#define SIZE 32*4*1024*1024
 
 void set_u8() {
   for (int i = 0; i < SIZE; i++) {
@@ -42,14 +42,14 @@ void check_values_u32() {
 
 int main() {
   printf("Testing mem_area[%x---%x]\n", memory, memory + SIZE);
-  printf("setting Uint8\n");
-  set_u8();
-  printf("checking Uint8\n");
-  check_values_u8();
-  printf("setting Uint16\n");
-  set_u16();
-  printf("checking Uint16\n");
-  check_values_u16();
+  // printf("setting Uint8\n");
+  // set_u8();
+  // printf("checking Uint8\n");
+  // check_values_u8();
+  // printf("setting Uint16\n");
+  // set_u16();
+  // printf("checking Uint16\n");
+  // check_values_u16();
   printf("setting Uint32\n");
   set_u32();
   printf("checking Uint32\n");
