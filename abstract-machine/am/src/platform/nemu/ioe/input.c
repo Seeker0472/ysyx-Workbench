023089dbb14f5772179481af06873ca4af4da091
tracer-ysyx <tracer@ysyx.org>
2024-08-09 22:48:07 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:48:07 up 42 min,  3 users,  load average: 0.54, 0.30, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 #include<stdio.h>
 
 #define KEYDOWN_MASK 0x8000
@@ -8,5 +9,6 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t code = inw(KBD_ADDR);
   // printf("kbd->keydown=%d,,kbd->keycode=%d\n",(code & KEYDOWN_MASK ? true : false),code & ~KEYDOWN_MASK);
   kbd->keydown =  (code & KEYDOWN_MASK );
-  kbd->keycode = code & ~KEYDOWN_MASK;
+  uint32_t ps2_keycode = code & ~KEYDOWN_MASK; // TODO:翻译
+  uint32_t am_keycode = 
 }
