187edbe796e75afe336efa2799bbab4f095e7ae8
tracer-ysyx <tracer@ysyx.org>
2024-08-09 11:10:47 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:10:47 up  1:03,  3 users,  load average: 1.05, 1.00, 0.96

--- abstract-machine/am/src/riscv/soc/input.c
+++ abstract-machine/am/src/riscv/soc/input.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include "../riscv.h"
+#include "stdio.h"
 
 #define KEYDOWN_MASK 0x8000
 
@@ -7,7 +8,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   bool have_code = (inb(SERIAL_PORT + 5) & 0x1) == 1;
   uint32_t code = have_code ? inb(SERIAL_PORT) : 0xff;
   if (have_code == 1)
-    putch('A');
-  kbd->keydown = (code & KEYDOWN_MASK);
-  kbd->keycode = code & ~KEYDOWN_MASK;
+    printf("%x",code);
+  kbd->keydown = 0;
+  kbd->keycode = AM_KEY_NONE;
 }
