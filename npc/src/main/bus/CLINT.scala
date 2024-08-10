d57541f9e4bd552e3a2d1509918a764a3654d041
tracer-ysyx <tracer@ysyx.org>
2024-08-10 10:48:03 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:03 up  1:34,  2 users,  load average: 1.01, 0.69, 0.46

--- npc/src/main/bus/CLINT.scala
+++ npc/src/main/bus/CLINT.scala
@@ -10,51 +10,83 @@ class CLINT extends Module {
   val s_r_idle :: s_r_read_data :: s_r_wait_data :: Nil = Enum(3)
 
   val r_state = RegInit(s_r_idle) //读状态
-  val r_data  = Reg(UInt(CVAL.DLEN.W))
+  val counter = RegInit(0.U(64.W))
   r_state := MuxLookup(r_state, s_r_idle)(
     List(
       s_r_idle -> Mux(io.RA.valid, s_r_read_data, s_r_idle), //等待地址
       s_r_read_data -> Mux(io.RD.ready, s_r_idle, s_r_read_data) //返回数据
     )
   )
-  val raddr = Module(new DPI_C_ReadAddr)
-  raddr.io.enable := io.RA.valid
-  raddr.io.clock  := clock
-  raddr.io.read_addr:=io.RA.bits.addr
-  r_data           := raddr.io.read_data
+  counter := counter+ 1.U
+  
 
-  io.RD.bits.data  := r_data
+  io.RD.bits.data  := Mux(io.RA.bits.addr(2,2)===1.U,counter(64,32),counter(31,0))
   io.RD.bits.rresp := 0.U
   io.RD.valid      := r_state === s_r_read_data
   io.RA.ready      := true.B
 
 }
 
-class DPI_C_ReadAddr extends BlackBox with HasBlackBoxInline {
-  val io = IO(new Bundle {
-    val read_data = Output(UInt(CVAL.DLEN.W))
-    val read_addr = Input(UInt(CVAL.DLEN.W))
-    val enable    = Input(Bool())
-    val clock     = Input(Clock())
-  })
-  setInline(
-    "dpi_c_ra.v",
-    """import "DPI-C" function int get_time(input int read_addr);
-      |module DPI_C_ReadAddr(
-      |  input [31:0] read_addr,
-      |  output reg [31:0] read_data,
-      |  input enable,
-      |  input clock
-      |);
-      |always @(negedge clock) begin
-      |  if (enable) begin // 有读写请求时
-      |    read_data = get_time(read_addr);
-      |  end
-      |end
-      |endmodule
-    """.stripMargin
-  )
-}
+
+
+
+
+
+
+
+
+
+
+// class CLINT extends Module {
+//   val io                                                = IO(new AXIReadIO)
+//   val s_r_idle :: s_r_read_data :: s_r_wait_data :: Nil = Enum(3)
+
+//   val r_state = RegInit(s_r_idle) //读状态
+//   val r_data  = Reg(UInt(CVAL.DLEN.W))
+//   r_state := MuxLookup(r_state, s_r_idle)(
+//     List(
+//       s_r_idle -> Mux(io.RA.valid, s_r_read_data, s_r_idle), //等待地址
+//       s_r_read_data -> Mux(io.RD.ready, s_r_idle, s_r_read_data) //返回数据
+//     )
+//   )
+//   val raddr = Module(new DPI_C_ReadAddr)
+//   raddr.io.enable := io.RA.valid
+//   raddr.io.clock  := clock
+//   raddr.io.read_addr:=io.RA.bits.addr
+//   r_data           := raddr.io.read_data
+
+//   io.RD.bits.data  := r_data
+//   io.RD.bits.rresp := 0.U
+//   io.RD.valid      := r_state === s_r_read_data
+//   io.RA.ready      := true.B
+
+// }
+
+// class DPI_C_ReadAddr extends BlackBox with HasBlackBoxInline {
+//   val io = IO(new Bundle {
+//     val read_data = Output(UInt(CVAL.DLEN.W))
+//     val read_addr = Input(UInt(CVAL.DLEN.W))
+//     val enable    = Input(Bool())
+//     val clock     = Input(Clock())
+//   })
+//   setInline(
+//     "dpi_c_ra.v",
+//     """import "DPI-C" function int get_time(input int read_addr);
+//       |module DPI_C_ReadAddr(
+//       |  input [31:0] read_addr,
+//       |  output reg [31:0] read_data,
+//       |  input enable,
+//       |  input clock
+//       |);
+//       |always @(negedge clock) begin
+//       |  if (enable) begin // 有读写请求时
+//       |    read_data = get_time(read_addr);
+//       |  end
+//       |end
+//       |endmodule
+//     """.stripMargin
+//   )
+// }
 // class DPI_C_ReadAddr extends Module {
 //   val io = IO(new Bundle {
 //     val read_data = Output(UInt(CVAL.DLEN.W))
