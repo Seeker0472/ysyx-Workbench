44b401f870a8c6e0de287ef59ea941e3e71cd429
tracer-ysyx <tracer@ysyx.org>
2024-08-15 22:00:20 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:00:20 up  1:32,  1 user,  load average: 0.78, 0.42, 0.65

--- npc/src/main/core/LSU.scala
+++ npc/src/main/core/LSU.scala
@@ -35,14 +35,15 @@ class LSU extends Module {
       s_idle -> Mux(
         (io.in.bits.mem_write_enable || io.in.bits.mem_read_enable) && io.in.valid,
         // Mux(io.in.bits.mem_read_enable, s_r_busy, s_w_busy),
-        Mux(io.in.bits.mem_read_enable, Mux(io.axi.RA.ready, s_r_busy, s_r_wait_ready), s_wait),
+        Mux(io.in.bits.mem_read_enable, Mux(io.axi.RA.ready, s_r_busy, s_r_wait_ready), s_w_busy),
         Mux(io.in.valid, s_valid, s_idle)
       ),
       s_r_wait_ready -> Mux(io.axi.RA.ready, s_r_busy, s_r_wait_ready),
       s_r_busy -> Mux(io.axi.RD.valid, s_valid, s_r_busy), //depends on the mem delay
       // s_w_busy -> Mux(io.axi.WR.valid, s_valid, s_w_busy),
-      s_wait -> s_w_busy,
-      s_w_busy -> Mux(io.axi.WD.ready, s_valid, s_w_busy), //不等返回值
+      s_wait -> Mux(io.axi.WR.valid,s_valid,s_wait),
+      // s_w_busy -> Mux(io.axi.WD.ready, s_valid, s_w_busy), //不等返回值
+      s_w_busy -> Mux(io.axi.WD.ready, s_wait, s_w_busy), //不等返回值
       s_valid -> Mux(io.out.ready, s_idle, s_valid)
     )
   )
