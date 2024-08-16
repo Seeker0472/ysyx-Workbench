ca69445ceee61b7bff4de40bfda05578eabbbb91
tracer-ysyx <tracer@ysyx.org>
2024-08-16 12:39:27 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux seeker-PC 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:39:27 up  3:20,  2 users,  load average: 1.07, 1.34, 3.03

--- npc/src/main/core/LSU.scala
+++ npc/src/main/core/LSU.scala
@@ -43,7 +43,7 @@ class LSU extends Module {
       s_r_busy -> Mux(io.axi.RD.valid, s_valid, s_r_busy), //depends on the mem delay
       // s_w_busy -> Mux(io.axi.WR.valid, s_valid, s_w_busy),
       s_wait -> Mux(io.axi.WR.valid,s_valid,s_wait),
-      s_w_busy -> Mux(io.axi.WD.ready, s_wait, s_w_busy), //不等返回值
+      s_w_busy -> Mux(io.axi.WD.ready, s_valid, s_w_busy), //不等返回值
       // s_w_busy -> Mux(io.axi.WD.ready, s_wait, s_w_busy), //不等返回值
       s_valid -> Mux(io.out.ready, s_idle, s_valid)
     )
