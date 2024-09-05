ca2b4021f4873863297c6419f088b4f406ade5af
tracer-ysyx <tracer@ysyx.org>
2024-09-05 19:54:35 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.48 #1-NixOS SMP PREEMPT_DYNAMIC Thu Aug 29 15:33:59 UTC 2024 x86_64 GNU/Linux  19:54:35  up   0:11,  1 user,  load average: 0.60, 0.45, 0.28

--- flake.nix
+++ flake.nix
@@ -7,12 +7,10 @@
   };
 
   outputs = { self, nixpkgs,flake-utils }: 
-   flake-utils.lib.eachDefaultSystem (system:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    projectRoot = pkgs.lib.cleanSource ./.;
-    my-verilator= (pkgs.verilator.overrideAttrs (oldAttrs: rec {
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        my-verilator= (pkgs.verilator.overrideAttrs (oldAttrs: rec {
           version = "5.008"; 
           src = pkgs.fetchFromGitHub {
             owner = "verilator";
@@ -22,10 +20,10 @@
           };
           patches = []; # 禁用所有补丁
         }));
-    llvmPackages = pkgs.llvmPackages_18;
-    fhsEnv = pkgs.buildFHSUserEnv {
-          name = "ysyx-fhs-env";
-          targetPkgs = pkgs: with pkgs; [
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
             # 指定verilator版本为5.008
             my-verilator
             #build-essential
@@ -48,8 +46,6 @@
             pkgs.gperftools
             pkgs.numactl
             # verilator end
-            # llvmPackages.llvm
-            # llvmPackages.libllvm
             pkgs.llvm_18
             pkgs.pkgsCross.riscv64.buildPackages.gcc
             pkgs.pkgsCross.riscv64.buildPackages.binutils
@@ -57,74 +53,18 @@
             # pkgs.ncurses6
             pkgs.ncurses
           ];
-          profile = ''
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-              pkgs.ncurses
-              pkgs.readline
-              pkgs.SDL2
-              pkgs.SDL2_image
-              pkgs.SDL2_ttf
-              pkgs.libz
-            # pkgs.llvm_18.llvm-lib  
-            ]}:$LD_LIBRARY_PATH
-            alias riscv64-linux-gnu-gcc='riscv64-unknown-linux-gnu-gcc'
-            export CFLAGS="-I${pkgs.ncurses.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.llvm_18.dev}/include -I${pkgs.SDL2.dev}/include $CFLAGS"
-            export LDFLAGS="-L${pkgs.ncurses.dev}/lib -L${pkgs.readline.dev}/lib -L${pkgs.llvm_18.dev}/lib -L${pkgs.libz.dev}/lib $LDFLAGS"
-            export CPPFLAGS="-I${pkgs.ncurses.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.llvm_18.dev}/include -I${pkgs.SDL2.dev}/include $CPPFLAGS"
-            export PATH=${llvmPackages.llvm}/bin:$PATH
-          '';
-      runScript = "bash";
-        };
-        in{
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ 
-            fhsEnv
-            #TODO Why??
-            pkgs.llvm_18
+          
+          NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            # 添加需要的库
+            pkgs.ncurses
+            pkgs.readline
             pkgs.SDL2
-            pkgs.mill
-         ];
-          shellHook = ''
-            echo "Welcome to ysyx_workbench development environment!"
-            echo "Run 'ysyx-fhs-env' to enter the FHS-compatible environment."
-          '';
+            pkgs.SDL2_image
+            pkgs.SDL2_ttf
+            pkgs.libz
+          ];
+          NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
         };
       }
     );
 }
-  # in {
-  #   devShells.${system}.default = pkgs.mkShell {
-  #     buildInputs = [
-  #       # 指定verilator版本为5.008
-  #       my-verilator
-  #       #build-essential
-  #       pkgs.man
-  #       #gcc-doc
-  #       pkgs.git
-  #       pkgs.gdb
-  #       pkgs.readline #libreadline-dev
-  #       pkgs.vim
-  #       pkgs.gcc
-  #       pkgs.python3
-  #       pkgs.SDL2
-  #       pkgs.SDL2_image
-  #       pkgs.SDL2_ttf
-  #       # verilator start
-  #       pkgs.perl
-  #       pkgs.flex
-  #       pkgs.bison
-  #       pkgs.ccache
-  #       pkgs.gperftools
-  #       pkgs.numactl
-  #       # verilator end
-  #       pkgs.llvm_18  
-  #       pkgs.pkgsCross.riscv64.buildPackages.gcc
-  #       pkgs.pkgsCross.riscv64.buildPackages.binutils
-  #       # TODO:add more!!!
-  #     ];
-  #     shellHook = ''
-  #     '';
-  #   };
-
-#   };
-# }
