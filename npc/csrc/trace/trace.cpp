c70bf2631e3a8ad9c239989716c64417f2a3be61
tracer-ysyx <tracer@ysyx.org>
2024-09-08 22:42:43 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux nixos 6.6.49 #1-NixOS SMP PREEMPT_DYNAMIC Wed Sep  4 11:28:31 UTC 2024 x86_64 GNU/Linux  22:42:43  up   0:23,  1 user,  load average: 0.96, 0.72, 0.52

--- npc/csrc/trace/trace.cpp
+++ npc/csrc/trace/trace.cpp
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include "../include/debug.h"
+#include "svdpi.h"
 
 #define OK(ITEM) (ITEM?"True":"False")
 
-void trace_decoder(bool mem_R, bool mem_W, bool calc, bool csr);
+void trace_decoder(svBit mem_R, svBit mem_W, svBit calc, svBit csr);
 void trace_exu();
-void trace_ifu(int unsigned addr,bool start_end);
-void trace_lsu(int unsigned addr,bool RW,bool start_end);
+void trace_ifu(int unsigned addr,svBit start_end);
+void trace_lsu(int unsigned addr,svBit RW,svBit start_end);
 
-void trace_decoder(bool mem_R, bool mem_W, bool calc, bool csr){
+void trace_decoder(svBit mem_R, svBit mem_W, svBit calc, svBit csr){
     Log("Decoded_result:   MEMR:%s,MEMW:%s,calc:%s,csr:%s\n",OK(mem_R),OK(mem_W),OK(calc),OK(csr));
 }
 void trace_exu(){
     Log("Trace_EXU\n");
 }
-void trace_ifu(int unsigned addr,bool start_end){
+void trace_ifu(int unsigned addr,svBit start_end){
     Log("IFU:addr = %x,start=%s",addr,OK(start_end));
 }
-void trace_lsu(int unsigned addr,bool RW,bool start_end){
+void trace_lsu(int unsigned addr,svBit RW,svBit start_end){
     Log("LSU:addr = %x,start=%s,Read=%s",addr,OK(start_end),OK(RW));
 }
