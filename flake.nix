d333d077c478e99287812d44aab6ad2e03cf571d
tracer-ysyx <tracer@ysyx.org>
2024-09-10 16:41:46 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux nixos 6.6.49 #1-NixOS SMP PREEMPT_DYNAMIC Wed Sep  4 11:28:31 UTC 2024 x86_64 GNU/Linux  16:41:46  up   4:30,  1 user,  load average: 1.76, 0.80, 0.58

--- flake.nix
+++ flake.nix
@@ -86,6 +86,7 @@
             pkgs.zulu17
             pkgs.yosys
             # libyaml_cpp_0_7
+            pkgs.pkg-config
           ];
 
           NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
