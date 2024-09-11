06d0dd2d48ee47e778b67138d10ac28444e601f3
tracer-ysyx <tracer@ysyx.org>
2024-09-11 19:31:52 +0800

    > sim RTL ysyx_23060325 郭明轩 Linux nixos 6.6.49 #1-NixOS SMP PREEMPT_DYNAMIC Wed Sep  4 11:28:31 UTC 2024 x86_64 GNU/Linux  19:31:52  up   1:20,  1 user,  load average: 0.35, 0.80, 0.95

--- npc/csrc/utils/difftest.c
+++ npc/csrc/utils/difftest.c
@@ -45,8 +45,8 @@ void difftest_init_all(){
     difftest_copy_regs();
     // difftest_memcpy(0x20000000,mrom,4 * 1024 *
     // sizeof(uint8_t),DIFFTEST_TO_REF);
-#ifndef NVB
-    printf("not NVB!!");
+#ifdef NVB
+    printf("NVB!!");
     difftest_memcpy(0x80000000, mem, mem_size, DIFFTEST_TO_REF);
 #else
     difftest_memcpy(0x30000000, flash, mem_size, DIFFTEST_TO_REF);
