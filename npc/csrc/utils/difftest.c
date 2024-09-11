c2f76a01d240823c8902ceea77e220c9607dff69
tracer-ysyx <tracer@ysyx.org>
2024-09-11 20:08:58 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux nixos 6.6.49 #1-NixOS SMP PREEMPT_DYNAMIC Wed Sep  4 11:28:31 UTC 2024 x86_64 GNU/Linux  20:08:58  up   1:57,  1 user,  load average: 0.19, 0.54, 0.80

--- npc/csrc/utils/difftest.c
+++ npc/csrc/utils/difftest.c
@@ -8,7 +8,7 @@
 
 extern CPU_state *cpu;
 CPU_state *ref_cpu;
-extern uint32_t mem[];
+extern uint32_t *mem;
 extern uint32_t* mrom;
 extern uint32_t* flash;
 extern word_t mem_size;
@@ -45,7 +45,8 @@ void difftest_init_all(){
     difftest_copy_regs();
     // difftest_memcpy(0x20000000,mrom,4 * 1024 *
     // sizeof(uint8_t),DIFFTEST_TO_REF);
-#ifdef NVB
+#ifdef NPC
+    printf("first---%x\n",*mem);
     difftest_memcpy(0x80000000, mem, mem_size, DIFFTEST_TO_REF);
 #else
     difftest_memcpy(0x30000000, flash, mem_size, DIFFTEST_TO_REF);
