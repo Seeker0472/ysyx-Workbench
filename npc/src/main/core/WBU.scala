8dcd28a0b3fce405ab86a1304d8d0c5203ddbe17
tracer-ysyx <tracer@ysyx.org>
2024-09-25 10:25:10 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux nixos 6.6.52 #1-NixOS SMP PREEMPT_DYNAMIC Wed Sep 18 17:24:10 UTC 2024 x86_64 GNU/Linux  10:25:10  up   2:27,  1 user,  load average: 1.26, 0.77, 0.76

--- npc/src/main/core/WBU.scala
+++ npc/src/main/core/WBU.scala
@@ -13,40 +13,41 @@ class WBU extends Module {
     val CSR_write  = (new CSRWriteIO)
     val out        = Decoupled(new WBU_O)
   })
-  io.in.ready:=true.B
-  val in_regbits = RegNext(io.in.bits)
-  val in_regvalid = RegNext(io.in.valid)
+  io.in.ready:=io.out.ready
+  // io.out.valid:=true.B
+  io.out.valid:=io.in.valid
 
-  io.out.valid:=in_regvalid
 
-  io.CSR_write.write_enable := in_regbits.csrrw && in_regvalid
+
+  io.CSR_write.write_enable := io.in.bits.csrrw && io.in.valid
 //TODO:其实可以临时抽取？
-  io.CSR_write.addr := in_regbits.imm
+  io.CSR_write.addr := io.in.bits.imm
 
-  io.CSR_write.data := Mux(in_regbits.ecall, in_regbits.pc, in_regbits.csr_alu_res) //ecall的时候保存pc寄存器/正常csr指令保存csr_alu的数据
+  io.CSR_write.data := Mux(io.in.bits.ecall, io.in.bits.pc, io.in.bits.csr_alu_res) //ecall的时候保存pc寄存器/正常csr指令保存csr_alu的数据
 
 //wb
   //如果是store，Reg_Write_Enable应该是False
   // val result   = alu.io.result
-  // val result   = Mux(in_regbits.mem_read_enable, mem_read_result, alu.io.result)
+  // val result   = Mux(io.in.bits.mem_read_enable, mem_read_result, alu.io.result)
   val result =
     Mux(
-      in_regbits.mem_read_enable,
-      in_regbits.mem_read_result,
-      Mux(in_regbits.csrrw, in_regbits.csr_val, in_regbits.alu_result)
+      io.in.bits.mem_read_enable,
+      io.in.bits.mem_read_result,
+      Mux(io.in.bits.csrrw, io.in.bits.csr_val, io.in.bits.alu_result)
     ) //内存读取/csr操作/算数运算结果
-  val pc_plus4 = in_regbits.pc + 4.U
+  val pc_plus4 = io.in.bits.pc + 4.U
 
+  // val next_pc = Mux(io.in.bits.pc_jump || (io.in.bits.is_branch && go_branch), result, pc_plus4)
   val next_pc = Mux(
-    in_regbits.pc_jump || (in_regbits.is_branch && in_regbits.go_branch),
+    io.in.bits.pc_jump || (io.in.bits.is_branch && io.in.bits.go_branch),
     result,
-    Mux(in_regbits.ecall, io.csr_mstvec, pc_plus4)
+    Mux(io.in.bits.ecall, io.csr_mstvec, pc_plus4)
   ) //跳转指令/ecall/正常pc+4
-  io.Rwrite.data         := Mux(in_regbits.pc_jump, pc_plus4, result) //跳转指令保存寄存器
-  io.Rwrite.addr         := in_regbits.reg_w_addr
-  io.Rwrite.write_enable := in_regbits.reg_w_enable && in_regvalid
+  io.Rwrite.data         := Mux(io.in.bits.pc_jump, pc_plus4, result) //跳转指令保存寄存器
+  io.Rwrite.addr         := io.in.bits.reg_w_addr
+  io.Rwrite.write_enable := io.in.bits.reg_w_enable && io.in.valid
   // io.out.n_pc    := next_pc
-  io.out.bits.n_pc := Mux(in_regbits.mret, in_regbits.csr_val, next_pc) //mret恢复pc
+  io.out.bits.n_pc := Mux(io.in.bits.mret, io.in.bits.csr_val, next_pc) //mret恢复pc
   // TODO：这个地方感觉会延迟很高？
 
 }
