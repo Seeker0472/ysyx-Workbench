805a4f8be92e2d2972c3524523af6ad15880da0e
tracer-ysyx <tracer@ysyx.org>
2024-10-09 21:03:54 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux nixos 6.6.53 #1-NixOS SMP PREEMPT_DYNAMIC Mon Sep 30 14:25:15 UTC 2024 x86_64 GNU/Linux  21:03:54  up   4:51,  1 user,  load average: 0.97, 0.82, 0.53

--- npc/src/main/core/ALU.scala
+++ npc/src/main/core/ALU.scala
@@ -11,17 +11,23 @@ class ALU extends Module {
     val in     = Input(new ALU_I)
     val result = Output(UInt(Constants_Val.CVAL.DLEN.W))
   })
+// // 默认情况下，dst 输出为 0
+//   io.dst := 0.U
 
+//   // 当 addi 信号为高时，执行加法运算
+//   when(io.addi) {
+//     io.dst := io.src1 + io.imm
+//   }
   val add_val  = io.in.src1.asSInt + io.in.src2.asSInt
   val sub_val  = io.in.src1.asSInt - io.in.src2.asSInt
   val xor      = io.in.src1 ^ io.in.src2
   val or       = io.in.src1 | io.in.src2
   val and      = io.in.src1 & io.in.src2
-  val sll      = io.in.src1 ^ (io.in.src2)
-  val srl      = io.in.src1 ^ (io.in.src2)
-  val sra      = io.in.src1.asSInt ^ (io.in.src2.asSInt) //TODO:Is That Okey???
-  val slt      = io.in.src1.asSInt ^ io.in.src2.asSInt
-  val sltu     = io.in.src1 ^ io.in.src2
+  val sll      = io.in.src1 << (io.in.src2)(4, 0)
+  val srl      = io.in.src1 >> (io.in.src2)(4, 0)
+  val sra      = io.in.src1.asSInt >> (io.in.src2)(4, 0) //TODO:Is That Okey???
+  val slt      = io.in.src1.asSInt < io.in.src2.asSInt
+  val sltu     = io.in.src1 < io.in.src2
   val pass_imm = io.in.src2
 
   val res = MuxLookup(io.in.alu_op_type, 0.U)(
@@ -37,6 +43,7 @@ class ALU extends Module {
       ALU_Op.slt -> slt.asUInt,
       ALU_Op.sltu -> sltu.asUInt,
       ALU_Op.pass_imm -> pass_imm
+      // ALU_Op.slli.asUInt ->  slli
     )
   )
   io.result := res
