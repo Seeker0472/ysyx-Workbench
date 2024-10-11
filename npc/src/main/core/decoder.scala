ec9c1f8e331bb52044a4a0fbcaa4c6a7b76ef45b
tracer-ysyx <tracer@ysyx.org>
2024-10-11 14:29:24 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux nixos 6.6.53 #1-NixOS SMP PREEMPT_DYNAMIC Mon Sep 30 14:25:15 UTC 2024 x86_64 GNU/Linux  14:29:24  up   1:29,  1 user,  load average: 0.39, 0.93, 0.92

--- npc/src/main/core/decoder.scala
+++ npc/src/main/core/decoder.scala
@@ -13,6 +13,229 @@ object Inst_Type_Enum extends ChiselEnum {
   val R_Type, I_Type, S_Type, B_Type, U_Type, J_Type = Value
 }
 
+//指令的类型
+object InstType extends DecodeField[InsP, Inst_Type_Enum.Type] {
+  def name: String = "InstType"
+  override def chiselType = Inst_Type_Enum()
+  def genTable(op: InsP): BitPat = {
+    val immType = op.Inst_Type
+    BitPat(immType.litValue.U((immType.getWidth).W))
+  }
+}
+//src2是否选择Imm
+object Use_IMM_2 extends BoolDecodeField[InsP] {
+  def name: String = "Use_IMM"
+  def genTable(op: InsP) = {
+    if (
+      op.Inst_Type == Inst_Type_Enum.I_Type || op.Inst_Type == Inst_Type_Enum.S_Type || op.Inst_Type == Inst_Type_Enum.B_Type || op.Inst_Type == Inst_Type_Enum.J_Type || op.name_in
+        .matches("auipc") || op.name_in.matches("lui")
+    )
+      y
+    else n
+  }
+}
+//src1是否选择PC -- J-Type & B-Type
+object Use_PC_1 extends BoolDecodeField[InsP] {
+  def name: String = "Use_PC_1"
+  def genTable(op: InsP) = {
+    if (
+      op.Inst_Type == Inst_Type_Enum.J_Type || op.Inst_Type == Inst_Type_Enum.B_Type || op.name_in.matches("auipc")
+    ) //auipc
+      y
+    else n
+  }
+}
+
+//是否是无条件跳转
+object Is_Jump extends BoolDecodeField[InsP] {
+  def name: String = "Is_Jump"
+  def genTable(op: InsP) = {
+    if (op.Inst_Type == Inst_Type_Enum.J_Type || op.opcode.rawString.matches("1100111"))
+      y
+    else n
+  }
+
+}
+
+//Reg_write_Enable
+object R_Write_Enable extends BoolDecodeField[InsP] {
+  def name: String = "Reg_W_En"
+  def genTable(op: InsP) = {
+    //ecall,ebreak有效因为rd为0
+    if (op.Inst_Type == Inst_Type_Enum.S_Type || op.Inst_Type == Inst_Type_Enum.B_Type) //！！注意这里是if() n else y!!!!!!!!
+      n
+    else y
+  }
+}
+
+//ebreak
+object Is_Ebreak extends BoolDecodeField[InsP] {
+  def name: String = "Is_Ebreak"
+  def genTable(op: InsP) = {
+    // if(op.opcode===BitPat("b1110011")&&op.func3===BitPat("b000")&&op.func7===BitPat("b0000001"))
+    if (op.name_in == "ebreak")
+      y
+    else n
+  }
+}
+
+//mem
+object Read_En extends BoolDecodeField[InsP] {
+  def name: String = "Read_En"
+  def genTable(op: InsP) = {
+    if (op.opcode.rawString.matches("0000011"))
+      y
+    else n
+  }
+}
+
+//fencei
+object Is_fenceI extends BoolDecodeField[InsP] {
+  def name: String = "Is_fencei"
+  def genTable(op: InsP) = {
+    if (op.name_in == "fence.i")
+      y
+    else n
+  }
+}
+
+//读取内存的类型
+// object Mem_LoadType extends DecodeField[InsP, Load_Type.Type] {
+//   def name: String = "Mem_LoadType"
+//   override def chiselType = Load_Type()
+//   def genTable(op: InsP): BitPat = {
+//     val load_type = op.func3.rawString match {
+//       case "000" => Load_Type.lb
+//       case "001" => Load_Type.lh
+//       case "010" => Load_Type.lw
+//       case "100" => Load_Type.lbu
+//       case "101" => Load_Type.lhu
+//       case _     => Load_Type.inv
+//     }
+//     BitPat(load_type.litValue.U((load_type.getWidth).W))
+//   }
+// }
+// object Mem_WriteType extends DecodeField[InsP, Store_Type.Type] {
+//   def name: String = "Mem_WriteType"
+//   override def chiselType = Store_Type()
+//   def genTable(op: InsP) = {
+//     val stype = op.func3.rawString match {
+//       case "000" => Store_Type.sb
+//       case "001" => Store_Type.sh
+//       case "010" => Store_Type.sw
+//       case _     => Store_Type.inv
+//     }
+//     BitPat(stype.litValue.U((stype.getWidth).W))
+//   }
+// }
+// object BranchType extends DecodeField[InsP, Branch_Type.Type] {
+//   def name: String = "Branch_Type"
+//   override def chiselType = Branch_Type()
+//   def genTable(op: InsP) = {
+//     val btype = op.func3.rawString match {
+//       case "000" => Branch_Type.beq
+//       case "001" => Branch_Type.bne
+//       case "100" => Branch_Type.blt
+//       case "101" => Branch_Type.bge
+//       case "110" => Branch_Type.bltu
+//       case "111" => Branch_Type.bgeu
+//       case _     => Branch_Type.inv
+//     }
+//     BitPat(btype.litValue.U((btype.getWidth).W))
+//   }
+// }
+
+//mem
+object Write_En extends BoolDecodeField[InsP] {
+  def name: String = "Write_En"
+  def genTable(op: InsP) = {
+    if (op.opcode.rawString.matches("0100011"))
+      y
+    else n
+  }
+}
+
+//Branch
+object Is_Branch extends BoolDecodeField[InsP] {
+  def name: String = "Is_Branch"
+  def genTable(op: InsP) = {
+    if (op.Inst_Type == Inst_Type_Enum.B_Type)
+      y
+    else n
+  }
+}
+
+//csrs
+//由于这部分指令都需要读取/写入csr存储器，所以就rw放一起了，同时这个信号也作用于Mux来选择写入存储器的数据
+object CSRRW extends BoolDecodeField[InsP] {
+  def name: String = "CSRRW"
+  def genTable(op: InsP) = {
+
+    //NO
+    if (
+      op.name_in.matches("csrrw") || op.name_in.matches("csrrs") || op.name_in
+        .matches("ecall") || op.name_in.matches("mret")
+    )
+      y
+    else n
+  }
+}
+
+object Is_Ecall extends BoolDecodeField[InsP] {
+  def name: String = "Is_Ecall"
+  def genTable(op: InsP) = {
+    if (op.name_in.matches("ecall"))
+      y
+    else n
+  }
+}
+
+object Is_Mret extends BoolDecodeField[InsP] {
+  def name: String = "Is_Mret"
+  def genTable(op: InsP) = {
+    if (op.name_in.matches("mret"))
+      y
+    else n
+  }
+}
+//目前csr指令的alu是独立于其他的alu
+object CSRR_ALU_Type extends DecodeField[InsP, CSRALU_Type.Type] {
+  def name: String = "CSRR_ALU_Type"
+  override def chiselType = CSRALU_Type()
+  def genTable(op: InsP) = {
+    val btype = op.name_in match {
+      case "csrrs" => CSRALU_Type.or
+      case _       => CSRALU_Type.passreg
+    }
+    BitPat(btype.litValue.U((btype.getWidth).W))
+  }
+}
+
+//alu的运算类型
+object ALUOp_Gen extends DecodeField[InsP, ALU_Op.Type] {
+  def name: String = "ALUOp_Gen"
+  override def chiselType = ALU_Op()
+  def genTable(op: InsP): BitPat = {
+    val op_type = op.name_in match {
+      case "add" | "addi" | "jal" | "jalr" | "auipc"               => ALU_Op.add
+      case "lb" | "lh" | "lw" | "lbu" | "lhu" | "sb" | "sh" | "sw" => ALU_Op.add
+      case "beq" | "bne" | "blt" | "bge" | "bltu" | "bgeu"         => ALU_Op.add
+      case "sub"                                                   => ALU_Op.sub
+      case "xor" | "xori"                                          => ALU_Op.xor
+      case "or" | "ori"                                            => ALU_Op.or
+      case "and" | "andi"                                          => ALU_Op.and
+      case "sll" | "slli"                                          => ALU_Op.sll
+      case "srl" | "srli"                                          => ALU_Op.srl
+      case "sra" | "srai"                                          => ALU_Op.sra
+      case "slt" | "slti"                                          => ALU_Op.slt
+      case "sltu" | "sltiu"                                        => ALU_Op.sltu
+      case "lui"                                                   => ALU_Op.pass_imm
+      case _                                                       => ALU_Op.inv
+    }
+    BitPat(op_type.litValue.U((op_type.getWidth).W))
+  }
+}
+
 class Decoder extends Module {
   val io = IO(new Bundle {
     val in     = Flipped(Decoupled(new IFUO))
