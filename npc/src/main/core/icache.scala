81d53632b2d78a6a479790e5665c800028e2764a
tracer-ysyx <tracer@ysyx.org>
2024-10-18 11:50:21 +0800

    > generate verilog ysyx_23060325 郭明轩 Linux nixos 6.6.56 #1-NixOS SMP PREEMPT_DYNAMIC Thu Oct 10 10:50:06 UTC 2024 x86_64 GNU/Linux  11:50:21  up   1:22,  1 user,  load average: 0.17, 0.67, 0.73

--- npc/src/main/core/icache.scala
+++ npc/src/main/core/icache.scala
@@ -91,7 +91,7 @@ class icache extends Module {
   io.axi.RA.bits.id   := 0.U //TODO!!!!!
   io.axi.RA.bits.len  := (block_size / 4 - 1).U
 
-  io.inst_valid := io.addr_valid&&hit//TODO : return the hit data instantly!!!!!!!
+  io.inst_valid := io.addr_valid&&state===s_idle&&hit//TODO : return the hit data instantly!!!!!!!
   // miss,update  cache
   val data_read = io.axi.RD.bits.data
   when(io.axi.RD.valid && state === s_wait_data) {
