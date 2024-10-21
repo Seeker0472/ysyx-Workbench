0fd792e2b35eb89dfa34cae814fc01a3cdb22959
tracer-ysyx <tracer@ysyx.org>
2024-10-21 10:50:42 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.56 #1-NixOS SMP PREEMPT_DYNAMIC Thu Oct 10 10:50:06 UTC 2024 x86_64 GNU/Linux  10:50:42  up   1:08,  1 user,  load average: 0.17, 0.34, 0.55

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -49,7 +49,7 @@ enum {
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1 ); } while(0)
 #define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8 ) << 1); } while(0)
 //^^^用于从指令中抽取出立即数W
-
+//TODO: Set Trace Here-----From Decode can get pc!
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
   int rs1 = BITS(i, 19, 15);
