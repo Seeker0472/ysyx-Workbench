07d010b2062074bccd19e53742cc64f69c61bbee
tracer-ysyx <tracer@ysyx.org>
2024-10-21 15:23:32 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.56 #1-NixOS SMP PREEMPT_DYNAMIC Thu Oct 10 10:50:06 UTC 2024 x86_64 GNU/Linux  15:23:32  up   5:41,  1 user,  load average: 1.85, 1.29, 1.06

--- nemu/src/utils/pctrace.c
+++ nemu/src/utils/pctrace.c
@@ -4,6 +4,7 @@
 #include "common.h"
 FILE *fp;
 void trace_pc(vaddr_t pc, word_t inst, int rs1, int rs2, int rd) {
+  printf("PC: %x, Inst: %x, RS1: %d, RS2: %d, RD: %d\n", pc, inst, rs1, rs2, rd);
   fprintf(fp, "%x\n", pc);
 }
 void init_pc_trace() { fp = fopen("pc_trace", "w"); }
