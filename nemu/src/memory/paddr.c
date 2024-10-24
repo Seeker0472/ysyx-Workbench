46a2a8e8155dbda2d3cc799e867bb8e06cc5ac12
tracer-ysyx <tracer@ysyx.org>
2024-10-24 10:20:58 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.57 #1-NixOS SMP PREEMPT_DYNAMIC Thu Oct 17 13:24:38 UTC 2024 x86_64 GNU/Linux  10:20:58  up   2:36,  1 user,  load average: 0.41, 0.52, 0.56

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -136,7 +136,7 @@ word_t paddr_read(paddr_t addr, int len)
   IFDEF(CONFIG_MTRACE, record_pread(addr, len);)
   if (likely(in_pmem(addr)))
     return pmem_read(addr, len);
-  IFDEF(CONFIG_SOC_DEVICE, return pmem_read(addr, len));
+  // IFDEF(CONFIG_SOC_DEVICE, return pmem_read(addr, len));
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 100;
@@ -150,7 +150,7 @@ void paddr_write(paddr_t addr, int len, word_t data)
     pmem_write(addr, len, data);
     return;
   }
-  IFDEF(CONFIG_SOC_DEVICE, pmem_write(addr, len, data);return;)
+  // IFDEF(CONFIG_SOC_DEVICE, pmem_write(addr, len, data);return;)
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
