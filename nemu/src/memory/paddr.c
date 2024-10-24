0fffc9933fe140f2cefcbac48af8a600e26eb4e5
tracer-ysyx <tracer@ysyx.org>
2024-10-24 10:22:44 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.57 #1-NixOS SMP PREEMPT_DYNAMIC Thu Oct 17 13:24:38 UTC 2024 x86_64 GNU/Linux  10:22:44  up   2:37,  1 user,  load average: 1.95, 0.91, 0.69

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -100,12 +100,15 @@ static void pmem_write(paddr_t addr, int len, word_t data)
   host_write(guest_to_host(addr), len, data);
 }
 
-static void out_of_bound(paddr_t addr)
-{
-  // panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
-  //       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
-    Log("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+static void out_of_bound(paddr_t addr) {
+#ifdef CONFIG_SOC_DEVICE
+  Log("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+    addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+#else
+  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
         addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+#endif
+
 }
 
 void init_mem()
