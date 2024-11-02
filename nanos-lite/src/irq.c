5ac70b00a8ad0cd028cb6fbd387301ce6c76a694
tracer-ysyx <tracer@ysyx.org>
2024-11-02 20:34:30 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.58 #1-NixOS SMP PREEMPT_DYNAMIC Tue Oct 22 13:46:36 UTC 2024 x86_64 GNU/Linux  20:34:30  up   1:30,  1 user,  load average: 0.21, 0.45, 0.44

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,6 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
   case EVENT_YIELD:
     printf("YIELD!");
-    do_syscall(c);
     break;
   case EVENT_SYSCALL:
     // printf("syscall!");
