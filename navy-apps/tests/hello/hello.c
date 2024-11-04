ceb7a93799f057760b29c7ff8e63a96f75f1b604
tracer-ysyx <tracer@ysyx.org>
2024-11-04 16:54:22 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.58 #1-NixOS SMP PREEMPT_DYNAMIC Tue Oct 22 13:46:36 UTC 2024 x86_64 GNU/Linux  16:54:22  up   4:43,  1 user,  load average: 0.95, 0.65, 0.53

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,8 +3,6 @@
 
 int main() {
   // write(1, "Hello World!\n", 13);
-  // write(1, "Hello World!\n", 13);
-  // write(1, "Hello World!\n", 13);
   printf("abcdefg\n");
   int i = 2;
   volatile int j = 0;
