aa1bdb7ced7345abb7d9b676f0f15bb060a28ac9
tracer-ysyx <tracer@ysyx.org>
2024-11-04 21:35:24 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.58 #1-NixOS SMP PREEMPT_DYNAMIC Tue Oct 22 13:46:36 UTC 2024 x86_64 GNU/Linux  21:35:24  up   9:24,  1 user,  load average: 0.68, 0.93, 0.77

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -56,7 +56,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   for (int i = 0; i < hader.e_shnum; i++) {
     if (shdrs[i].sh_type == SHT_SYMTAB) {
       Elf_Shdr sh = shdrs[i];
-      // Elf_Sym* sym=(Elf_Sym*)sh.sh_addr;
+      Elf_Sym *sym = (Elf_Sym *)sh.sh_addr;
+      Log("%x",sym);
       for (int j = 0; j < sh.sh_size / sizeof(Elf_Shdr); j++) {
         // if (strcmp((void *)sym[i].st_name, "_end")==0) {
         //   assert(0);
