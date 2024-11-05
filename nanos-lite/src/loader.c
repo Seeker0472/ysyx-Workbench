d5221205e00b02cdcd39db7242c8a4b8dd983463
tracer-ysyx <tracer@ysyx.org>
2024-11-05 22:03:57 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.58 #1-NixOS SMP PREEMPT_DYNAMIC Tue Oct 22 13:46:36 UTC 2024 x86_64 GNU/Linux  22:03:57  up   8:10,  1 user,  load average: 2.43, 3.86, 2.94

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,25 +34,24 @@
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Log("reading Headers");
   Elf_Ehdr *hader = malloc(sizeof(Elf_Ehdr));
   int fd = fs_open(filename, 0, 0);
-  Log("fd=%d",fd);
   assert(fs_read(fd, hader, sizeof(Elf_Ehdr))==sizeof(Elf_Ehdr));
   assert(hader->e_ident[EI_MAG0] == ELFMAG0 &&
          hader->e_ident[EI_MAG1] == ELFMAG1 &&
          hader->e_ident[EI_MAG2] == ELFMAG2 &&
          hader->e_ident[EI_MAG3] == ELFMAG3); // check magic number of elf
   assert(hader->e_machine == EXPECT_TYPE);    // check isa
-  Log("reading Phdrs");
   Elf_Phdr *phdrs = malloc(sizeof(Elf_Phdr) * hader->e_phnum);
   fs_lseek(fd, hader->e_phoff, SEEK_SET);
   fs_read(fd, phdrs, sizeof(Elf_Phdr) * hader->e_phnum);
-  Log("readed Phdrs");
   for (int i = 0; i < hader->e_phnum; i++) {
     Elf_Phdr ph = phdrs[i];
     if (ph.p_type == PT_LOAD) {
-      ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
+      // read the data
+      //  ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
+      fs_lseek(fd, ph.p_offset, SEEK_SET);
+      fs_read(fd, (void *)ph.p_vaddr, ph.p_filesz);
       for (char *empty = (char *)ph.p_vaddr + ph.p_filesz;
            empty < (char *)ph.p_vaddr + ph.p_memsz; empty++)
         *empty = 0;
