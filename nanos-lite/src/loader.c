1f16ad5e3e6e49e85342ee26339293350c2a9841
seeker <gmx472@qq.com>
2024-11-05 22:13:43 +0800

    fin simple file system

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,8 @@
 #include "debug.h"
+#include "fs.h"
 #include <proc.h>
 #include <elf.h>
+
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -26,32 +28,41 @@
 //TODO!!
 #endif
 // extern uintptr_t end_symbol;
-extern Elf_Ehdr ramdisk_start;
+// extern Elf_Ehdr ramdisk_start;
 
 // 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+// size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
+// loads elf file from fs
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  // TODO();
-  Elf_Ehdr hader = ramdisk_start;
-  Elf_Phdr *phdrs = (Elf_Phdr *)((char *)&ramdisk_start + hader.e_phoff);
-  assert(hader.e_ident[EI_MAG0] == ELFMAG0 &&
-         hader.e_ident[EI_MAG1] == ELFMAG1 &&
-         hader.e_ident[EI_MAG2] == ELFMAG2 &&
-         hader.e_ident[EI_MAG3] == ELFMAG3); // check magic number of elf
-  assert(hader.e_machine == EXPECT_TYPE);    // check isa
-  // printf("%x\n", &hader);
-  // load program
-  for (int i = 0; i < hader.e_phnum; i++) {
+  // read header
+  Elf_Ehdr *hader = malloc(sizeof(Elf_Ehdr));
+  int fd = fs_open(filename, 0, 0);
+  assert(fs_read(fd, hader, sizeof(Elf_Ehdr)) == sizeof(Elf_Ehdr));
+  // check magic number 
+  assert(hader->e_ident[EI_MAG0] == ELFMAG0 &&
+         hader->e_ident[EI_MAG1] == ELFMAG1 &&
+         hader->e_ident[EI_MAG2] == ELFMAG2 &&
+         hader->e_ident[EI_MAG3] == ELFMAG3); // check magic number of elf
+  assert(hader->e_machine == EXPECT_TYPE);    // check isa
+  // read phdrs
+  Elf_Phdr *phdrs = malloc(sizeof(Elf_Phdr) * hader->e_phnum);
+  fs_lseek(fd, hader->e_phoff, SEEK_SET);
+  fs_read(fd, phdrs, sizeof(Elf_Phdr) * hader->e_phnum);
+  // load segments
+  for (int i = 0; i < hader->e_phnum; i++) {
     Elf_Phdr ph = phdrs[i];
     if (ph.p_type == PT_LOAD) {
-      ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
+      // read the data
+      //  ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
+      fs_lseek(fd, ph.p_offset, SEEK_SET);
+      fs_read(fd, (void *)ph.p_vaddr, ph.p_filesz);
       for (char *empty = (char *)ph.p_vaddr + ph.p_filesz;
            empty < (char *)ph.p_vaddr + ph.p_memsz; empty++)
         *empty = 0;
     }
   }
-  // find _end symbol
+  // find _end symbol maybe not the os work
   // Elf_Shdr *shdrs = (Elf_Shdr *)((char *)&ramdisk_start + hader.e_shoff);
   // Elf_Shdr *strtab = NULL;
   // Elf_Shdr *symtab = NULL;
@@ -75,7 +86,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //     Log("find symbol_end:%x", symbols[i].st_value);
   //   }
 	// }
-  return hader.e_entry;
+  return hader->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
