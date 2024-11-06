74d6deef028cff6488cab707e116a465d915ea76
tracer-ysyx <tracer@ysyx.org>
2024-11-06 14:57:15 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.58 #1-NixOS SMP PREEMPT_DYNAMIC Tue Oct 22 13:46:36 UTC 2024 x86_64 GNU/Linux  14:57:15  up   2:45,  1 user,  load average: 0.46, 0.70, 0.59

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -34,6 +34,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+
 size_t sys_stdout(const void *buf, size_t offset, size_t len) {
   for (int i = 0; i < len; i++)
     putch(((const char *)buf)[i]);
@@ -75,7 +76,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 size_t fs_write(int fd, const void *buf, size_t len) {
   if (file_table[fd].write != NULL) {
-        return ((size_t(*)(const void *buf, size_t offset, size_t len))file_table[fd].read)(buf,file_table[fd].open_offset,len);
+        return ((size_t(*)(const void *buf, size_t offset, size_t len))file_table[fd].write)(buf,file_table[fd].open_offset,len);
   }
   if (file_table[fd].open_offset + len < file_table[fd].size) {
     ramdisk_write(buf, file_table[fd].disk_offset +file_table[fd].open_offset, len);
