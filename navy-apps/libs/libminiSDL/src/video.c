dc7cb62960afe0fcad679151be119ef77bca1be0
tracer-ysyx <tracer@ysyx.org>
2024-11-11 21:14:27 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  21:14:27  up   7:21,  1 user,  load average: 1.03, 1.98, 2.42

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -29,8 +29,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   // TODO
-  printf("%d,%d",s->w,s->h);
-  NDL_DrawRect(s->pixels, 0,0, s->w, s->h-4);
+  NDL_DrawRect(s->pixels, 0,0, s->w, s->h-3);
 }
 
 // APIs below are already implemented.
