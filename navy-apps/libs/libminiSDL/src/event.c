3d8e39e5148a153d7ab8ae323b9a0860b70b7111
tracer-ysyx <tracer@ysyx.org>
2024-11-11 22:11:51 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  22:11:51  up   8:19,  1 user,  load average: 1.49, 1.36, 1.56

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -24,7 +24,7 @@ int SDL_WaitEvent(SDL_Event *event) {
       if (strncmp(buffer, "kd", 2))
         event->type = SDL_KEYDOWN;
       for (int i = 0; i < 256; i++) {
-        printf("%s---%s", buffer +3, keyname[i]);
+        printf("%s", buffer +3);
         if (strcmp(buffer + 3, keyname[i])) {
           event->key.keysym.sym=i;
         }
