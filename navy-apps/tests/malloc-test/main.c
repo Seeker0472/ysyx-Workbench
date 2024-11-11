adcee2c748abf3870b55071af5178b1a0cd65068
tracer-ysyx <tracer@ysyx.org>
2024-11-11 11:22:46 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  11:22:46  up   1:34,  1 user,  load average: 1.32, 1.45, 1.53

--- navy-apps/tests/malloc-test/main.c
+++ navy-apps/tests/malloc-test/main.c
@@ -8,7 +8,7 @@ int main() {
   printf("small:%x\n",small);
   uint32_t *med = malloc(sizeof(uint32_t) * 0x100);
   printf("med:%x\n",med);
-  uint32_t *large = malloc(sizeof(uint32_t)*0x1000);
+  uint32_t *large = malloc(0x5000);
   printf("large:%x\n",large);
   printf("PASS!!!\n");
 
