7ab8a0a65408c2ee23dd333a59510ae909840510
seeker <gmx472@qq.com>
2024-11-11 22:40:01 +0800

    sdl_event_key

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,12 +1,11 @@
+#include "fcntl.h"
+#include "sdl-event.h"
+#include "unistd.h"
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
-#define keyname(k) #k,
-
-static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+// #define keyname(k) #k,
 
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
@@ -17,7 +16,31 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  // return 1;
+  char buffer[100];
+  event->key.keysym.sym = SDLK_NONE;
+  event->type = SDL_USEREVENT;
+  while (1) {
+    int fd = open("/dev/events", O_RDONLY);
+    if (read(fd, buffer, 100)) {
+      if (strncmp(buffer, "kd", 2)) {
+        event->type = SDL_KEYDOWN;
+      }
+      if (strncmp(buffer, "ku", 2)) {
+        event->type = SDL_KEYUP;
+      }
+      for (int i = 0; i < 256; i++) {
+        printf("%s----%s----%d\n", buffer + 3, keyname[i], strcmp(buffer + 3, keyname[i]));
+        if (keyname[i] &&strcmp(buffer + 3, keyname[i]) == 0) {
+          event->key.keysym.sym = i;
+          break;
+        }
+      }
+      return 1;
+      // event->key.keysym.sym = SDLK_J;
+    }
+    return 1;
+  }
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
