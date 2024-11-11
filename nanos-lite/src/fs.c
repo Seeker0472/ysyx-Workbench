11afb5e7422f99ebad1e18a4aa113bad29a412f2
tracer-ysyx <tracer@ysyx.org>
2024-11-11 15:48:25 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  15:48:25  up   1:55,  1 user,  load average: 0.58, 1.08, 0.98

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -78,7 +78,7 @@ size_t display_pixel(const void *buf, size_t offset, size_t len) {
   // TODO
   // AM_GPU_FBDRAW_T gpudraw;
   // ioe_write(int reg, void *buf)
-  printf("%d-%d\n",(int)offset,screen_w);
+  printf("%d,%d-%d\n",(int)offset,(int)len,screen_w);
   io_write(AM_GPU_FBDRAW, offset%screen_w, offset/screen_w, (void *)buf, len-200, 1, true);
   file_table[FD_FB].open_offset +=len;
   return len;
