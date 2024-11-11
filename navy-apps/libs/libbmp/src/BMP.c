bc51b257e24a1b383ab7868f9a87e9891d62c553
tracer-ysyx <tracer@ysyx.org>
2024-11-11 10:52:23 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  10:52:23  up   1:04,  1 user,  load average: 1.11, 1.06, 0.95

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -39,12 +39,13 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   int line_off = (w * 3 + 3) & ~0x3;
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
+
+      printf("%x",&pixels);
     int nread = fread(&pixels[w * i], 3, w, fp);
     for (int j = w - 1; j >= 0; j --) {
       uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
       uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
       uint8_t r = *(((uint8_t *)&pixels[w * i]) + 3 * j + 2);
-      printf("%x",&pixels);
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
