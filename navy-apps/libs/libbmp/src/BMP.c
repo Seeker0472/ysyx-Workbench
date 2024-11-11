60eca83858653edd0ef20fdaa084df90475d5046
tracer-ysyx <tracer@ysyx.org>
2024-11-11 11:00:03 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  11:00:03  up   1:11,  1 user,  load average: 0.59, 0.83, 0.89

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -31,16 +31,12 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   if (hdr.compression != 0) return NULL;
   int w = hdr.width;
   int h = hdr.height;
-
-  printf("%d,%d",w,h);
-
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
 
   int line_off = (w * 3 + 3) & ~0x3;
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
-
-    printf("\n%x\n%x\n",&(pixels[0]),&pixels[w * i]);
+    printf("\n%x\n%x\n",&pixels,&pixels[1]);
     int nread = fread(&pixels[w * i], 3, w, fp);
     for (int j = w - 1; j >= 0; j --) {
       uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
@@ -49,7 +45,6 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
-  printf("exiting");
 
   fclose(fp);
   if (width) *width = w;
