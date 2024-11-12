359c575883702586e2933e59acba448ad49370fa
tracer-ysyx <tracer@ysyx.org>
2024-11-12 19:44:31 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  19:44:31  up   1:20,  1 user,  load average: 2.22, 1.55, 1.26

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -24,6 +24,7 @@ SDL_Surface *IMG_Load(const char *filename) {
   lseek(fd, 0, SEEK_SET);
   read(fd, buffer, size);
   close(fd);
+  free(buffer);
   SDL_Surface* surface= STBIMG_LoadFromMemory(buffer,size);
   return surface;
 }
