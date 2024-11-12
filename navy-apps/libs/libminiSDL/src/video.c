09ba121d213b084809e1e6c66251aa36d2aa4926
tracer-ysyx <tracer@ysyx.org>
2024-11-12 14:37:44 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  14:37:44  up   2:34,  1 user,  load average: 1.04, 1.06, 0.68

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -15,9 +15,9 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   int bpp = src->format->BitsPerPixel;
   for (int y = 0; y < src_area.h; y++) {
     // calc the source and destination row pointers
-    write(1,"bs\n",1);
     uint8_t *src_row = (uint8_t *)src->pixels + (src_area.y + y) * src->pitch + src_area.x * bpp;
     uint8_t *dst_row = (uint8_t *)dst->pixels + (dst_area.y + y) * dst->pitch + dst_area.x * bpp;
+    write(1, src_row, 100);
     memcpy(dst_row, src_row, dst_area.w * bpp);
   }
 }
