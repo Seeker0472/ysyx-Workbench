7fe2e523abdf39299da7987f51d744a4d393ed25
tracer-ysyx <tracer@ysyx.org>
2024-11-13 09:30:03 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  09:30:03  up   0:20,  1 user,  load average: 3.63, 1.48, 0.74

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,6 +64,8 @@ size_t fs_read(int fd, void *buf, size_t len) {
   if (file_table[fd].read != NULL) {
     return ((size_t(*)(void *buf, size_t offset, size_t len))file_table[fd].read)(buf,file_table[fd].open_offset,len);
   }
+  if (file_table[fd].open_offset + len > file_table[fd].size)
+    len=file_table[fd].size-file_table[fd].open_offset;
   ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   file_table[fd].open_offset += len;
   return len;
@@ -72,6 +74,8 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   if (file_table[fd].write != NULL) {
         return ((size_t(*)(const void *buf, size_t offset, size_t len))file_table[fd].write)(buf,file_table[fd].open_offset,len);
   }
+  if (file_table[fd].open_offset + len > file_table[fd].size)
+    len=file_table[fd].size-file_table[fd].open_offset; 
   ramdisk_write(buf, file_table[fd].disk_offset +file_table[fd].open_offset, len);
   file_table[fd].open_offset += len;
   return len;
