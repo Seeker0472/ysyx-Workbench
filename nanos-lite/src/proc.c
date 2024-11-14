c27849b8d1389c90435b415ff8d28b9ef456338d
tracer-ysyx <tracer@ysyx.org>
2024-11-14 09:17:41 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  09:17:41  up   1:08,  1 user,  load average: 1.83, 2.56, 1.93

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
 
   Log("Initializing processes...");
   // naive_uload(NULL, "/bin/float");
-  naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL, "/bin/coremark");
   // load program here
 }
 
--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,10 +1,13 @@
 #include <am.h>
+#include <unistd.h>
 
 Area heap;
 
 void putch(char ch) {
-  //TODO!!!!
+  // TODO!!!!
+  write(1,&ch,1);
 }
 
 void halt(int code) {
+  _exit(code);
 }
