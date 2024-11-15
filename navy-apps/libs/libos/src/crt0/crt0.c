8e060fadbadfbca1ea917e6798516367e664c5d5
tracer-ysyx <tracer@ysyx.org>
2024-11-15 21:39:20 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  21:39:20  up   1:27,  1 user,  load average: 0.76, 0.75, 0.59

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,11 +5,11 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  // char *empty[] =  {NULL };
-  // environ = empty;
+  char *empty[] =  {NULL };
+  environ = empty;
   uint32_t argc = *(uint32_t *)args;
   char **argv = (char **)((uint32_t *)args + 1);
   char **envp = (char **)((uint32_t*)args+2+argc);
-  exit(main(argc, argv, envp));
+  exit(main(argc, empty, empty));
   assert(0);
 }
