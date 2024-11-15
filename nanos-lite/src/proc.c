a3b5296b6ab698cf7ef0cef61c9f4700943faf5d
tracer-ysyx <tracer@ysyx.org>
2024-11-15 20:26:04 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  20:26:04  up   0:14,  1 user,  load average: 0.63, 0.75, 0.86

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,14 +24,14 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 void context_kload(PCB *pcb, void *func,void*args);
 void naive_uload(PCB *pcb, const char *filename);
 void init_proc() {
-
+  char *empty[] =  {NULL };
 
   Log("Initializing processes...");
   // naive_uload(NULL, "/bin/float");
   // "/share/games/nes/mario.nes"
   // context_kload(&pcb[0], hello_fun, "aaa");
   // context_kload(&pcb[1], hello_fun, "BBB");
-  context_uload(&pcb[1], "/bin/env",NULL,NULL);//sig-fault!!
+  context_uload(&pcb[1], "/bin/env",empty,empty);//sig-fault!!
   switch_boot_pcb();
 
   // context_uload(&pcb[2], "/bin/pal");
