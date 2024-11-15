ea44b83e80b9ed70fd84d5847f024594662ed5b9
tracer-ysyx <tracer@ysyx.org>
2024-11-15 21:38:11 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.59 #1-NixOS SMP PREEMPT_DYNAMIC Fri Nov  1 00:58:34 UTC 2024 x86_64 GNU/Linux  21:38:11  up   1:26,  1 user,  load average: 1.15, 0.82, 0.60

--- navy-apps/tests/env-test/env.c
+++ navy-apps/tests/env-test/env.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]) {
-    printf("%x,%x\n",argv,envp);
+    printf("%x,%x\n",*argv,*envp);
     printf("argv:\n");
     for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, argv[i]);
