42fad79d2b3fe4cfd1718770adc7012c643b93da
tracer-ysyx <tracer@ysyx.org>
2024-11-21 14:28:40 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.61 #1-NixOS SMP PREEMPT_DYNAMIC Thu Nov 14 12:19:41 UTC 2024 x86_64 GNU/Linux  14:28:40  up   5:19,  1 user,  load average: 1.66, 1.50, 1.39

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -48,8 +48,8 @@ int mm_brk(uintptr_t brk) {
     uintptr_t prevbrk = current->max_brk+PAGE_SIZE;
     while ((prevbrk & PAGE_NUMBER_MASK) <= (brk & PAGE_NUMBER_MASK)) {
       // printf("BRK_NP");
-      // void *page = new_page(1);
-      // map(&current->as, (void *)(prevbrk & PAGE_NUMBER_MASK), page, 0b111);
+      void *page = new_page(1);
+      map(&current->as, (void *)(prevbrk & PAGE_NUMBER_MASK), page, 0b111);
       prevbrk+=PAGE_SIZE;
     }
     current->max_brk = brk;
