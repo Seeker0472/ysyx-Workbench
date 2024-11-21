f5f60f7cbba83a7d727c4ab73f04a37f1cf080e3
tracer-ysyx <tracer@ysyx.org>
2024-11-21 10:46:43 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.61 #1-NixOS SMP PREEMPT_DYNAMIC Thu Nov 14 12:19:41 UTC 2024 x86_64 GNU/Linux  10:46:43  up   1:37,  1 user,  load average: 0.68, 0.85, 0.71

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -150,9 +150,9 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   // uint8_t *stack = pcb->stack;
   // init an Context struct on top of stack
   //the cp pointer stores at the bottom of stack
-  pcb->cp = ucontext(&(AddrSpace){.area = {}, .pgsize = 0, .ptr = 0},
-                     (Area){.start = stack, .end = stack + 8 * PGSIZE},
-                     (void *)entry);
+  pcb->cp =
+      ucontext(&pcb->as, (Area){.start = stack, .end = stack + 8 * PGSIZE},
+               (void *)entry);
   pcb->cp->pdir=pcb->as.ptr;
   pcb->active=true;
   // Log("NEW_PAGE:%x-%x-%x\n", stack,pcb->cp,pcb->cp->mepc);
