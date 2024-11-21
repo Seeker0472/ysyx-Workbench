b30d2eff45e14cc9808602a18c1b82168fcae265
tracer-ysyx <tracer@ysyx.org>
2024-11-21 16:39:31 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.61 #1-NixOS SMP PREEMPT_DYNAMIC Thu Nov 14 12:19:41 UTC 2024 x86_64 GNU/Linux  16:39:31  up   7:30,  1 user,  load average: 1.45, 0.98, 1.03

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -102,7 +102,7 @@ int NDL_Init(uint32_t flags) {
     dispinfodev = 5;
     read(dispinfodev, buffer, 0);
     sscanf(buffer,"WIDTH:%d\nHEIGHT:%d" , &screen_w_h,&screen_h_h);
-    // printf("WIDTH : %d\nHEIGHT:%d\n" , screen_w_h,screen_h_h);
+    printf("WIDTH : %d\nHEIGHT:%d\n" , screen_w_h,screen_h_h);
     // }
   return 0;
 }
