552304e3515bf2c7fc33120f635b6d39fb5b5444
tracer-ysyx <tracer@ysyx.org>
2024-11-23 22:43:13 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.61 #1-NixOS SMP PREEMPT_DYNAMIC Thu Nov 14 12:19:41 UTC 2024 x86_64 GNU/Linux  22:43:13  up 1 day  3:12,  1 user,  load average: 0.52, 0.70, 0.81

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc() {
   // context_kload(&pcb[1], hello_fun, "BBB");
   char *argv[] = {"/bin/pal", NULL};
   char *envp[] = {NULL};
-  context_uload(&pcb[1], "/bin/pal", argv, envp);
+  context_uload(&pcb[1], "/bin/hello", argv, envp);
   // context_uload(&pcb[1], "/bin/pal", argv, envp);
   switch_boot_pcb();
 
@@ -42,7 +42,7 @@ void init_proc() {
 void execue_menu() {
   char *argv[] = {"/bin/menu", NULL};
   char *envp[] = {NULL};
-  context_uload(&pcb[0], "/bin/hello", argv, envp);
+  context_uload(&pcb[0], "/bin/menu", argv, envp);
   switch_boot_pcb();
   yield();
 }
