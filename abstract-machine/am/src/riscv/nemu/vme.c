992a8f0353d0a5bf3c382e70e4104a3851d34067
tracer-ysyx <tracer@ysyx.org>
2024-11-23 22:49:45 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.61 #1-NixOS SMP PREEMPT_DYNAMIC Thu Nov 14 12:19:41 UTC 2024 x86_64 GNU/Linux  22:49:45  up 1 day  3:18,  1 user,  load average: 0.88, 0.92, 0.90

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -1,3 +1,4 @@
+// #include "SDL.h"
 #include "stdio.h"
 #include <am.h>
 #include <nemu.h>
@@ -108,15 +109,18 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 // kstack是内核栈,用于分配上下文结构,
 // entry则是用户进程的入口.
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
-  // void *ustack=pgalloc_usr(PGSIZE*8);
-  void *ustack=kstack.end;
+  void *ustack=pgalloc_usr(PGSIZE*8);
+  // void *ustack=kstack.end;
   Context *top = (Context *)(((void *)kstack.end) - sizeof(Context));
   top->GPRx=(uintptr_t)ustack;//pass the stack addr,seems OKEY for riscv--ARCH-spec
   //map stack
-  // for (int i = 0; i < 8; i++) {
-  //   map(as,(void*)as->area.end-(8-i)*PGSIZE,ustack+PGSIZE*i,0b111);
-  //   printf("MAP:%x,%x\n",(void*)as->area.end-(8-i)*PGSIZE,ustack+PGSIZE*i);
-  // }
+  for (int i = 0; i < 8; i++) {
+    map(as,(void*)as->area.end-(8-i)*PGSIZE,ustack+PGSIZE*i,0b111);
+    printf("MAP:%x,%x\n",(void*)as->area.end-(8-i)*PGSIZE,ustack+PGSIZE*i);
+  }
+  for (uint32_t *target = (uint32_t*)ustack; target != ustack + PGSIZE * 8; target++) {
+    *target=0;
+  }
   top->mepc = (uintptr_t)entry;
   top->mstatus = 0x0;//set to user mode
   top->mcause = 0xb; // 0xb is external interrupt
