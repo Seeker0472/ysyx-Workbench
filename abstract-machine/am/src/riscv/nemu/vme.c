a26e482941e34fc962317827a51162a29819d457
tracer-ysyx <tracer@ysyx.org>
2024-11-24 08:30:07 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.61 #1-NixOS SMP PREEMPT_DYNAMIC Thu Nov 14 12:19:41 UTC 2024 x86_64 GNU/Linux  08:30:07  up 1 day 12:58,  1 user,  load average: 1.44, 1.31, 1.02

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -111,7 +111,7 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   void *ustack=((void*)pgalloc_usr(PGSIZE*8))+PGSIZE*8;
   //TODO!!
   // void *ustack=kstack.end;
-  Context *top = (Context *)(((void *)kstack.end) - sizeof(Context));
+  Context *top = (Context *)(((void *)ustack) - sizeof(Context));
   top->GPRx=(uintptr_t)ustack;//pass the stack addr,seems OKEY for riscv--ARCH-spec
   // //map stack
   for (int i = 0; i < 8; i++) {
