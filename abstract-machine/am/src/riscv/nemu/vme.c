cef4848f4ec73b854a3d280261e8e542e1916aef
tracer-ysyx <tracer@ysyx.org>
2024-11-25 16:49:23 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.62 #1-NixOS SMP PREEMPT_DYNAMIC Sun Nov 17 14:08:59 UTC 2024 x86_64 GNU/Linux  16:49:23  up   4:42,  1 user,  load average: 0.93, 0.96, 1.03

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -108,8 +108,8 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 // kstack是内核栈,用于分配上下文结构,
 // entry则是用户进程的入口.
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
-  void *ustack1 = pgalloc_usr(PGSIZE * 8);
-  printf("%x\n",ustack1);
+  // void *ustack1 = pgalloc_usr(PGSIZE * 8);
+  // printf("%x\n",ustack1);
   //TODO!!
   void *ustack=kstack.end;
   Context *top = (Context *)(((void *)ustack) - sizeof(Context));
