3ec0d1f5cd7f6cc616524eae8011222821562ee0
tracer-ysyx <tracer@ysyx.org>
2024-11-25 08:44:03 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux nixos 6.6.62 #1-NixOS SMP PREEMPT_DYNAMIC Sun Nov 17 14:08:59 UTC 2024 x86_64 GNU/Linux  08:44:03  up   0:31,  1 user,  load average: 2.10, 1.61, 1.12

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -57,7 +57,7 @@ bool cte_init(Context *(*handler)(Event, Context *)) {
 uint32_t mie = 0x8;
 asm volatile(
     "csrr t0, mstatus  \n\t"  // 从 mtvec 读取值到 t0
-    "and t0,t0, %0      \n\t"  // 对 t0 和输入操作数进行按位与操作
+    "or t0,t0, %0      \n\t"  // 对 t0 和输入操作数进行按位与操作
     "csrw mstatus, t0 \n\t" // 将 t0 的值写入 mstatus
     : 
     : "r" (mie)             // 输入操作数
