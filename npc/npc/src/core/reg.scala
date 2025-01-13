bf5faf7473c45e7c87751b6e907c39ae5545252d
tracer-ysyx <tracer@ysyx.org>
2025-01-13 10:25:59 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.6.69 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan  2 09:32:11 UTC 2025 x86_64 GNU/Linux  10:25:59  up ???? days ??:??,  1 user,  load average: 1.60, 1.74, 1.36

--- npc/npc/src/core/reg.scala
+++ npc/npc/src/core/reg.scala
@@ -17,7 +17,7 @@ class REG extends Module {
     val csr_mstvec = Output(UInt(CVAL.DLEN.W))
   })
 
-  val regs = RegInit(VecInit(Seq.fill(15)(0.U(CVAL.DLEN.W))))
+  val regs = RegInit(VecInit(Seq.fill(31)(0.U(CVAL.DLEN.W))))
   val csrs = RegInit(VecInit(Seq.fill(4)(0.U(CVAL.DLEN.W))))
 
   //:暂时简化实现，这两个寄存器的值保持不动
@@ -25,8 +25,8 @@ class REG extends Module {
   csrs(2.U) := 0x1800.U; //mstatus
 
   // regs(0.U)      := 0.U
-  io.Rread1.data := Mux(io.Rread1.addr(3, 0) === 0.U, 0.U, regs(io.Rread1.addr(3, 0) - 1.U))
-  io.Rread2.data := Mux(io.Rread2.addr(3, 0) === 0.U, 0.U, regs(io.Rread2.addr(3, 0) - 1.U))
+  io.Rread1.data := Mux(io.Rread1.addr(3, 0) === 0.U, 0.U, regs(io.Rread1.addr(4, 0) - 1.U))
+  io.Rread2.data := Mux(io.Rread2.addr(3, 0) === 0.U, 0.U, regs(io.Rread2.addr(4, 0) - 1.U))
 //目前只实例化了这几个reg，使用mux来获取寄存器的数值,默认获取mepc
   val csr_r_ADDR = MuxLookup(io.CSRread.addr, 3.U)(
     Seq(
@@ -56,7 +56,7 @@ class REG extends Module {
   io.csr_mstvec := csrs(0.U) //mstvec--支持ecall
 
   when(io.Rwrite.write_enable && io.Rwrite.addr =/= 0.U) { //保证寄存器不被写入
-    regs(io.Rwrite.addr(3, 0) - 1.U) := io.Rwrite.data
+    regs(io.Rwrite.addr(4, 0) - 1.U) := io.Rwrite.data
   }
   when(io.CSRwrite.write_enable) {
     csrs(csr_w_addr) := io.CSRwrite.data
