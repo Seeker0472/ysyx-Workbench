672f820ba034b025ad7bf250e65dcedd34808003
tracer-ysyx <tracer@ysyx.org>
2025-01-25 17:43:25 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.6.69 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan  2 09:32:11 UTC 2025 x86_64 GNU/Linux  17:43:25  up ???? days ??:??,  1 user,  load average: 0.58, 0.71, 0.78

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -50,6 +50,7 @@ static uint32_t dummy = 0;
 
 static inline bool check_defined(uint32_t idx, Decode *s) {
   bool okey = false;
+#ifdef CLOCHEBLOCK
   switch (idx) {
 #define GenCSR(name, paddr)                                                    \
   case NEMU_CSR_V_##name :
@@ -61,6 +62,8 @@ static inline bool check_defined(uint32_t idx, Decode *s) {
   default:
     Log("WARRNING:Unsupported CSR NO:(0x%x)", idx);
   }
+
+#endif
   switch (idx) {
 #define GenCSR(name, paddr)                                                    \
   case NEMU_CSR_V_##name:                                                      \
@@ -69,6 +72,9 @@ static inline bool check_defined(uint32_t idx, Decode *s) {
     CSR_LIST
 #undef GenCSR
   default:
+    s->dnpc = isa_raise_intr(2, s->pc);
+    cpu.csr[NEMU_CSR_V_MTVAL]=s->isa.inst.val;
+
     Log("WARRNING:Unsupported CSR NO:(0x%x)", idx);
   }
   return okey;
