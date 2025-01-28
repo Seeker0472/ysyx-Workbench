5a92bb88e0c99f00dbd52e07c97bbdaaf88a5263
tracer-ysyx <tracer@ysyx.org>
2025-01-29 00:01:36 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.6.69 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan  2 09:32:11 UTC 2025 x86_64 GNU/Linux  00:01:36  up  19:29,  1 user,  load average: 0.88, 0.64, 0.51

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -31,7 +31,7 @@ void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 // 初始化REF的DiffTest功能
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
-
+// 自己实现的
 void (*ref_difftest_csr_notexist)(void) = NULL;
 
 #ifdef CONFIG_DIFFTEST
