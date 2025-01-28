c91d736183d6eae8f3a78154059eac6d73bc4ad7
tracer-ysyx <tracer@ysyx.org>
2025-01-28 13:35:39 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.6.69 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan  2 09:32:11 UTC 2025 x86_64 GNU/Linux  13:35:39  up   9:03,  1 user,  load average: 0.24, 0.14, 0.27

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -69,6 +69,10 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
   }
 }
 
+void difftest_csr_notexist(){
+  ref_difftest_csr_notexist();
+}
+
 void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
 
