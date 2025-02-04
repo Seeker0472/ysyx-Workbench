db0cfd409b04d9598c36a13474eb43f20f23652a
tracer-ysyx <tracer@ysyx.org>
2025-02-04 17:40:44 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  17:40:44  up  18:38,  1 user,  load average: 0.84, 1.26, 4.13

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,21 +18,23 @@
 #include "../local-include/reg.h"
 extern const char* regs[];
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  bool ret=true;
   if(!difftest_check_reg("pc",pc,ref_r->pc,cpu.pc))
-    return false;
+    ret = false;
   for(int i=0;i<32;i++){
     if(!difftest_check_reg(regs[i],pc,ref_r->gpr[i],cpu.gpr[i]))
-      return false;
+      ret =  false;
   }
-  return true;
+  return ret;
 }
 
 bool isa_difftest_checkcsrs(word_t *ref_csr,vaddr_t pc) {
+  bool ret=true;
   for(int i=0;difftest_csr_idx[i]!=0;i++){
     if(!difftest_check_reg(difftest_csr_name[i],pc,ref_csr[i],cpu.csr[difftest_csr_idx[i]]))
-      return false;
+      ret =  false;
   }
-  return true;
+  return ret;
 }
 
 void isa_difftest_attach() {
