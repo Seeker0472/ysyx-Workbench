5eb1cd7b4a14030714d52ccb17fe2c9acbff8745
tracer-ysyx <tracer@ysyx.org>
2025-02-04 20:29:12 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  20:29:12  up  21:26,  1 user,  load average: 1.00, 0.84, 0.97

--- nemu/tools/gdbstub.mk
+++ nemu/tools/gdbstub.mk
@@ -1,7 +1,7 @@
 LIB_GDBSTUB := $(NEMU_HOME)/tools/mini-gdbstub/build/libgdbstub.a
 
 $(LIB_GDBSTUB):
-	cd  $(NEMU_HOME)/tools/mini-gdbstub && make
+	cd $(NEMU_HOME)/tools/mini-gdbstub && make
 
 
 .PHONY: $(LIB_GDBSTUB)
