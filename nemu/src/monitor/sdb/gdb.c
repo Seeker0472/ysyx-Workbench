5dd372da4ab70d4189eae43ca866841662681ff2
tracer-ysyx <tracer@ysyx.org>
2025-02-04 22:48:39 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  22:48:39  up  23:46,  1 user,  load average: 0.92, 0.76, 0.85

--- nemu/src/monitor/sdb/gdb.c
+++ nemu/src/monitor/sdb/gdb.c
@@ -44,7 +44,7 @@ assert(0);
 static int nemu_read_mem(void *args, size_t addr, size_t len, void *val) {
   printf("READMEM:%lx,len:%lx\n",addr,len);
   if(!in_pmem(addr)||!in_pmem((paddr_t)addr+len)){
-    return -1;
+    return 0;
   }
   uint8_t* host = guest_to_host(addr);
   memcpy(val, host, len);
