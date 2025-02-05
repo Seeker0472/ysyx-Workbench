ae6b6f8549b8f84343c5c74f04657c920c84f528
tracer-ysyx <tracer@ysyx.org>
2025-02-05 15:42:06 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  15:42:06  up ???? days ??:??,  1 user,  load average: 4.45, 3.67, 2.33

--- nemu/src/monitor/sdb/gdb.c
+++ nemu/src/monitor/sdb/gdb.c
@@ -70,7 +70,6 @@ char* int_to_string(int value) {
 #define NEMU_FEATURES \
   "<target version=\"1.0\"><architecture>riscv:rv32</architecture>"  \
   NEMU_REG_TAGS \
-  NEMU_OTHER_TAGS \
   NEMU_CSR_TAGS \
   "</target>"
 
