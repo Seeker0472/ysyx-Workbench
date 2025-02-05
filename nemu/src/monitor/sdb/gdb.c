9035743def36e51cb0ee83e147bfefe8c4d9f957
tracer-ysyx <tracer@ysyx.org>
2025-02-05 16:05:47 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  16:05:47  up ???? days ??:??,  1 user,  load average: 0.74, 1.44, 1.99

--- nemu/src/monitor/sdb/gdb.c
+++ nemu/src/monitor/sdb/gdb.c
@@ -73,6 +73,9 @@ char* int_to_string(int value) {
   NEMU_OTHER_TAGS \
   NEMU_CSR_TAGS \
   "</target>"
+#define DEFAULT_FET \
+  "<target version=\"1.0\"><architecture>riscv:rv32</architecture>"  \
+  "</target>"
 
 void step(uint64_t n){
   cpu_exec(n);
@@ -190,7 +193,7 @@ void init_gdb() {
                         .smp = 1,
                         .reg_num = 33,
                         .reg_byte = 4,
-                        .target_desc = NEMU_FEATURES,
+                        .target_desc = DEFAULT_FET,
                     },
                     "127.0.0.1:1234")) {
     fprintf(stderr, "Fail to create socket.\n");
