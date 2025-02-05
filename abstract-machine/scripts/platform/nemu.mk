de3a58b1f7223c14386a7316d0ca8e85ae7205f1
tracer-ysyx <tracer@ysyx.org>
2025-02-05 10:29:47 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  10:29:47  up 1 day 11:27,  1 user,  load average: 1.32, 1.00, 0.76

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,7 +12,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 # -b 是用来通过批处理方式运行NEMU
-ELF += $(IMGAE).elf
+ELF += $(IMAGE).elf
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt  -e $(IMAGE).elf -b $(COMMAND_FLAGS_BASE)
 GDB_NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt  -e $(IMAGE).elf $(COMMAND_FLAGS_BASE)
 
