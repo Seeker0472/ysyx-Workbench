4aa9628ba3909f29689db8a5b6b5088f09c1d0b4
tracer-ysyx <tracer@ysyx.org>
2025-02-06 11:19:31 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  11:19:31  up  15:49,  1 user,  load average: 2.83, 2.92, 2.30

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -34,7 +34,7 @@ IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 ifdef CONFIG_DEBUG_GDB
-	RUN_REMOTE := tmux split-window -h -p 65 "riscv64-unknown-linux-gnu-gdb -ex \"target remote localhost:1234\" $(ELF)"
+	RUN_REMOTE := tmux split-window -h -p 65 "riscv64-unknown-linux-gnu-gdb -ex \"target remote /tmp/gdbstub.sock\" $(ELF)"
 else
 	RUN_REMOTE := 
 endif
