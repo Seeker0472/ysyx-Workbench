6f04c2c58b823fd68cf6ee181f80ba017fa20322
tracer-ysyx <tracer@ysyx.org>
2025-02-10 16:04:19 +0800

    > compile NEMU ysyx_23060325 郭明轩 Linux miLaptop 6.12.11 #1-NixOS SMP PREEMPT_DYNAMIC Thu Jan 23 16:23:05 UTC 2025 x86_64 GNU/Linux  16:04:19  up ???? days ??:??,  1 user,  load average: 3.54, 3.12, 3.18

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -36,7 +36,7 @@ paddr_t riscv_intr_gotos (word_t NO,vaddr_t epc){
   NEMU_sstatus->bits.SPP=cpu.PRIV==NEMU_PRIV_HS?1:0;
   printf("STVALN>%x\n",stval_nextvalue);
   cpu.csr[NEMU_CSR_STVAL] = stval_nextvalue;
-  stval_nextvalue = 0;//???ecall!
+  //stval_nextvalue = 0;//???ecall!
   cpu.PRIV=NEMU_PRIV_HS;
   return cpu.csr[NEMU_CSR_STVEC];
 }
